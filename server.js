b44575507a0792af71e205e1253eba715dd942c7
Ryan Hamblin <ryanhca@gmail.com>
2018-01-23 18:32:45 -0700

    server updates

--- server.js
+++ server.js
@@ -5,58 +5,58 @@ const cors = require('cors');
 const app = express();
 
 const friends = [
-	{
-		name: 'Ben',
-		age: 30,
-		email: 'ben@lambdaschool.com',
-	},
-	{
-		name: 'Austen',
-		age: 45,
-		email: 'austen@lambdaschool.com',
-	},
-	{
-		name: 'Ryan',
-		age: 15,
-		email: 'ryan@lambdaschool.com',
-	},
-	{
-		name: 'Sean',
-		age: 35,
-		email: 'sean@lambdaschool.com',
-	},
-	{
-		name: 'Michelle',
-		age: 67,
-		email: 'michelle@gmail.com',
-	},
+  {
+    name: 'Ben',
+    age: 30,
+    email: 'ben@lambdaschool.com'
+  },
+  {
+    name: 'Austen',
+    age: 45,
+    email: 'austen@lambdaschool.com'
+  },
+  {
+    name: 'Ryan',
+    age: 15,
+    email: 'ryan@lambdaschool.com'
+  },
+  {
+    name: 'Sean',
+    age: 35,
+    email: 'sean@lambdaschool.com'
+  },
+  {
+    name: 'Michelle',
+    age: 67,
+    email: 'michelle@gmail.com'
+  }
 ];
 
 app.use(bodyParser.json());
 
 app.use(cors());
 
-app.get('/friends', (req, res) => {
-	res.send(friends);
+app.get('/api/friends/get', (req, res) => {
+  res.send(friends);
 });
 
-app.post('/new-friend', (req, res) => {
-	friends.push(req.body);
-	res.send(friends);
+app.post('/api/friends/create', (req, res) => {
+  friends.push(req.body);
+  res.send(friends);
 });
 
-app.put('/update-friend', (req, res) => {
-	const index = req.body.index;
-	friends[index] = req.body.update;
-	res.send(friends);
+app.put('/api/friends/update', (req, res) => {
+  const index = req.body.index;
+  friends[index] = req.body.update;
+  res.send(friends);
 });
 
-app.delete('/delete-friend', (req, res) => {
-	const index = req.body.index;
-	friends.splice(index, 1);
-	res.send(friends);
+app.delete('/api/friends/delete', (req, res) => {
+  const index = req.body.index;
+  friends.splice(index, 1);
+  res.send(friends);
 });
 
-app.listen(5000, () => {
-	console.log('server listening on port 5000');
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
