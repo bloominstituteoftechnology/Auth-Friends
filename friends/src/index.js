eeb9e204f0e4377c7547d25b95af31b13aebf3d1
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-22 14:55:07 -0700

    Import Provider and createStore

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+// import reducers
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let STORAGE_WAREHOUSE = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store={STORAGE_WAREHOUSE}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
+
 registerServiceWorker();
