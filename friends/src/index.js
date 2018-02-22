ac71154aed2b70fe06ee4d2f62bb3d56c9f3c056
Terrie Boeun Kim <TERR1E@Terries-MacBook-Pro.local>
2018-02-22 12:51:35 -0800

    set up actions, components, reducers - Terrie

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+import './index.css';
+
+const store = createStore( rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
