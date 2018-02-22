b07e7c9eb206acd89a52b9e7ec130004f1da7826
Lo Saephan <teamsky916@gmail.com>
2018-02-22 13:41:37 -0800

    set up index.js - added provider and store, created reducers folder, added friend reducer

---
+++ friends/src/reducers/friendReducer.js
@@ -0,0 +1,29 @@
+import { ADD_FRIEND, UPDATE_FRIEND} from '.actions';
+
+const initialState = {
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
+};
+
+export const friendReducer = (state = initialState) => {
+	switch () {
+		case FETCHING_FRIENDS:
+			return {...state, fetchingFriends: true}
+
+		case FETCH_FRIENDS:
+			return {
+				...state,
+				friendFetched: true,
+				fetchingFriends: false,
+				friends: Object.keys(action.payload)
+			}
+	}
+}
