2d48c877deb589ce8c782e390fa0448caf8f3b2a
Smithface <kylewibblesmith@gmail.com>
2018-02-22 17:05:56 -0500

    filled out Friends.js
    ready to test

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,3 +1,14 @@
-const Friends = () => {
-  return null;
+import React from 'react';
+
+
+const Friends = (props) => {
+  return (
+    <ul>
+      {props.friends.map((friend, i) => {
+        return <li key={i+'a'}> {friend.name} </li>
+      })}
+    </ul>
+  );
 }
+
+export default Friends;
