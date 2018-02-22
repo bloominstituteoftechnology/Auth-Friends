e46da2bcb280817eadc784bf8e4fbe3ed244f35c
rawalhome@gmail.com <rawalhome@gmail.com>
2018-02-22 17:01:03 -0600

    Punit Rawal + Eric Hechavarria..working on stuff

---
+++ friends/src/actions/index.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
+export const FRIENDS_FETCHED = 'FRIENDS_FETCHED';
+export const FRIENDS_SAVED = 'FRIENDS_SAVED';
+export const SAVING_FRIENDS = 'SAVING_FRIENDS';
+// export const UPDATING_FRIEND = 'UPDATING_FRIEND';
+// export const FRIEND_UPDATED = 'FRIEND_UPDATED';
+// export const DELETING_FRIEND = 'DELETING_FRIEND';
+// export const FRIEND_DELETED = 'FRIEND_DELETED';
+export const ERROR_FETCHING_FRIENDS = 'ERROR_FETCHING_FRIENDS';
+export const ERROR_SAVING_FRIENDS = 'ERROR_SAVING_FRIENDS';
+// export const ERROR_UPDATING_FRIENDS = 'ERROR_UPDATING_FRIENDS';
+// export const ERROR_DELETING_FRIENDS = 'ERROR_DELETING_FRIENDS';
+
+export const getFriends = () => {
+  const friends = axios.get('http://localhost:5000/api/friends/get');
+  return dispatch => {
+    dispatch({ type: FETCHING_FRIENDS });
+    friends
+      .then(response => {
+        dispatch({ type: FRIENDS_FETCHED, payload: response.data.friends });
+      })
+      .catch(error => {
+        dispatch({ type: ERROR_FETCHING_FRIENDS, payload: error });
+      });
+  };
+};
+
+
+export const createFriend = newFriend => {
+  const friends = axios.post('http://localhost:5000/api/friends/create', newFriend ) ;
+  return dispatch => {
+    dispatch({ type: SAVING_FRIENDS });
+    friends
+      .then(response => getFriends())
+      .then(response => {
+        dispatch({ type: FRIENDS_SAVED });
+      })      
+      .catch(error => {
+        dispatch({ type: ERROR_SAVING_FRIENDS, payload: error });
+      });
+  };
+};
+
+
+// export const updateFriend = newFriend => {
+//   const friends = axios.put('http://localhost:5000/api/friends/update', newFriend ) ;
+//   return dispatch => {
+//     dispatch({ type: SAVING_FRIENDS });
+//     friends
+//       .then(response => getFriends())
+//       .then(response => {
+//         dispatch({ type: FRIENDS_SAVED });
+//       })      
+//       .catch(error => {
+//         dispatch({ type: ERROR_SAVING_FRIENDS, payload: error });
+//       });
+//   };
+// };
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+//import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+import './index.css';
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <AddToDo />
+    </div>
+  </Provider>,
+  document.getElementById('root')
+);
