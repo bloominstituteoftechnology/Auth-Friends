5005218b6440496942891ae82bbdc58d5ee3dade
Charlie Sparks <csparks@antioch.edu>
2018-02-22 14:51:02 -0800

    file structure modified and setup

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from "react-redux";
+import FriendForm from "./FriendForm";
+import { getFriends } from "./FriendForm";
+import Friends from "./Friends";
+
+
 
 class App extends Component {
   render() {
