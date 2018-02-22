0e600137139fd3e825a0a79e57766028da11df6c
Tommy Coleman <tommycoleman87@gmail.com>
2018-02-22 16:16:03 -0500

    set up skeleton for index.js

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
