932a4faa64ecf604200a71d52ecadd0ea77450b7
Smithface <kylewibblesmith@gmail.com>
2018-02-22 17:00:27 -0500

    filled out App.js
    also added basic Friends.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import { getFriends } from './actions/getFriends';
+import Friends from './components/Friends';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getFriends();
+  }
   render() {
+    const { friends } = this.props;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Friends friends={friends} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    friends: state.friends,
+    fetchingFriends: state.fetchingFriends,
+    error: state.error,
+  };
+};
+
+export default connect(mapStateToProps, { getFriends })(App);
