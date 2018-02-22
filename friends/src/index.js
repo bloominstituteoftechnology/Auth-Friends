6a98d395523ba6e33988a02383127d1e1b302d26
Christopher Beards <beardscm@gmail.com>
2018-02-22 16:19:08 -0500

    Wrapped App with Provider

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+<Provider>
+  <App />
+</Provider>, document.getElementById('root')
+);
+
