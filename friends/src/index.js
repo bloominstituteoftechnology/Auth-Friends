c553667cf886fdba7d5edbb9971ec8597c9e04de
ala ayaad <ala_ayaad@yahoo.com>
2018-02-22 13:51:10 -0800

    next step

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom';
 import Friends from './components/Friends';
 //the redux stuff
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import friend from './reducers/Reducers';
-
+import logger from 'redux-thunk';
+import thunk from 'redux-logger';
+//styles goes here
 import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 
-const store = createStore(friend);
+const store = createStore(friend, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
     <Provider store={store}>
