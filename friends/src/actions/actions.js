b351dae37a85444b4f0917e8dd3c8e7ba5167ee9
David Soudry <davsoudry@gmail.com>
2018-02-22 15:57:09 -0600

    store and actions

---
+++ friends/src/actions/actions.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+export const IS_FETCHING = 'IS_FETCHING';
+export const ADDING_FRIEND = 'ADDING_FRIEND';
+export const FRIEND_ADDED = 'FRIEND_ADDED';
+export const ERROR = 'ERROR';
+export const FRIENDS_FETCHED = 'FRIENDS_FETCHED';
+
+export const getFriends = () => {
+    const friends = axios.get('http://localhost:5000/api/friends');
+    return dispatch => {
+        dispatch({ type: IS_FETCHING });
+        friends
+            .then(({ data }) => {
+                dispatch({ type: FRIENDS_FETCHED, payload: res.data });
+            })
+            .catch(err => {
+                dispatch({ type: ERROR, payload: err });
+            });  
+    };
+};
+
+export const addFriend = friend => {
+    const newFriend = axios.post('http://localhost:5000/api/friends/create', friend);
+    return dispatch => {
+        dispatch({ type: ADDING_FRIEND });
+        addFriend
+            .then(({ data }) => {
+                dispatch({ type: FRIEND_ADDED, payload: data });
+            })
+            .catch(err => {
+                dispatch({ type: ERROR, payload: err });
+            });  
+    };
+}
--- friends/src/index.js
+++ friends/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
+
 registerServiceWorker();
