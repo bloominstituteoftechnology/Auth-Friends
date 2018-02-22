3eb6e0257231b9d9590ef9b471e56fc68391344e
itsdevinci <devinbaldwin315@gmail.com>
2018-02-22 14:40:34 -0800

    Reducers and Index changes
    -added middleware to index.js.
    -added thunk and logger to middleware.
    -created NEW_FRIEND reducer.

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
-
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 
 ReactDOM.render(
-  <Provider store={createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <Provider store={createStore(rootReducer, applyMiddleware(thunk, logger), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
       <App />
   </Provider>
 , document.getElementById('root'));
--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,8 +1,23 @@
-import friendsReducer from './friendsReducer';
-import { combineReducers } from 'redux';
+import { NEW_FRIEND } from '../actions';
 
-const rootReducer = combineReducers({ 
-    friends: friendsReducer
- });
+const friendState = {
+    fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
+}
 
- export default rootReducer;
+export default (friends = [], action) => {
+    switch (action.type) {
+        case NEW_FRIEND:
+            return [[...friends], action.payload];
+        default: 
+            return friends;
+    }
+};
