852bd8de887fa99c9be353c5285f50cc2797879b
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-02-22 15:03:57 -0600

    Update index.html
    add imports and redux Provider wrapper to index.html

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <App />
+</Provider>, document.getElementById('root'));
+
 registerServiceWorker();
