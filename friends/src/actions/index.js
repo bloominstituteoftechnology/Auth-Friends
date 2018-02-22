51910cd834f290e2604e596ea16d2fa3224969f5
aceredesign <steven.magadan@hotmail.com>
2018-02-22 13:38:36 -0800

    added get

---
+++ friends/src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+export const GET_FRIENDS = "GET_FRIENDS";
+export const GETTING_FRIENDS = "GETTING_FRIENDS";
+export const ERROR = "ERROR";
+
+export const getFriends = () => {
+  const friends = axios.get("https://localhost:5000/api/friends");
+  return dispatch => {
+    dispatch({ type: GETTING_FRIENDS });
+    friends
+      .then(response => {
+        dispatch({ type: GET_FRIENDS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
