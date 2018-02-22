0d5d85421af1d2dc52254a7e3ec464537151446b
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-22 16:04:30 -0700

    Begin creating friend actions

---
+++ friends/src/actions/friendActions.js
@@ -0,0 +1,19 @@
+export const ADD_FRIEND = 'ADD_FRIEND';
+export const UPDATE_FRIEND = 'UPDATE_FRIEND';
+export const DELETE_FRIEND = 'DELETE_FRIEND';
+
+/*
+{
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
+}
+*/
+
