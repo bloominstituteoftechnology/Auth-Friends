917d1d091649fb3f1da5638ba28e1dea9207c4a3
Smithface <kylewibblesmith@gmail.com>
2018-02-22 16:46:03 -0500

    added store to index.js
    also deleted random React nonsense

--- friends/src/index.js
+++ friends/src/index.js
@@ -7,9 +7,10 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-<Provider>
+<Provider store={store}>
   <App />
 </Provider>, document.getElementById('root')
 );
