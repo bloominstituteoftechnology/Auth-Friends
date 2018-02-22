2c1bb280afe7ea15c5c2cc0fdf2c06497a5bdca6
Tommy Coleman <tommycoleman87@gmail.com>
2018-02-22 17:49:43 -0500

    filled out actions

--- friends/src/actions/actions.js
+++ friends/src/actions/actions.js
@@ -1,10 +1,23 @@
 export const GETTING_FRIENDS = 'GETTING_FRIENDS';
+export const FRIENDS_RECEIVED = 'FRIENDS_RECEIVED';
+export const ERROR_GETTING_FRIENDS = 'ERROR_GETTING_FRIENDS';
+
+export const CREATE_FRIEND = 'CREATE_FRIEND';
+export const FRIEND_CREATED = 'FRIEND_CREATED';
+export const ERROR_CREATING_FRIEND = 'ERROR_CREATING_FRIEND';
+
 export const DELETE_FRIEND = 'DELETE_FRIEND';
+export const FRIEND_DELETED = 'FRIEND_DELETED';
+export const ERROR_DELETING_FRIEND = 'ERROR_DELETING_FRIEND';
+
 export const UPDATE_FRIEND = 'UPDATE_FRIEND';
+export const FRIEND_UPDATED = 'FRIEND_UPDATED';
+export const ERROR_UPDATING_FRIEND = 'ERROR_UPDATING_FRIEND';
 
 
 export const getFriends = () => {
     dispatch => {
+        dispatch({type: GETTING_FRIENDS});
         axios.get('http://localhost:5000/api/friends/get')
         .then(({data}) =>
         dispatch({type: FRIENDS_RECEIVED, payload: data}))
@@ -16,15 +29,39 @@ export const getFriends = () => {
 
 export const createFriend = () => {
     dispatch => {
+        dispatch({type: CREATE_FRIEND})
         axios.post('http://localhost:5000/api/friends/create')
-        .then()
-        .catch()
+        .then(({data}) => {
+            dispatch({type: FRIEND_CREATED, payload: data})
+        })
+        .catch(error => {
+            dispatch({type: ERROR_CREATING_FRIEND, payload: error})
+        })
+    }
+}
+
+export const deleteFriend = () => {
+    dispatch => {
+        dispatch({type: DELETE_FRIEND})
+        axios.delete(`http://localhost:5000/api/friends/delete/${id}`)
+        .then(({data}) =>
+        dispatch({type: FRIEND_DELETED, payload: data}))
+        .catch(error =>{
+            dispatch({type: ERROR_DELETING_FRIEND, payload: error})
+        })
     }
 }
 
 export const updateFriend = () => {
     dispatch => {
+        dispatch({type: UPDATE_FRIEND})
         axios.put(`http://localhost:5000/api/friends/update/${id}`)
+        .then(({data}) => {
+            dispatch({type: FRIEND_UPDATED, payload: dada})
+        })
+        .catch(error => {
+            dispatch({type: ERROR_UPDATING_FRIEND, payload: error})
+        })
     }
 }
 
