65a358a64d697a9d82ef77e1f4f88bff0821ad72
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-22 15:21:13 -0700

    Import reducers

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import { Provider } from 'react-redux';
-// import reducers
+import reducers from './reducers/friendReducers';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
