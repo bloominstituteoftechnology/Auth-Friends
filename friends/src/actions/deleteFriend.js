7d7ed876b998af3fa62ce5e9fa76a924d07cdda2
Christopher Beards <beardscm@gmail.com>
2018-02-23 12:43:16 -0500

    Created deleteFriend.js

---
+++ friends/src/actions/deleteFriend.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+export const IS_DELETING = 'IS_DELETING';
+export const FRIEND_DELETED = 'FRIEND_DELETED';
+export const ERROR_DELETING_FRIEND = 'ERROR_DELETING_FRIEND';
+
+export const deleteFriend = () => {
+  const friends = axios.delete('http://localhost:5000/api/friends/delete');
+  return dispatch => {
+    dispatch({ type: IS_DELETING });
+    friends
+      .then(({ data }) => {
+        dispatch({ type: FRIEND_DELETED, payload: data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR_DELETING_FRIEND, payload: err });
+      });
+  };
+};
