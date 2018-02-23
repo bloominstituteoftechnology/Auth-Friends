47b5a2758376668e1d0182a7c39c9b73ad722157
Res Yeg <shaqk702@gmail.com>
2018-02-22 17:02:16 -0800

    introduced const URL for easier access

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -3,8 +3,10 @@ export const GET_FRIENDS = "GET_FRIENDS";
 export const GETTING_FRIENDS = "GETTING_FRIENDS";
 export const ERROR = "ERROR";
 
+const URL = 'http://localhost:5000/api/friends';
+
 export const getFriends = () => {
-  const friends = axios.get("https://localhost:5000/api/friends/get");
+  const friends = axios.get(`${URL}/get`);
   return dispatch => {
     dispatch({ type: GETTING_FRIENDS });
     friends
