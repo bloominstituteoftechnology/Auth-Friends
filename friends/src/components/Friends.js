0c1f4bb0ff69cce8ce7da4e1755b2e2847e485c1
Dani Tacheny <dani.tacheny@gmail.com>
2018-02-23 12:25:02 -0600

    refactor Friends to functional component
    we didn't need a state so we made it functional

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -2,24 +2,22 @@ import React from 'react';
 import { deleteFriend } from '../actions';
 import { connect } from 'react-redux';
 
-class Friends extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Friends</h1>
-        {this.props.friends.map((friend, index) => {
-          return (
-            <ul key={index} id={index} style={ { listStyle: 'none' } }>
-              <li>Name: {friend.name}</li>
-              <li>Age: {friend.age}</li>
-              <li>Email: {friend.email}</li>
-              <button onClick={() => this.props.deleteFriend(index)}>Delete</button>
-            </ul>
-          );
-        })}
-      </div>
-    );
-  }
+const Friends = props => {
+  return (
+    <div>
+      <h1>Friends</h1>
+      {props.friends.map((friend, index) => {
+        return (
+          <ul key={index} id={index} style={{ listStyle: 'none' }}>
+            <li>Name: {friend.name}</li>
+            <li>Age: {friend.age}</li>
+            <li>Email: {friend.email}</li>
+            <button onClick={() => props.deleteFriend(index)}>Delete</button>
+          </ul>
+        );
+      })}
+    </div>
+  );
 };
 
 export default connect(null, { deleteFriend })(Friends);
