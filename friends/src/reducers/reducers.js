eb9f519037cc479f5e2e5c7cfe101b7867c38495
Peter Gray <peter@petergraycreative.com>
2018-02-22 18:06:13 -0600

    Add structure of reducers
    Imported Actions, added initial state, wrote switch and cases for reducers

---
+++ friends/src/reducers/reducers.js
@@ -0,0 +1,49 @@
+import {
+  GETTING_FRIENDS,
+  FRIENDS_RECEIVED,
+  ERROR_GETTING_FRIENDS,
+  CREATE_FRIEND,
+  FRIEND_CREATED,
+  ERROR_CREATING_FRIEND,
+  DELETE_FRIEND,
+  ERROR_DELETING_FRIEND,
+  UPDATE_FRIEND,
+  ERROR_UPDATING_FRIEND,
+} from '../actions/actions';
+
+const intialState = {
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  deletingFriend: false,
+  friends: [],
+  error: null,
+};
+
+export const rootReducer = (state = intitialSate, action) => {
+  console.log(`The reducer ran ${action}`);
+  switch (action.type) {
+    case GETTING_FRIENDS:
+    return { ...state, fetchingFriends: true };
+    case FRIENDS_RECEIVED:
+      return [...state, actions.payload];
+    case ERROR_GETTING_FRIENDS:
+      return console.log(actions.error);
+    case CREATE_FRIEND:
+      return state;
+    case FRIEND_CREATED:
+      return state;
+    case ERROR_CREATING_FRIEND:
+      return state;
+    case DELETE_FRIEND:
+      return state;
+    case ERROR_DELETING_FRIEND:
+      return state;
+    case UPDATE_FRIEND:
+      return state;
+    case ERROR_UPDATING_FRIEND:
+      return state;
+  }
+};
