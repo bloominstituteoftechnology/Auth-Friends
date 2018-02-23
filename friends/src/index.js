af20b666fca59e8e83b81a82408bedd64508db76
Charlie Sparks <csparks@antioch.edu>
2018-02-22 16:30:59 -0800

    committing with full file structure

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from "redux";
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
