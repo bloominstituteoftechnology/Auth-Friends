95f65a15023f7524befdf0c3fda6a2777251f1a2
Glenn-David Daniel <d2rd@outlook.com>
2018-02-23 09:53:41 -0800

    Form edits

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,14 +1,11 @@
 import React from 'react';
-
+import FriendList from 'FriendList';
+import 
 class FriendForm extends React.Component {
-  // // method 1: put state in class component but outside render
-  // state = {
-  //   friend: {
-  //     name: 'david',
-  //     age: '64',
-  //     email: 'd2rd@outlook.com'
-  //   }
-  // }
+  // method 1: put state in class component but outside render
+  state={
+    friends:[]
+  }
   
   render() {  //'render' MUST be lower case to be recognized by React!!!!
     return (
@@ -25,7 +22,7 @@ class FriendForm extends React.Component {
         <label>Email</label>
         <input type="email"/>  {/* changing type to email gets help from browser */}
 
-        <button type="submit">Save Friends</button>
+        <button type="submit">Add New Friend</button>
         {/* everything you grab from the server will be a string.  The server will typically handle the conversion of age to a number. */}
       </form>
     );
