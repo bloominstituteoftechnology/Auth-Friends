0dc49d78f88806d13136addb1d5723d7487821e4
Arkoma <aaron@adburk.net>
2018-02-23 13:50:53 -0600

    take out unnecessary axios import

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import axios from 'axios';
 
 //class Friends extends Component {
     // state = {
