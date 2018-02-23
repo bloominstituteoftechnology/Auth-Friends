7a3d00134f3b21d0a411c60bbdb186e32a5cc4ad
Arkoma <aaron@adburk.net>
2018-02-23 09:29:02 -0600

    seperated out Friends component into Friends.js file

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,48 +3,18 @@ import React, { Component } from 'react';
 import './App.css';
 // import { connect } from 'react-redux';
 // import { newFriend } from '../actions';
-import axios from 'axios';
+// import axios from 'axios';
 // import CreateFriendForm from './components/CreateFriendForm';
-// import Friends from './components/Friends';
+import Friends from './components/Friends';
 
 class App extends Component {
-  state = {
-    friends: []
-  }
   render() {
     return (
       <div className="App">
-          {this.state.friends.map((friend, index) => {
-            return (
-            <ul key={index}>
-              <li>{friend.name}</li>
-              <li>{friend.age}</li>
-              <li>{friend.email}</li>
-            </ul>
-          )}
-          )}
-        {/* <CreateFriendForm />
-        <Friends /> */}
+        <Friends />
       </div>
     );
   }
-  componentDidMount() { 
-    this.loadFriends(); 
-  } 
-
-  loadFriends = () => { 
-    axios 
-      .get('http://localhost:5000/api/friends/get') 
-      .then(response => { 
-        this.setState({ 
-        friends: response.data, 
-        }); 
-      }) 
-      .catch(() => { 
-        console.error('error getting data'); 
-      }); 
-  }; 
-  
 }
 
 export default App;
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,3 +1,40 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+class Friends extends Component {
+    state = {
+        friends: []
+    }
+    render() {
+        return (
+            <div>
+                {this.state.friends.map((friend, index) => {
+                return (
+                    <ul key={index}>
+                        <li>{friend.name}</li>
+                        <li>{friend.age}</li>
+                        <li>{friend.email}</li>
+                    </ul>
+                )}
+                )}
+            </div>
+        )
+    }
+    componentDidMount() {
+        this.loadFriends();
+    }
+    loadFriends = () => { 
+        axios 
+          .get('http://localhost:5000/api/friends/get') 
+          .then(response => { 
+            this.setState({ 
+            friends: response.data, 
+            }); 
+          }) 
+          .catch(() => { 
+            console.error('error getting data'); 
+          }); 
+      }; 
+}
+
+export default Friends;
