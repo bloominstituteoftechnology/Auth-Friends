990ec3a6e5dc9c3b6bb54879ff14876c34f9d376
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-02-23 14:12:01 -0500

    Create Friends List
    This functional component will map over the array of friends
    and return each one along with their data as a list item.
    
    Nonfunctioning delete button

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 function Friends (props) {
-  console.log('props.friends', props.friends.length);
+  console.log('friends.js props', props);
   return (
     <ul>
       {props.friends.map((friend, index) => {
-        return <div> 
+        return <div key={index}> 
           <li>Name: {friend.name}</li>
           <li>Age: {friend.age}</li>
           <li>Email: {friend.email}</li>
+          <button onSubmit={props.delete()}>Delete Friend</button>
         </div>
       })}
     </ul>
   )
 }
+
+// const handleDelete = (index) => {
+//   this.props.deleteFriend(index)
+// }
 
 export default Friends;
