7c7df27896c7a02410d83d477afa1b3a51b8c378
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-23 11:30:46 -0700

    Create actions

--- friends/src/actions/friendActions.js
+++ friends/src/actions/friendActions.js
@@ -1,19 +1,33 @@
+export const GET_FRIENDS = 'GET_FRIENDS';
 export const ADD_FRIEND = 'ADD_FRIEND';
 export const UPDATE_FRIEND = 'UPDATE_FRIEND';
 export const DELETE_FRIEND = 'DELETE_FRIEND';
 
-/*
-{
-  fetchingFriends: false,
-  friendsFetched: false,
-  friendsSaved: false,
-  savingFriends: false,
-  updatingFriend: false,
-  friendUpdated: false,
-  deletingFriend: false,
-  friendDeleted: false,
-  friends: [],
-  error: null
-}
-*/
+export const getFriends = (friendData) => {
+  const promise = axios.get('http://localhost:5000/api/friends/get');
+  return {
+      type: GET_FRIENDS,
+      payload: promise,
+  };
+};
 
+export const addFriend = friend => {
+  return {
+    type: ADD_FRIEND,
+    payload: friend,
+  }
+};
+
+export const updateFriend = friend => {
+  return {
+    type: UPDATE_FRIEND,
+    payload: index,
+  }
+};
+
+export const deleteFriend = index => {
+  return {
+    type: DELETE_FRIEND,
+    payload: index,
+  }
+};
