1ebc000f19055b85b37cd19b3d35c55fc9e1a6e5
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-23 11:10:19 -0700

    Import thunk and logger from redux. Small organizational restructure

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,11 +1,17 @@
+// react
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+// redux
 import {createStore} from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import reducers from './reducers/friendReducers';
-import './index.css';
-import App from './App';
+// service worker
 import registerServiceWorker from './registerServiceWorker';
+// styles
+import './index.css';
 
 let STORAGE_WAREHOUSE = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
