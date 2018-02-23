863773d05e3b5d143c51f27eb27d05ed76686dbd
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-02-23 14:06:44 -0500

    Created action functions
    Created action functions which would perform the desired
    axios requests with the server.
    
    Each action function returns the current status of the request,
    and upon successful request, it will return a type and payload
    to be passed to our reducers.
    
    Upon error, they should return an error as well.

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 
 export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
 export const FRIENDS_FETCHED = 'FRIENDS_FETCHED';
-export const ERROR_FETCHING_FRIENDS = 'ERROR_FETCHING_FRIENDS';
-export const ADD_FRIENDS = 'ADD_FRIENDS'
+export const SAVING_FRIENDS = 'SAVING_FRIENDS';
+export const FRIENDS_SAVED = 'FRIENDS_SAVED';
+export const DELETING_FRIENDS = 'DELETING_FRIENDS';
+export const FRIEND_DELETED = 'FRIEND_DELETED';
+export const ERROR = 'ERROR';
+
 
 export const getFriends = () => {
   const friends = axios.get('http://localhost:5000/api/friends/get');
@@ -16,27 +20,52 @@ export const getFriends = () => {
         dispatch({ type: FRIENDS_FETCHED, payload: response.data });
       })
       .catch(error => {
-        dispatch({ type: ERROR_FETCHING_FRIENDS, payload: error });
+        dispatch({ type: ERROR, payload: error });
       });
   }
 }
 
-export const addFriends = (friend) => {
-  return {
-    type: ADD_FRIENDS,
-    payload: friend,
-  }
-}
-export const saveFriends = () => {
-  const friends = axios.post('http://localhost:5000/api/friends/create');
+export const saveFriends = (data) => {
+  const friends = axios.post('http://localhost:5000/api/friends/create', data);
   return dispatch => {
     dispatch({ type: SAVING_FRIENDS });
     friends
       .then(response => {
-        dispatch({ type: FRIENDS_SAVED})
+        console.log('saveFriends response', response)
+        console.log('saveFriends', data)
+        dispatch({ type: FRIENDS_SAVED, payload: data })
       })
       .catch(error => {
-        dispatch({ type: ERROR_SAVING_FRIENDS, payload: error });
+        dispatch({ type: ERROR, payload: error });
+      });
+  }
+}
+
+// export const updateFriends = () => {
+//   const friends = axios.put('http://localhost:5000/api/friends/update', data);
+//   return dispatch => {
+//     dispatch({ type: UPDATING_FRIEND});
+//     friends
+//       .then(response => {
+//         dispatch({ type: FRIEND_UPDATED, payload: data })
+//       })
+//       .catch(error => {
+//         dispatch({ type: ERROR, payload: error };)
+//       });
+//   }
+// }
+
+export const deleteFriend = (id) => {
+  const friends = axios.delete(`http://localhost:5000/api/friends/delete/${id}`);
+  return dispatch => {
+    dispatch({ type: DELETING_FRIENDS });
+    friends
+      .then(response => {
+        dispatch({ type: FRIEND_DELETED, payload: id })
+        this.getFriends();
+      })
+      .catch(error => {
+        dispatch({ type: ERROR, payload: error });
       });
   }
 }
