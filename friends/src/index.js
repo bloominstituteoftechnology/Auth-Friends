0e789b87962d9c09704380853855ef84d1b2a7b2
Arkoma <aaron@adburk.net>
2018-02-23 17:31:18 -0600

    re-added Chrome Devtools with proper syntax

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,14 +4,17 @@
  import App from './App';
  //the redux stuff
  import { Provider } from 'react-redux';
- import { createStore, applyMiddleware } from 'redux';
+ import { createStore, applyMiddleware, compose } from 'redux';
  import friends from './reducers';
  import thunk from 'redux-thunk';
  import logger from 'redux-logger';
  //styles goes here
  import './index.css';
  
- const store = createStore(friends, applyMiddleware(thunk, logger));
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(friends, composeEnhancers(
+   applyMiddleware(thunk, logger)
+ ));
  
  ReactDOM.render(
      <Provider store={store}>
