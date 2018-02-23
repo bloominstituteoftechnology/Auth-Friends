73b3461c8519607058a4b66aec996220699ff334
Res Yeg <shaqk702@gmail.com>
2018-02-22 17:07:11 -0800

    added createFriend action

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -2,6 +2,8 @@ import axios from "axios";
 export const GET_FRIENDS = "GET_FRIENDS";
 export const GETTING_FRIENDS = "GETTING_FRIENDS";
 export const ERROR = "ERROR";
+export const CREATING_FRIEND = 'CREATING_FRIEND';
+export const CREATE_FRIEND = 'CREATE_FRIEND';
 
 const URL = 'http://localhost:5000/api/friends';
 
@@ -12,6 +14,20 @@ export const getFriends = () => {
     friends
       .then(response => {
         dispatch({ type: GET_FRIENDS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
+
+export const createFriend = friend => {
+  const newFriend = axios.post(`${URL}/create`, friend);
+  return dispatch => {
+    dispatch({ type: CREATING_FRIEND });
+    newFriend
+      .then(({ data }) => {
+        dispatch({ type: CREATE_FRIEND, payload: data });
       })
       .catch(err => {
         dispatch({ type: ERROR, payload: err });
