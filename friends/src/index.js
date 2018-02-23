d36da213876f7dbb554b83b597556ada6d17c5b9
itsdevinci <devinbaldwin315@gmail.com>
2018-02-22 16:11:30 -0800

    Index Modification -imported logger from 'redux-thunk'. -imported  thunk from 'redux-logger'. -set store as a const.

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,3 +1,4 @@
+/* Old Code
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -14,3 +15,26 @@ ReactDOM.render(
       <App />
   </Provider>
 , document.getElementById('root'));
+*/
+ //here the react stuff
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+ import App from './components/App';
+ //the redux stuff
+ import { Provider } from 'react-redux';
+ import { createStore, applyMiddleware } from 'redux';
+ import friends from './reducers/Reducers';
+ import logger from 'redux-thunk';
+ import thunk from 'redux-logger';
+ //styles goes here
+ import registerServiceWorker from './registerServiceWorker';
+ import './styles/index.css';
+ 
+ const store = createStore(friends, applyMiddleware(thunk, logger));
+ 
+ ReactDOM.render(
+     <Provider store={store}>
+         <App />
+     </Provider>
+     , document.getElementById('root'));
+ registerServiceWorker();
