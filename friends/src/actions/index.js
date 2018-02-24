cf26fd2db77fea3784b0fcafb9ad0f039bc8a7b5
Roy <Roy.tanjx@gmail.com>
2018-02-23 19:53:38 -0500

    initial Pull

---
+++ friends/src/actions/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const error = 'ERROR';
+export const GET_FRIENDS = 'GET_FRIENDS';
+export const
--- friends/src/index.js
+++ friends/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+
+
+const store = createStore(rootReducer, applyMiddleware (thunk, logger));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
