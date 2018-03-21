d5fd8491607d3033392618a552a199eee9f3733e
KJ Sourcing <tmkogwork@gmail.com>
2018-03-21 16:01:38 -0700

    index init

---
+++ friends/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import './index.css';
+
+import rootReducer from './reducers';
+import App from './App';
+
+
+const middleware = applyMiddleware(thunk)
+const store = createStore( rootReducer, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
