80c323209d32eeb1d5e651d20053bbaa3607ab33
Carlos Andrade <carlos.andrade@outlook.com>
2018-03-21 15:21:59 -0400

    init commit

---
+++ friends/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Redux two
+      </div>
+    );
+  }
+}
+
+export default App;
