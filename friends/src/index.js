718e73ac802705ab2294c46ab2b2b70fc1bc7c90
Nedu Robert <nedumrobert@gmail.com>
2018-04-19 00:13:06 +0100

    set up app to use redux, thunk and logger middlewares

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import './index.css';
-import App from './App';
+import App from './components/App';
+import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 registerServiceWorker();
