75d96e43521a067c4327c6b1ad301398fee1bfdb
Jared Cuffe <jcuffe@gmail.com>
2018-04-18 10:40:29 -0700

    reducers created for fetch actions

---
+++ client/src/reducers/index.js
@@ -0,0 +1,31 @@
+import {
+  FETCH_START,
+  FETCH_SUCCESS,
+  FETCH_ERROR
+} from '../actions'
+
+const initialState = {
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
+}
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case FETCH_START:
+      return Object.assign({}, state, { fetchingFriends: true, friendsFetched: false })
+    case FETCH_SUCCESS:
+      return Object.assign({}, state, { friends: payload, fetchingFriends: false, friendsFetched: true })
+    case FETCH_ERROR:
+      return Object.assign({}, state, { error: payload, fetchingFriends: false, friendsFetched: false })  
+    default:
+      return state  
+  }
+}
