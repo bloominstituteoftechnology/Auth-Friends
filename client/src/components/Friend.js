b15901e17f9a40c50ece5248afecb15e21b41388
Jared Cuffe <jcuffe@gmail.com>
2018-04-18 12:57:59 -0700

    Added edit link

--- client/src/components/Friend.js
+++ client/src/components/Friend.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 const Friend = (props) => (
   <div className='Friend'>
     {props.name}
     {props.age}
     {props.email}
+    <Link to={`/edit/${props.id}`}>Edit</Link>
   </div>
 )
 
