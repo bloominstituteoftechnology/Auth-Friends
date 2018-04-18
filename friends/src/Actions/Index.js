b5b545809c33f05ea65087049078465d2a7c973c
mgchmiel <michaelchmielniak@yahoo.com>
2018-04-18 15:28:00 -0400

    Started to add friends and introduce axios under action folder

---
+++ friends/src/Actions/Index.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+export const 
+ ADD = 'ADD',
+ ADDED = 'ADDED',
+ UPDATE = 'UPDATE',
+ UPDATED = 'UPDATED',
+ DELETE = 'DELETE',
+ DELETED = 'DELETED',
+ FETCHING = 'FETCH',
+ FETCHED = 'FETCHED'
+ ERROR = 'ERROR';
+
+ export const Friends = () => dispatch => {
+     dispatch({ type= Fetch});
+     axios
+        .get(`http://localhost:5000/api/friends`)
+        .then(response => {
+            dispatch({ type: FETCHED, friends: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: Error, errorMessage: 'You Have No Friends'})
+        });
+ };
+
+ export const addFriend = (newFriend) => dispatch => {
+     dispatch({ type: Adding });
+     axios
+        .post(`http://localhost:5000/api/friens`, newFriend)
+        .then(response => {
+            dispatch({ type: ADDED, friends: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: ERROR, errorMessage: 'Cant Add Friends that Arent There'})
+        })
+ }
