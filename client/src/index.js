beada4c652a988def8aeb9a421539b8bff9db55d
Jared Cuffe <jcuffe@gmail.com>
2018-04-18 10:34:14 -0700

    Folder structure, Provider wrapped onto App

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore, applyMiddleware, Provider } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
+)
+registerServiceWorker()
