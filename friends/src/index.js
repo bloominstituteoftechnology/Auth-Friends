74e85324e31c197e2a6ddde5b82496053490cb36
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-19 13:39:27 -0700

    fixed syntax error being thrown

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
