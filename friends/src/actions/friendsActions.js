5604be15f0d18100b0e4141b6fce86be5db17820
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-04-19 14:43:54 -0600

    added promise for fetchFriends

--- friends/src/actions/friendsActions.js
+++ friends/src/actions/friendsActions.js
@@ -6,8 +6,24 @@ export const FETCHING_FRIENDS_ERROR = 'FETCHING_FRIENDS_ERROR';
 
 export const fetchFriends = () => {
   // Set up actions here
-  const friends = axios.get( '/api/friends');
+  const friends = axios.get('/api/friends');
   return dispatch => {
-    dispatch({ type:FETCHING-FRIENDS })
+    dispatch({ type: FETCHING_FRIENDS });
+    friends
+      .then(
+        response => {
+          dispatch({
+            type: FETCHING_FRIENDS_SUCCESS,
+            payload: response.data
+          });
+        })
+        .catch( error => {
+          dispatch({ 
+            type: FETCHING_FRIENDS_ERROR, 
+            payload: "No friends found!"
+          })
+        })
+        }
+      )
   }
 }
