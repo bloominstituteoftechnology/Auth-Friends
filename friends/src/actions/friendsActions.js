41940952106defb48484f8b62720aa0d04709303
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-04-19 14:26:35 -0600

    Fixed fetching values

--- friends/src/actions/friendsActions.js
+++ friends/src/actions/friendsActions.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
-export const FETCHING_FRIENDS;
-export const FETCHING_FRIENDS_SUCCESS;
-export const FETCHING_FRIENDS_ERROR;
+export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
+export const FETCHING_FRIENDS_SUCCESS = 'FETCHING_FRIENDS_SUCCESS';
+export const FETCHING_FRIENDS_ERROR = 'FETCHING_FRIENDS_ERROR';
+
 export const fetchFriends = () => {
   // Set up actions here
-  
+  const friends = axios.get( '/api/friends');
+  return dispatch => {
+    dispatch({ type:FETCHING-FRIENDS })
+  }
 }
