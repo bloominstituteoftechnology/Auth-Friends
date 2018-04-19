b90fa8803e9fb5aa3b414f9bd5c2470df559bbbc
David Finley <dfinley2917@gmail.com>
2018-04-19 18:12:19 -0400

    dinner commit

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,21 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { fetchFriends, createFriend } from "./actions";
+import { connect } from "react-redux";
 
 class App extends Component {
+  state = {
+    friend: ""
+  };
+  componentDidMount() {
+    this.props.fetchFriends();
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {this.props.pending ? (
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        ) : null}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input
+          type="text"
+          placeholder="friend"
+          name="friend"
+          value={this.state.friend}
+          onChange={e => this.setState({ [e.target.name]: e.target.value })}
+        />
+        <button
+          onClick={() => {
+            this.props.createFriend({ friend: this.state.friend });
+            this.setState({ friend: "" });
+          }}
+        >
+          Add Friend
+        </button>
+        {this.props.error !== null ? <h4>{this.props.error}</h4> : null}
+        <ul>
+          {this.props.friends.map(friend => (
+            <li key={friend.id}>{friend.name}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    friends: state.friends,
+    error: state.error,
+    pending: state.pending
+  };
+};
+
+export default connect(mapStateToProps, { fetchFriends, createFriend })(App);
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
