39eea5757c0f56d1fe4f5057fd2ce92f8ea9dcd3
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-04-19 15:12:03 -0700

    input fields implemented & data rendering to web browser

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -4,6 +4,7 @@ export const FETCH_START = 'FETCH_START';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_ERROR = 'FETCH_ERROR';
 
+//Fetch friends
 export const fetchFriends = () => {
     const promise = axios.get(`http://localhost:5000/api/friends`);
     return dispatch => {
@@ -15,5 +16,20 @@ export const fetchFriends = () => {
             .catch(err => {
                 dispatch({ type: FETCH_ERROR, payload: err });
             });
-    };
-};
+        };
+    };    
+
+//Add friends
+export const addFriends = data => {
+    const promise = axios.post(`http://localhost:5000/api/friends`, data);
+    return dispatch => {
+        // dispatch({ type: FETCH_START });
+        promise 
+            .then(response => {
+                dispatch({ type: FETCH_SUCCESS, payload: response.data })
+            })
+            .catch(err => {
+                dispatch({ type: FETCH_ERROR, payload: err });
+            });
+        };
+    };            
--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { fetchFriends, addFriends } from '../actions';  
+import Friend from './Friend';
 
 class App extends Component {
+  state = { 
+    name: '',
+    age: '',
+    email: ''
+  };
+
+  componentDidMount() {
+    this.props.fetchFriends();
+  }
+  
+  onInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onFormSubmit = () => {
+    this.props.addFriends({name: this.state.name, age: this.state.age, email: this.state.email});
+    this.setState({name: '', age: '', email: ''});
+  }
+
+// class App extends Component {
   render() {
     return (
       <div className="App">
@@ -10,12 +33,45 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Kaitlyn's Friend List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <form onSubmit={this.onFormSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={this.state.name}
+          onChange={this.onInputChange}
+        />
+        <input 
+          type="text"
+          name="age"
+          placeholder="Age"
+          value={this.state.age}
+          onChange={this.onInputChange}
+        />
+        <input 
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={this.onInputChange}
+        />
+        <button type='submit'>Add New Friend</button>
+      </form>
+      {this.props.friends.map(friend => (
+        <Friend key={`friend${friend.id}`} friend={friend}/>
+      ))}
+
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = friendsReducer => {
+  return {
+    fetching: friendsReducer.fetching,
+    friends: friendsReducer.friends
+  };
+};
+//actions go here
+export default connect(mapStateToProps, { fetchFriends, addFriends })(App);
