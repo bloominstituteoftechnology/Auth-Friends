2c98cde3ceb55e2cf3aee1f03ca0bb3db285f3ee
David Pok <david.pok@gmail.com>
2018-04-19 13:11:06 -0700

    created inistialState and friendsReducer in index.js

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -4,16 +4,27 @@ import {
 	ERROR
 } from "../actions/fetchFriends";
 
-const initialState = {};
+const initialState = {
+	fetching: false,
+	error: null,
+	friends: []
+};
 
 export const friendsReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case "":
-			return;
-		case "":
-			return;
-		case "":
-			return;
+		case FETCHING_FRIENDS:
+			return Object.assign({}, state, { fetching: true });
+		case FETCHED_FRIENDS:
+			return Object.assign({} , state, {
+				fetching: false,
+				error: null,
+				friends: [...action.payload]
+			});
+		case ERROR:
+			return Object.assign({} , state, {
+				pending: false,
+				error: action.payload
+			});
 		default:
 			return state;
 	}
