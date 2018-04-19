49cc033b9c426ce752010d86a77095f7552f36b6
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-04-19 16:05:36 -0700

    Added deleteFriend

---
+++ friends/src/actions/actions.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+export const GETTING_FRIENDS = 'GETTING_FRIENDS';
+export const FRIENDS_RECEIVED = 'FRIENDS_RECEIVED';
+export const ERROR_GETTING_FRIENDS = 'ERROR_GETTING_FRIENDS';
+
+export const CREATE_FRIEND = 'CREATE_FRIEND';
+export const FRIEND_CREATED = 'FRIEND_CREATED';
+export const ERROR_CREATING_FRIEND = 'ERROR_CREATING_FRIEND';
+
+export const DELETE_FRIEND = 'DELETE_FRIEND';
+export const FRIEND_DELETED = 'FRIEND_DELETED';
+export const ERROR_DELETING_FRIEND = 'ERROR_DELETING_FRIEND';
+
+export const UPDATE_FRIEND = 'UPDATE_FRIEND';
+export const FRIEND_UPDATED = 'FRIEND_UPDATED';
+export const ERROR_UPDATING_FRIEND = 'ERROR_UPDATING_FRIEND';
+
+export const getFriends = () => {
+    return dispatch => {
+        dispatch({ type: GETTING_FRIENDS });
+        axios
+            .get('http://localhost:5000/api/friends/get')
+            .then(({ data }) => dispatch({ type: FRIENDS_RECEIVED, payload: data }))
+            .catch(error => {
+                dispatch({ type: ERROR_GETTING_FRIENDS, payload: error });
+            });
+        
+    };
+};
+
+export const createFriend = data => {
+    return dispatch => {
+        dispatch({ type: CREATE_FRIEND });
+        axios
+            .post('http://localhost:5000/api/friends/create', data)
+            .then(({ data }) => {
+                dispatch({ type: FRIEND_CREATED, payload: data });
+            })
+            .catch(error => {
+                dispatch({ type: ERROR_CREATING_FRIEND, payload: error });
+            });
+    };
+};
+
+export const deleteFriend = () => {
+    return dispatch => {
+        dispatch({ type: DELETE_FRIEND });
+        axios
+            .delete('http://localhost:5000/api/friends/delete')
+            .then(({ data }) => dispatch({ type: FRIEND_DELETED, payload: data }))
+            .catch(error => {
+                dispatch({ type: ERROR_DELETING_FRIEND, payload: error });
+            });
+    };
+};
+
+export const
