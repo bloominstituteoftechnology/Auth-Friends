263827ddba1e921bb5c9fb547b95cdeb30dbd622
totsuya <supertatortots@gmail.com>
2018-04-19 16:00:54 -0400

    added state tree  in friendsReducer

--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -1,7 +1,19 @@
 import { /* Actions */} from './actions/friendsActions.js';
 
-const initialState = { /* Set state here */ };
+const initialState = {
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
+};
 
 export default ( state = initialState, action ) => {
   // Set up switch here
+  
 }
