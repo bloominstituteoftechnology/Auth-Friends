c8be4e094d391c4f5a9dbaa06ac61d300ac93361
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-19 12:40:24 -0400

    built the main App index

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root'));
 registerServiceWorker();
