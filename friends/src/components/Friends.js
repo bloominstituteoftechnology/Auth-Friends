0bff979c3a3861abcd17a46b5182e6232e206ac8
Justin Hong <jhong2009@gmail.com>
2018-04-19 15:10:53 -0700

    in Friends.js: added keys

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 const Friends = props => {
-    // console.log("FRIEND PROPS", props);
-    return (
-        <div>
-            {props.friendsList.map(friend => {
-                return [
-                    <div>{friend.name}</div>,
-                    <div>{friend.age}</div>,
-                    <div>{friend.email}</div>
-                ];
-            })}
-        </div>
-    );
+  console.log("FRIEND PROPS", props);
+  return (
+    <div>
+      {props.friendsList.map(friend => {
+        return [
+          <div key={friend.name}>{friend.name}</div>,
+          <div key={friend.id}>{friend.age}</div>,
+          <div key={friend.id}>{friend.email}</div>
+        ];
+      })}
+    </div>
+  );
 };
 
 export default Friends;
