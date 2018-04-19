87c50d94b8c18f3b3ae7dad6aab68b48e763e0b7
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-04-19 12:55:28 -0700

    Setup folders, app setup for redux/thunk/logger done  & reducers index.js created

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//create store & connect react components
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+//hook up store using Provider tag
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, 
+document.getElementById('root')
+);
