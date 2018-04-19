468f160a9123ee570d091ae34ec103997ed4c6d4
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-04-19 16:19:33 -0700

    Add input fields for Name, Age & Email

--- friends/src/components/CreateFriendForm.js
+++ friends/src/components/CreateFriendForm.js
@@ -12,5 +12,42 @@ class CreateFriendForm extends Component {
         this.setState ({ [e.target.name]: e.target.value });
     };
 
-    
+    submitFriend = e => {
+        e.preventDefault();
+        this.props.createFriend({
+            name: this.state.name,
+            age: this.state.age,
+            email: this.state.email,
+        });
+        this.setState({
+            name: '',
+            age: '',
+            email: '',
+        });
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.submitFriend}>
+                <input
+                    onChange={this.handleInput}
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                />
+                <input
+                    onChange={this.handleInput}
+                    type="number"
+                    name="age"
+                    placeholder="age"
+                />
+                <input
+                    onChange={this.handleInput}
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                />
+                <button type="submit">Add/Update Friend</button>
+        )
+    }
 }
