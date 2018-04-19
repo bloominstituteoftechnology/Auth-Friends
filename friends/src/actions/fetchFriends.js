3112f3a5f9e081d7d8b8ca822032dd0016ee5f4e
Justin Hong <jhong2009@gmail.com>
2018-04-19 12:57:32 -0700

    in fetchFriends.js: exported action types. in reducers/index.js: import action types, set up initial state, and set up friendsReducer()

--- friends/src/actions/fetchFriends.js
+++ friends/src/actions/fetchFriends.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 // action types
-const FETCHED_FRIENDS = "FETCHED_FRIENDS";
-const FETCHING_FRIENDS = "FETCHING_FRIENDS";
-const CREATE_FRIENDS = "CREATE_FRIENDS";
-const ERROR = "ERROR";
+export const FETCHED_FRIENDS = "FETCHED_FRIENDS";
+export const FETCHING_FRIENDS = "FETCHING_FRIENDS";
+export const ERROR = "ERROR";
 
 export const fetchFriends = () => {
 	const friends = axios.get("http://localhost:5000/api/friends");
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,20 @@
+import {
+	FETCHED_FRIENDS,
+	FETCHING_FRIENDS,
+	ERROR
+} from "../actions/fetchFriends";
+
+const initialState = {};
+
+export const friendsReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "":
+			return;
+		case "":
+			return;
+		case "":
+			return;
+		default:
+			return state;
+	}
+};
