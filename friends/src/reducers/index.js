171677654a6af29a8f1e2bd9864f35fbda11ade6
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-19 13:13:12 -0700

    made the reducer function for the action creators

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,13 +1,26 @@
+import { PENDING_FRIENDS, SUCCESS_FRIENDS, ERROR_FRIENDS } from '../actions';
 
 const initialState = {
-    postingFriend: false,
-    fetchingFriends: false,
+    pending: false,
     error: null,
     friends: [],
 };
 
 const friendsReducer = (state = initialState, action) => {
-    return state;
+    switch(action.type) {
+       case PENDING_FRIENDS:
+        return Object.assign({}, state, { pending: true });
+       case SUCCESS_FRIENDS:
+        return Object.assign({}, state,{ 
+            pending: false, 
+            friends: [...state.friends, ...action.payload] });
+       case ERROR_FRIENDS:
+        return Object.assign({}, state, { 
+            pending: false, 
+            error: action.payload });
+       default:
+        return state;
+    }
 };
 
 export default friendsReducer;
