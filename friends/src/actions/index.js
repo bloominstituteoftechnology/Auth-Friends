141811cbeac47e2b57db4666323e2323d0669945
Natalie Schennum <natalieschennum@gmail.com>
2018-04-19 13:41:46 -0700

    Working on Actions and Reducers

---
+++ friends/src/actions/index.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+export const PENDING_FRIENDS = 'PENDING_FRIENDS';
+export const SUCCESS_FRIENDS = 'SUCCESS_FRIENDS';
+export const ERROR_FRIENDS = 'ERROR_FRIENDS';
+
+export const fetchFriends = () => {
+    const friends = axios.get('http://localhost:5000/api/friends');
+    return dispatch => {
+      dispatch({ type: PENDING_FRIENDS });
+      friends
+        .then(response => {
+          dispatch({
+            type: SUCCESS_FRIENDS,
+            payload: response.data
+          });
+        })
+        .catch(err => {
+          dispatch({
+            type: ERROR_FRIENDS,
+            payload: 'ERROR Fetching Friends'
+          });
+        });
+    };
+  };
+
+  export const createFriend = data => {
+    const friends = axios.post('http://localhost:5000/api/friends', data);
+    return dispatch => {
+        friends
+        .then(response => {
+          dispatch(fetchFriends());
+        })
+        .catch(err => {
+          dispatch({
+            type: ERROR_FRIENDS,
+            payload: 'ERROR Posting Friends'
+          });
+        });
+    };
+  };
