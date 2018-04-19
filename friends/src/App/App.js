5e11df865f700e2551d83a62aeff6f55d49ee0d8
axhon <axhonruiz@gmail.com>
2018-04-19 12:31:29 -1000

    moved app up 1 level and began adding redux

---
+++ friends/src/App/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+
+import Friends from "../components/Friends"
+import { fetchFriends } from "../actions"
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchFriends()
+  }
+  render() {
+    return (
+      <div className="App">
+        <Friends greet="hello" />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  friends: state.friends,
+  error: null,
+  fetchingFriends: false,
+  FriendsFetched: false
+})
+export default connect(mapStateToProps, { fetchFriends })(App)
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import logger from "redux-logger"
+
 import "./index.css"
-import App from "./components"
+import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+import rootReducer from "./reducers"
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
 registerServiceWorker()
