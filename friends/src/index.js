9b05f06a4e60c86caf1ccce43e6dd6ea714bbd4f
Paul Fang <paulfang@pauls-mbp.home>
2018-04-19 15:26:49 -0700

    setting up index.js

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-think';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>
+    , document.getElementById('root'));
