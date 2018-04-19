bfeda1c69ee60c8e9b1f8f9c9d1ec92c1c9c6293
Jared Cuffe <jcuffe@gmail.com>
2018-04-19 08:26:35 -0700

    Refactored actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+// action types
 const FETCH_START = 'fetch-start'
 const MULTI_FETCH_SUCCESS = 'multi-fetch-success'
 const FETCH_ERROR = 'fetch-error'
@@ -7,62 +8,67 @@ const SINGLE_FETCH_SUCCESS = 'single-fetch-success'
 const UPDATING_FRIEND = 'updating-friend'
 const UPDATED_FRIEND = 'updated-friend'
 
+// internal actions dispatchers
+const fetchStart = () => ({ type: FETCH_START })
+const multiFetchSuccess = (payload) => ({ type: MULTI_FETCH_SUCCESS, payload })
+const singleFetchSuccess = (payload) => ({ type: SINGLE_FETCH_SUCCESS, payload })
+const updatingFriend = () => ({ type: UPDATING_FRIEND })
+const updatedFriend = () => ({ type: UPDATED_FRIEND })
+const fetchError = (payload) => ({ type: FETCH_ERROR, payload })
+
+// exposed action dispatchers
 const fetchFriends = () => {
   return (dispatch) => {
-    dispatch({ type: FETCH_START })
+    dispatch(fetchStart())
     axios.get('http://localhost:5000/api/friends')
-      .then(({ data }) => dispatch({ type: MULTI_FETCH_SUCCESS, payload: data }))
-      .catch((error) => dispatch({ type: FETCH_ERROR, payload: error }))
+      .then(({ data }) => dispatch(multiFetchSuccess(data)))
+      .catch((error) => dispatch(fetchError(error)))
   }
 }
 
 const fetchFriend = (id) => {
   return (dispatch) => {
-    dispatch({ type: FETCH_START })
+    dispatch(fetchStart())
     axios.get(`http://localhost:5000/api/friends/${id}`)
       .then(({ data }) => {
-        dispatch({ type: SINGLE_FETCH_SUCCESS, payload: data })
-        dispatch({ type: UPDATING_FRIEND })
+        dispatch(singleFetchSuccess(data))
+        dispatch(updatingFriend())
       })
-      .catch((error) => dispatch({ type: FETCH_ERROR, payload: error }))
+      .catch((error) => dispatch(fetchError(error)))
   }
 }
 
 const createFriend = ({ name, age, email }) => {
   const friend = { name, age, email }
   return (dispatch) => {
-    dispatch({ type: FETCH_START })
+    dispatch(fetchStart())
     axios.post('http://localhost:5000/api/friends', friend)
-      .then(({ data }) => dispatch({ type: MULTI_FETCH_SUCCESS, payload: data }))
-      .catch((error) => dispatch({ type: FETCH_ERROR, payload: error }))
+      .then(({ data }) => dispatch(multiFetchSuccess(data)))
+      .catch((error) => dispatch(fetchError(error)))
   }
 }
 
 const updateFriend = (friend) => {
   return (dispatch) => {
-    dispatch({ type: FETCH_START })
+    dispatch(fetchStart())
     axios.put(`http://localhost:5000/api/friends/${friend.id}`, friend)
       .then(({ data }) => {
-        dispatch({ type: MULTI_FETCH_SUCCESS, payload: data })
-        dispatch({ type: UPDATED_FRIEND })
+        dispatch(multiFetchSuccess(data))
+        dispatch(updatedFriend())
       })
-      .catch((error) => dispatch({ type: FETCH_ERROR, payload: error }))
+      .catch((error) => dispatch(fetchError(error)))
   }
 }
 
 const deleteFriend = (id) => {
   return (dispatch) => {
-    dispatch({ type: FETCH_START })
+    dispatch(fetchStart())
     axios.delete(`http://localhost:5000/api/friends/${id}`)
       .then(({ data }) => dispatch(multiFetchSuccess(data)))
       .catch((error) => dispatch(fetchError(error)))
   }
 }
 
-// internal actions dispatchers
-
-const multiFetchSuccess = (payload) => ({ type: MULTI_FETCH_SUCCESS, payload })
-const fetchError = (payload) => ({ type: FETCH_ERROR, payload })
 
 export {
   FETCH_START,
