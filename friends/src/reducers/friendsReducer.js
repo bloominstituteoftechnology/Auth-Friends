fdf35b356f62132b5fddb3c6d9f8034ca70b48c5
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-04-19 15:15:47 -0600

    added cases

--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -1,4 +1,4 @@
-import { /* Actions */} from './actions/friendsActions.js';
+import { FETCHING_FRIENDS,FETCHING_FRIENDS_SUCCESS, FETCHING_FRIENDS_ERROR } from './actions/friendsActions.js';
 
 const initialState = {
   fetchingFriends: false,
@@ -13,7 +13,23 @@ const initialState = {
   error: null
 };
 
-export default ( state = initialState, action ) => {
+export const friendsReducer = ( state = initialState, action ) => {
   // Set up switch here
-  
-}
+  switch ( action.type ) {
+    case FETCHING_FRIENDS: 
+    return Object.assign({}, state, { fetchingFriends: true });
+    case FETCHING_FRIENDS_SUCCESS:
+    return Object.assign({}, state, { 
+      fetchingFriends: false, 
+      error: null,
+      friends: [...action.payload]
+    } );
+    case FETCHING_FRIENDS_ERROR:
+    return Object.assign({}, state, {
+      fetchingFriends: false,
+      error: action.payload
+    });
+    default: 
+    return state;
+  }
+};
