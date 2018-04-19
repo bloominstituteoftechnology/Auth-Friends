938734d78f7c5a39c92d8d40694d3483cf30fa0c
Justin Hong <jhong2009@gmail.com>
2018-04-19 12:27:39 -0700

    in App.js: added some comments; changed some file names

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -13,9 +13,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* Render CreateFriendForm here - fire action from App*/}
+        {/* Render Friends here - send list of friends as props */}
       </div>
     );
   }
 }
 
 export default App;
+
+// mapStateToProps here and connect App with Redux store
