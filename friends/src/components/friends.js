90003748be9e37729e001547a126a749173e3904
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-04-19 14:39:23 -0700

    Added class/componenet & ampStateToProps

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -6,4 +6,32 @@ class Friends extends Component {
     componentDidMount() {
         this.props.getFriends();
     }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.props.friends.map(friend, i) => {
+                        return (
+                            <li key={friend.i}>
+                                <p>{friend.name}</p>
+                                <p>{friend.age}</p>
+                                <p>{friend.email}</p>
+                            </li>
+                        );
+                    }}}
+                </ul>
+            </div>
+        );
+    }
 }
+
+const mapStateToProps = state => {
+    return {
+        friends: state.friends,
+        fetching: state.fetching,
+        error: state.error,
+    };
+};
+
+export default connect(mapStateToProps, { getFriends })(Friends);
