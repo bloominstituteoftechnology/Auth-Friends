0e68382577ebb35cf24d817dd927413d39cd6e72
totsuya <supertatortots@gmail.com>
2018-04-19 16:09:24 -0400

    added export actions

--- friends/src/actions/friendsActions.js
+++ friends/src/actions/friendsActions.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export const ;
-
+export const FETCHING_FRIENDS;
+export const FETCHING_FRIENDS_SUCCESS;
+export const FETCHING_FRIENDS_ERROR;
 export const fetchFriends = () => {
   // Set up actions here
+  
 }
