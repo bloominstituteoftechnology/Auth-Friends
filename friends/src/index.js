8bee0ad324a07fce528e809704e73be7ae9ca1c7
Nedu Robert <nedumrobert@gmail.com>
2018-04-20 01:22:38 +0100

    imported bootstap into the app

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 import './index.css';
 import App from './containers/App';
