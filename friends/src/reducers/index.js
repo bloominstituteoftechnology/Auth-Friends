0ad210ac4c7259c22e17826821af5a0378929814
Nedu Robert <nedumrobert@gmail.com>
2018-04-20 01:26:47 +0100

    implemented combine reducers in the reducers root file

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,34 +1,6 @@
-import * as AT from '../actions/actionTypes';
+import { combineReducers } from 'redux';
+import friendsReducer from './friendsReducer.js';
 
-const initialState = {
-  friends: [],
-  pending: false,
-//   updatingFriend: false,
-//   friendUpdated: false,
-//   deletingFriend: false,
-//   friendDeleted: false,
-  error: null,
-};
-
-const friendsReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case AT.PENDING_FRIENDS:
-            return Object.assign({}, state, { 
-                pending: true,
-             });
-        case AT.SUCCESS_FRIENDS:
-            return Object.assign({}, state, { 
-                pending: false,
-                friends: [...state.friends, ...action.payload],
-            })
-        case AT.ERROR_FRIENDS:
-            return Object.assign({}, state, { 
-                pending: false,
-                error: action.payload
-            })
-        default:
-            return state;
-    }
-}
-
-export default friendsReducer;
+export default combineReducers({
+  friendsReducer,
+});
