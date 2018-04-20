b980057a2f3d84230e43465c8a95554ad88a1d08
Justin Hong <jhong2009@gmail.com>
2018-04-19 23:28:18 -0700

    in UpdateFriendForm.js: modified handler to call updateFriend() a friend's new data and an id

--- friends/src/components/UpdateFriendForm.js
+++ friends/src/components/UpdateFriendForm.js
@@ -11,18 +11,50 @@ class UpdateFriendForm extends React.Component {
 			email: ""
 		};
 	}
-
+	handleNewInput = e => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+	handleUpdateFriend = () => {
+		const friend = { id: this.props.friend };
+		if (this.state.name !== "") {
+			friend.name = this.state.name;
+		}
+		if (this.state.age !== "") {
+			friend.age = this.state.age;
+		}
+		if (this.state.email !== "") {
+			friend.email = this.state.email;
+		}
+		this.props.updateFriends(friend);
+		this.setState({ name: "", age: "", email: "" });
+	};
 	render() {
-		console.log("Props: ", this.props);
+		// console.log("Props: ", this.props);
+		// console.log(this.state.name);
 		return (
 			<form>
-				<input type="text" name="name" placeholder="Enter friend's name" />
-				<input type="text" name="age" placeholder="Enter friend's age" />
-				<input type="text" name="email" placeholder="Enter friend's email" />
-				<button
-					type="button"
-					onClick={() => this.props.updateFriends(this.props.friend)}
-				>
+				<input
+					type="text"
+					name="name"
+					value={this.state.name}
+					onChange={this.handleNewInput}
+					placeholder="Enter friend's name"
+				/>
+				<input
+					type="text"
+					name="age"
+					value={this.state.age}
+					onChange={this.handleNewInput}
+					placeholder="Enter friend's age"
+				/>
+				<input
+					type="text"
+					name="email"
+					value={this.state.email}
+					onChange={this.handleNewInput}
+					placeholder="Enter friend's email"
+				/>
+				<button type="button" onClick={this.handleUpdateFriend}>
 					Update Friend
 				</button>
 			</form>
