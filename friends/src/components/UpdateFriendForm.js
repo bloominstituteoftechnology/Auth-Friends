50847976783fb5ce8e461046fef7b000b1c56474
Justin Hong <jhong2009@gmail.com>
2018-04-19 22:54:43 -0700

    in UpdateFriendForm.js: converted component to a class component

--- friends/src/components/UpdateFriendForm.js
+++ friends/src/components/UpdateFriendForm.js
@@ -2,19 +2,33 @@ import React from "react";
 import { connect } from "react-redux";
 import { updateFriends } from "../actions/fetchFriends";
 
-const UpdateFriendForm = props => {
-	console.log("Props: ", props);
-	return (
-		<form>
-			<input type="text" name="name" placeholder="Enter friend's name" />
-			<input type="text" name="age" placeholder="Enter friend's age" />
-			<input type="text" name="email" placeholder="Enter friend's email" />
-			<button type="button" onClick={() => props.updateFriends(props.friend)}>
-				Update Friend
-			</button>
-		</form>
-	);
-};
+class UpdateFriendForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: "",
+			age: "",
+			email: ""
+		};
+	}
+
+	render() {
+		console.log("Props: ", this.props);
+		return (
+			<form>
+				<input type="text" name="name" placeholder="Enter friend's name" />
+				<input type="text" name="age" placeholder="Enter friend's age" />
+				<input type="text" name="email" placeholder="Enter friend's email" />
+				<button
+					type="button"
+					onClick={() => this.props.updateFriends(this.props.friend)}
+				>
+					Update Friend
+				</button>
+			</form>
+		);
+	}
+}
 
 const mapStateToProps = state => {
 	// console.log("State: ", state);
