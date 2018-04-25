efb5de6e27735cc302668eb21f148300106fffcb
Bhavik Ravani <ravanibhavik@gmail.com>
2018-04-24 23:31:08 -0400

    Created react app and added boilerplate for src/index.js and reducers/index.js

---
+++ friends/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import './index.css';
+import App from './App';
+import { reducer } from './reducers';
+
+const store = createStore(reducer, applyMiddleware(logger, thunk))
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
