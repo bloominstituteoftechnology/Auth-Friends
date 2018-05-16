54c51a7c2ceace63ede51c50428ce2a30ce15bb8
jesuarva <jean.ariza@jesuarva.com>
2018-05-17 00:05:56 +0200

    SETUP folder structure

--- friends-redux-thunk/src/index.js
+++ friends-redux-thunk/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+ 
