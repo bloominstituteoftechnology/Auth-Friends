b9a034d17d7ea68b4009cb9cf24da2feceab5e4e
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-05-16 17:13:20 -0400

    Setup Redux store and connect to App - also adding thunk and logger

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import './index.css';
+
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// todo: bring in reducer
+const store = createStore(null, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
