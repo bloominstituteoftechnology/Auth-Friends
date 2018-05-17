4ce1ecebe660610058e44a0c624803cab92f4845
Niloc92 <prestonresult@github.com>
2018-05-17 17:46:30 -0500

    adjusted css

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'; //async js
