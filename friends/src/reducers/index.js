5da1b45a2e09e9a0b423c107407daeefab408f69
Zach Campbell <beazlebubbles@gmail.com>
2018-05-17 13:58:33 -0600

    Installed reactstrap Installed provider Created skeleton reducer

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,13 @@
+import { FETCHING, FETCHED, ERROR } from '../actions';
+const initialState = {
+
+};
+export const friendsReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
+
+export default charsReducer;
