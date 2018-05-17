21c3a857d4abd7eccf64f7f717d40ad033ec9424
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-17 17:58:55 -0500

    got my index back up and importing correctly

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { App } from './components';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// needed dependancies
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import rootReducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// needed dependancies
+// applyMiddleware from redux
+// thunk from redux-thunk
+// logger from redux-logger
+// rootReducer from ./reducers
+const store = createStore(rootReducer, applyMiddleware (thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
