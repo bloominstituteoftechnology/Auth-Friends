2d935c3ee384cca0a0fb826ec4ee03752354b46d
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-05-17 08:43:55 -0700

    actions created

---
+++ friends/src/actions/index.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export const PENDING_FRIENDS = "PENDING_FRIENDS";
+export const SUCCESS_FRIENDS = "SUCCESS_FRIENDS";
+export const ERROR_FRIENDS = "ERROR_FRIENDS";
+
+export const fecthFriends = () => {
+  const friends = axios.get("http://localhost:5000/api/friends");
+  return dispatch => {
+    dispatch({ type: PENDING_FRIENDS });
+    friends
+      .then(response => {
+        console.log(response);
+        dispatch({ type: SUCCESS_FRIENDS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({
+          type: ERROR_FRIENDS,
+          payload: "Something went wrong with fetching Friends"
+        });
+      });
+  };
+};
+
+export const createFriend = data => {
+  const friends = axios.post("http://localhost:5000/api/friends", data);
+  return dispatch => {
+    dispatch({ type: PENDING_FRIENDS });
+    friends
+      .then(response => {
+        console.log(response);
+        dispatch({ type: SUCCESS_FRIENDS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({
+          type: ERROR_FRIENDS,
+          payload: "Something went wrong with posting friend"
+        });
+      });
+  };
+};
