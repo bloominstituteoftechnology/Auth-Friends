8c85d6c9e36fc4dd9081b88275b420f9d413e95c
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-17 17:54:13 -0400

    -imported Middleware, thunk, logger, -declare store for root reducer -Wrapped App with Provider

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+import App from './App';
+
+const store = createStore( applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
+    registerServiceWorker();
