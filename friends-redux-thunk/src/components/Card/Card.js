79c94bd4430722fc200ab5ab009fef78259e0c61
jesuarva <jean.ariza@jesuarva.com>
2018-05-17 23:40:14 +0200

    ADD flipping card functionality.

--- friends-redux-thunk/src/components/Card/Card.js
+++ friends-redux-thunk/src/components/Card/Card.js
@@ -1,18 +1,41 @@
-import React from "react";
-import './Card.css';
+import React, { Component } from "react";
+import "./Card.css";
 
-const Card = props => {
-  const { friend } = props;
-  console.log("Card friend", friend);
-  const { name } = friend;
-  return (
-    <div class="scene scene--card">
-      <div class="card">
-        <div class="card__face card__face--front">front</div>
-        <div class="card__face card__face--back">back</div>
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false
+    };
+  }
+
+  toggleCard = () => {
+    console.log("toggleCard clicked");
+    this.setState({ toggle: !this.state.toggle });
+  };
+
+  render() {
+    const { name } = this.props.friend;
+    const { toggle } = this.state.toggle;
+    console.log("toggle", this.state.toggle);
+    return (
+      <div className="scene scene--card">
+        <div className={this.state.toggle ? "card is-flipped" : "card"}>
+          <div
+            onClick={this.toggleCard}
+            className="card__face card__face--front"
+          >
+            front-{name}
+          </div>
+          <div
+            onClick={this.toggleCard}
+            className="card__face card__face--back"
+          >
+            back-{name}
+          </div>
+        </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 export default Card;
