bca7f1fcf21e8b116e1a1f039e83f74c10571a86
Zach Campbell <beazlebubbles@gmail.com>
2018-05-17 14:51:19 -0600

    Filled in reducer

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,13 +1,21 @@
-import { FETCHING, FETCHED, ERROR } from '../actions';
+import { FETCHING_FRIENDS, FETCHED_FRIENDS, ERROR } from '../actions';
 const initialState = {
-
+  friends: [],
+  fetching: false,
+  fetched: false,
+  error: null
 };
 export const friendsReducer = (state = initialState, action) => {
   switch (action.type) {
-
+    case FETCHING_FRIENDS:
+      return Object.assign({}, state, {fetching: true});
+    case FETCHED_FRIENDS:
+      return Object.assign({}, state, {friends: action.payload, fetching: false, fetched: true});
+    case ERROR:
+      return Object.assign({}, state, {fetching: false, error: action.payload});
     default:
       return state;
   }
 };
 
-export default charsReducer;
+export default friendsReducer;
