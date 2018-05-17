041e028e5c23eeb2d7e7c40c6addb67803ec6b9c
Zach Campbell <beazlebubbles@gmail.com>
2018-05-17 14:52:01 -0600

    Connected app component to redux

--- friends/src/components/index.js
+++ friends/src/components/index.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import fetcher from '../actions';
+import { Route } from 'react-router-dom';
+import Friends from './friends.js';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.fetcher('http://localhost:5000/api/friends');
+  }
+
   render() {
     return (
       <div className="App">
-        PLACEHOLDER!
+
+        <Route path="/" render={() => (
+          <Friends friendData={this.props.friends} />
+        )} />
+
       </div>
     );
   }
 }
 
-export default App;
+
+const mapStateToProps = (state) => {
+  return {
+    friends: state.friends,
+    fetching: state.fetching,
+    fetched: state.fetched,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps, {
+  fetcher
+})(App);
