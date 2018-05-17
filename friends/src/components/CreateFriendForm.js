b0807ea0e34a7057c32dd7f12f7fe35e92b7b0d5
jaklundun <govi.escudero@gmail.com>
2018-05-17 14:02:27 -0700

    Added click handler

--- friends/src/components/CreateFriendForm.js
+++ friends/src/components/CreateFriendForm.js
@@ -17,11 +17,18 @@ export default class CreateFriendForm extends Component {
         this.setState({ [e.target.name]: e.target.value });
       };
 
-    
+
+    handleSubmitFriend = () => {
+        const newFriend = this.state
+
+        };
+       
+
+
     render () {
         return (
             <form>
-                <input onChange={ this.handleFriendName } placeholder='name' name='name' value={this.state.name}/>
+                <input onChange={this.handleFriendName } placeholder='name' name='name' value={this.state.name}/>
                 <input onChange={this.handleFriendName } placeholder='age' name='age' value={this.state.age}/>
                 <input onChange={this.handleFriendName } placeholder='email' name='email' value={this.state.email}/>
             </form>
