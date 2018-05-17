bc6d1578104693344b3146822328e83771843f68
masontmorrow <masontmorrow@gmail.com>
2018-05-17 23:44:18 +0200

    Added fetchingFriends in actions

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 
-export const ACTION = 'ACTION';
+export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
+export const FRIENDS_FETCHED = 'FRIENDS_FETCHED';
+export const ERROR = 'ERROR';
+
 
 export const fetchingFriends = () => {
-    const getData = axios.get('http://localhost:5000/api/friends');
+    const getData = axios.get(`http://localhost:5000/api/friends`);
+    return function(dispatch) {
+        dispatch({type: FETCHING_FRIENDS});
+        getData
+            .then(response => {
+                dispatch({type: FRIENDS_FETCHED, payload: response.data})
+            })
+            .catch(err => {
+                dispatch({type: ERROR, payload: err})
+            })
+    }
 }
