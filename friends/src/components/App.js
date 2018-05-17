6743369bf18af9d3ee97d32647078a714776df0d
udykas <udykas@gmail.com>
2018-05-17 18:28:51 -0400

    Set up index file in src and made component/action/reducer files and folders

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux';
+
 class App extends Component {
   render() {
     return (
@@ -18,4 +20,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, {  } )(App);
