38a5f84342404a1761773595e399679632e0c0d5
Zach Campbell <beazlebubbles@gmail.com>
2018-05-17 15:14:42 -0600

    Created and styled friends component

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Media } from 'reactstrap';
+import './friends.css';
 
 
 const Friends = (props) => {
   return(
     <div>
-    {props.friendData.map((friend) => {
-      return (
-        <div>{friend.name}</div>
-      );
-    })}
+      {props.friendData.map((friend) => {
+        return (
+          <div key={friend.id} className="mt-5 friend">
+            <Media>
+              <Media left>
+                <img src="https://picsum.photos/64/64/?random" alt="Random placeholder" />
+              </Media>
+              <Media body>
+                <Media heading>
+                  {friend.name}
+                </Media>
+                <p>age: {friend.age}</p>
+                <p>email: {friend.email}</p>
+              </Media>
+            </Media>
+          </div>
+        );
+      })}
     </div>
   );
 }
