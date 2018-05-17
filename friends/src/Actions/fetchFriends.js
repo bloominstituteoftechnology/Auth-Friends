4a64d61b7f2ef8e92e8c5747721d3c6cc696a205
Niloc92 <prestonresult@github.com>
2018-05-17 17:11:55 -0500

    Actions set

---
+++ friends/src/Actions/fetchFriends.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import * as ActionTypes from './actionTypes';
+
+export const fetchFriends = () => {
+  const friends = axios.get('http://localhost:5000/api/friends');
+  return dispatch => {
+    dispatch({ type: ActionTypes.PENDING_FRIENDS });
+    friends
+      .then(response => {
+        dispatch({
+          type: ActionTypes.SUCCESS_FRIENDS,
+          payload: response.data
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: ActionTypes.ERROR_FRIENDS,
+          payload: 'Error fetching friends...'
+        });
+      });
+  };
+};
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/app';
 import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'; //async js
+import logger from 'redux-logger'; // makes console.log look good
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// import friendsReducer from './Reducers'
+
+const store = createStore(friendsReducer, applyMiddleware(thunk,logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
