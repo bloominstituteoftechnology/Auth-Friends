cfe68116af2b477aa43b6b7f56ce6cdb3627a2df
masontmorrow <masontmorrow@gmail.com>
2018-05-18 02:49:54 +0200

    Reducer updated with Update & Delete

--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -1,4 +1,4 @@
-import { FETCHING_FRIENDS, FRIENDS_FETCHED, SAVING_FRIEND, FRIEND_SAVED, ERROR } from '../actions';
+import { FETCHING_FRIENDS, FRIENDS_FETCHED, SAVING_FRIEND, FRIEND_SAVED, UPDATING_FRIEND, FRIEND_UPDATED, DELETING_FRIEND, FRIEND_DELETED, ERROR } from '../actions';
 
 const initialState = {
         fetchingFriends: false,
@@ -19,10 +19,22 @@ export const friendsReducer = (state = initialState, action) => {
           return {...state, fetchingFriends: true};
         case FRIENDS_FETCHED:
           return {...state, fetchingFriends: false, friendsFetched: true, friends: state.friends.concat(action.payload)};
+
         case SAVING_FRIEND:
           return {...state, savingFriend: true};
         case FRIEND_SAVED:
           return {...state, savingFriend: false, friendSaved: true, friends: state.friends.concat(action.payload)};
+
+        case UPDATING_FRIEND:
+          return {...state, updatingFriend: true};
+        case FRIEND_UPDATED:
+          return {...state, updatingFriend: false, friendUpdated: true, friends: state.friends.concat(action.payload)};
+
+        case DELETING_FRIEND:
+          return {...state, deletingFriend: true};
+        case FRIEND_DELETED:
+          return {...state, deletingFriend: false, friendDeleted: true, friends: state.friends.concat(action.payload)};
+
         case ERROR:
           return {...state, error: action.payload}; 
         default:
