4bdc0b4c8cd6822db8d573df3e3384013b529762
mister-corn <code@ronaldlibago.com>
2018-05-17 19:43:50 -0700

    Completion Commit
    * Deleted App.text.js
    * Refined "Notebook" styling
    * Changed input form to conform to new aesthetic.

--- friends/src/components/CreateFriendForm.js
+++ friends/src/components/CreateFriendForm.js
@@ -12,6 +12,7 @@ class CreateFriendForm extends React.Component {
       age: '',
       email: '',
       likes: '',
+      edit: false,
     }
   }
 
@@ -21,76 +22,54 @@ class CreateFriendForm extends React.Component {
 
   goPostFriend = (e) => {
     e.preventDefault();
-    const newFriend = {
-      name: this.state.name,
-      age: this.state.age,
-      email: this.state.email,
-      likes: this.state.likes,
-    };
-    this.props.postFriend(newFriend);
-    this.setState({
-      name: '',
-      age: '',
-      email: '',
-      likes: '',
-    })
+
+    if (this.state.edit === false) this.setState({
+      ...this.state,
+      edit: true, 
+    });
+
+    if (this.state.edit === true) {
+      
+      const newFriend = {
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email,
+        likes: this.state.likes,
+      };
+      this.props.postFriend(newFriend);
+      this.setState({
+        name: '',
+        age: '',
+        email: '',
+        likes: '',
+        edit: false,
+      })
+    }
   }
 
   render() {
+    const { name, id, age, email, likes } = this.props;
+    const inputField = property => <input 
+      name={property}
+      type="text"
+      value={this.state[property]}
+      size={this.state[property].toString().length - 1}
+      onChange={(e) => this.handleTextChange(e)}
+    />;
     return (
-      <main className="pa4 black-80">
-        <form onSubmit={(e) => this.goPostFriend(e)} className="measure center">
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f4 fw6 ph0 mh0">Who's your new friend?</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-dark-gray hover-white w-100" 
-                type="text" 
-                name="name"  
-                id="name"
-                value={this.state.name}
-                onChange={(e) => this.handleTextChange(e)} 
-              />
-            </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="email">E-mail</label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-dark-gray hover-white w-100" 
-                type="text" 
-                name="email"  
-                id="email"
-                value={this.state.email}
-                onChange={(e) => this.handleTextChange(e)} 
-              />
-            </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="age">Age</label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-dark-gray hover-white w-100" 
-                type="text" 
-                name="age"  
-                id="age"
-                value={this.state.age}
-                onChange={(e) => this.handleTextChange(e)}  
-              />
-            </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="likes">What does your friend like?</label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-dark-gray hover-white w-100" 
-                type="text" 
-                name="likes"  
-                id="likes"
-                value={this.state.age}
-                onChange={(e) => this.handleTextChange(e)}  
-              />
-            </div>
-          </fieldset>
-          <div className="">
-            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Add to the List" />
-          </div>
-          {/* <div className="lh-copy mt3">
-            <a href="#0" className="f6 link dim black db">Go back</a>
-          </div> */}
-        </form>
-      </main>
+      <form onSubmit={this.goPostFriend} className="friend flex justify-between ph2 pt2 ma3">
+        { this.state.edit ? 
+          <span>I'm {inputField(`name`)} 
+            and I'm {inputField(`age`)} 
+            years old. I like{` `} 
+            {inputField(`likes`)}
+          </span> :
+          `Wanna add a new friend? Click on the pencil.`
+        }
+        <div className="button-div">
+          <i type="submit" onClick={this.goPostFriend} className="fas fa-pencil-alt mh2"></i>
+        </div>
+      </form>
     );
   }
 }
