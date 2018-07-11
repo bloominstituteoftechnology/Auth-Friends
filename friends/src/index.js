e7b3d7d08dc5fcb7ce9bb0fc7be2c6cf49937349
cmok4290 <cmok4290@gmail.com>
2018-07-11 19:49:01 -0400

    added dependencies

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, logger)
+);
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
