e495355521280568a603db90052d6917da435585
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-11 18:23:25 -0400

    minor changes

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+      <h1>Hello</h1>
       </div>
     );
   }
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components";
+import {App} from "./components/App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
