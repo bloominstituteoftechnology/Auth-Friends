0ad6300ec3a7799571878038bcde407520664b9e
DirupT <iamcooled@gmail.com>
2018-07-11 10:38:41 -0500

    small tweak

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
 export const FRIENDS_FETCHED = 'FRIENDS_FETCHED';
-export const ERROR = 'ERROR';
 
 export const SAVING_FRIENDS = 'SAVING_FRIENDS';
 export const FRIENDS_SAVED = 'FRIENDS_SAVED';
@@ -12,6 +11,8 @@ export const FRIEND_UPDATED = 'FRIEND_UPDATED';
 
 export const DELETING_FRIEND = 'DELETING_FRIEND';
 export const FRIEND_DELETED = 'FRIEND_DELETED';
+
+export const ERROR = 'ERROR';
 
 export const getFriends = () => {
     const request = axios.get('http://localhost:5000/api/friends/');
