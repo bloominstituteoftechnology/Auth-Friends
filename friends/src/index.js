fb7d13dd1a37f829d49af26c52b98b2b76730f1e
Wdifulvio523 <w.difulvio523@gmail.com>
2018-07-11 18:00:55 -0400

    William DiFulvio [Redux-Friends]
    Initial PR commit. Created react app and installed needed dependencies. Imported needed dependencies into index.js

---
+++ friends/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import Thunk from "redux-thunk";
+
+const store = createStore(reducer, applyMiddleware(Thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
