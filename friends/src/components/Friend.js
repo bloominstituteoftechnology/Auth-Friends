7fe92e4b3cbbe558e8686866ba8d012d992d345e
DirupT <iamcooled@gmail.com>
2018-07-10 21:43:49 -0500

    update Friends component to pass down friends to Friend component

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Friend from './Friend';
 
 const Friends = props => {
     return (
         <div>
-            {props.friends.map(friend => <Friend key={friend.id} friend={friend} />)}
+            Friend
         </div>
     );
 }
