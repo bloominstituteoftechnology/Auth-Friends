8e8165d30932d0299d7417736ec52cdfdc241e4a
emaquino44 <prexy44@yahoo.com>
2018-07-12 14:10:42 -0700

    setup component files

---
+++ friends/src/components/CreateFriend.js
@@ -0,0 +1,1 @@
+CreateFriend.js
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
