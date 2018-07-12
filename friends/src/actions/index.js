924c8a56c3ed3f319f644886038cb98332a556d5
Kevin Tena <kevten09@gmail.com>
2018-07-12 17:56:49 -0500

    Added action creator to add friend

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 export const FETCHING = "FETCHING";
 export const FETCHED = "FETCHED";
 export const ERROR = "ERROR";
+export const SAVING_FRIEND = "SAVING_FRIEND";
+export const SAVE_FRIEND = "SAVE_FRIEND";
 
-
+const url = "http://localhost:5000/api/friends";
 export const fetchFriends = () => {
-    const friendData = axios.get("http://localhost:5000/api/friends");
+    const friendData = axios.get(url);
     return function(dispatch) {
         dispatch({ type: FETCHING });
         friendData.then(({data}) => {
@@ -18,3 +20,17 @@ export const fetchFriends = () => {
         })
     }
 }
+
+export const createFriend = friend => {
+    const newFriend = axios.post(url, friend);
+    return dispatch => {
+      dispatch({ type: SAVING_FRIEND });
+      newFriend
+        .then(({ data }) => {
+          dispatch({ type: SAVE_FRIEND, payload: data });
+        })
+        .catch(error => {
+          dispatch({ type: ERROR, payload: error });
+        });
+    };
+  };
