654f8cc66dc6051883325a79bd72a0be868cc649
Kevin <brackkevin@gmail.com>
2018-07-12 16:28:20 -0500

    added neccessary dependencies with yarn

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,12 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-import React from "react";
-import ReactDOM from "react-dom";
-import "index.css";
-import { App } from "./components";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 
@@ -17,3 +11,5 @@ import { applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducers";
+
+ReactDOM.render(<App />, document.getElementById("root"));
