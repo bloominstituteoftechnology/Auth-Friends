ad9e531c4166d1526abcee1b7371a90aef0f91fa
Kevin <brackkevin@gmail.com>
2018-07-12 16:25:40 -0500

    started imports in root index.js

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,3 +4,16 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.css";
+import { App } from "./components";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+// more dependencies
+
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./reducers";
