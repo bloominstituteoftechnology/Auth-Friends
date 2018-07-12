f8828638d8994a848548ad1cce588ca5ac95c991
Libby Hart <libbjo@gmail.com>
2018-07-11 18:45:34 -0600

    init

---
+++ friends/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, 
+document.getElementById('root'));
