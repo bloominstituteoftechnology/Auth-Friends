ef8e07685636495d613214e8865784a07554b994
Vu Cao <cdhvu@yahoo.com>
2018-07-12 12:01:00 -0700

    Created a table layout for friendlist

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -16,28 +16,50 @@ class App extends Component {
         {
           this.props.fetchingFriends
             ? <img src={logo} />
-            : <ul style={{}}>
-                  {this.props.friends.map(friend => <li>{friend.name}</li>)}
-              </ul>
-      }
+            : <table>
+              <thead>
+                <tr>
+                  <th> Name </th>
+                  <th> Age </th>
+                  <th> Email </th>
+                  <th> Delete </th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.friends.map(friend => {
+                  return (
+                    <tr key={friend.id}>
+                      <td>{friend.name}</td>
+                      <td>{friend.age}</td>
+                      <td>{friend.email}</td>
+                      <td className="delete"><button>X</button></td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          // <ul style={{}}>
+          //   {this.props.friends.map(friend => <li>{friend.name}</li>)}
+          // </ul>
+        }
       </div>
     );
-      }
-    }
-    
+  }
+}
+
 const mapStateToProps = state => {
   return {
-          fetchingFriends: state.fetchingFriends,
-        friendsFetched: state.friendsFetched,
-        friendsSaved: state.friendsSaved,
-        savingFriends: state.savingFriends,
-        updatingFriend: state.updatingFriend,
-        friendUpdated: state.friendUpdated,
-        deletingFriend: state.deletingFriend,
-        friendDeleted: state.friendDeleted,
-        friends: state.friends,
-        error: state.error
-      }
-    }
-    
-export default connect(mapStateToProps, {fetchData})(App);
+    fetchingFriends: state.fetchingFriends,
+    friendsFetched: state.friendsFetched,
+    friendsSaved: state.friendsSaved,
+    savingFriends: state.savingFriends,
+    updatingFriend: state.updatingFriend,
+    friendUpdated: state.friendUpdated,
+    deletingFriend: state.deletingFriend,
+    friendDeleted: state.friendDeleted,
+    friends: state.friends,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps, { fetchData })(App);
