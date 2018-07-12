387cfa9bd6c656ad81d6604348158f7875cb9344
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-11 19:33:14 -0500

    Ahmed Belhadj - Redux Friends
    Constructed initial state tree.

---
+++ friends/src/reducers/friendReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
+};
