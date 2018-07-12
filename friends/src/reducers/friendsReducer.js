d908fe4f688300d3de06aca2e0c0b357a050ce47
Kevin <brackkevin@gmail.com>
2018-07-12 16:36:07 -0500

    created root reducer and friendsReducer, combined with combineReducers

---
+++ friends/src/reducers/friendsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  friends: [],
+  fetching: false,
+  fetched: false,
+  error: null
+};
+
+export const friendsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
