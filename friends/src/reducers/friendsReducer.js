433548b8aa2f6b0805affa7c29a6afe1f72fde21
Kevin <brackkevin@gmail.com>
2018-07-12 16:56:16 -0500

    built out friends reducer to handle initial get request

--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -1,3 +1,9 @@
+import {
+  FETCHING_FRIENDS,
+  FRIENDS_FETCHED,
+  ERROR_FETCHING_FRIENDS
+} from "../actions";
+
 const initialState = {
   friends: [],
   fetching: false,
@@ -7,6 +13,24 @@ const initialState = {
 
 export const friendsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING_FRIENDS:
+      return Object.assign({}, state, {
+        fetching: true,
+        fetched: false,
+        error: null
+      });
+    case FRIENDS_FETCHED:
+      return Object.assign({}, state, {
+        fetching: false,
+        fetched: true,
+        chars: action.payload
+      });
+    case ERROR_FETCHING_FRIENDS:
+      return Object.assign({}, state, {
+        fetching: false,
+        fetched: false,
+        error: action.payload
+      });
     default:
       return state;
   }
