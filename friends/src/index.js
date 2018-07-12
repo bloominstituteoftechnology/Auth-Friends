85b80682211915d26fd6f323a5ffaaff508200f8
Simon <Simon_Freelance@hotmail.com>
2018-07-12 16:40:26 -0400

    Removed logger and just integrated redux developer tools

--- friends/src/index.js
+++ friends/src/index.js
@@ -5,10 +5,11 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+     applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
