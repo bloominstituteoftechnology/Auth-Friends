ef5d04ef0ef18c09f988babab6751f216fe5a593
Kamry Bowman <kamry.bowman@gmail.com>
2018-07-12 13:19:25 -0600

    Refactor reducersr into separate modules

--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -1,38 +1,44 @@
 // import { combineReducers } from 'redux';
 
-// import { FETCH_START } from '../actions/actions'
-// import { FETCH_ERROR } from '../actions/actions'
-// import { FETCH_SUCCESS } from '../actions/actions'
+import { FETCH_START } from '../actions/actions'
+import { FETCH_ERROR } from '../actions/actions'
+import { FETCH_SUCCESS } from '../actions/actions'
 
-// const byId = (state = {}, action) => {
-//   switch (action.type) {
-//     case FETCH_SUCCESS: {
-//       const { payload: resultArr } = action.payload;
-//       const newState = {...state}
-//       resultArr.forEach((friend) => {
-//         newState[friend.id] = friend;
-//       });
-//       return newState;
-//     }
-//     default:
-//       return state;
-//   }
-// };
 
-// const allIds = (state = [], action) => {
-//   switch (action.type) {
-//     case FETCH_SUCCESS: {
-//       let { payload: resultArr } = action.payload;
-//       resultArrIds = resultArr.map(friend => friend.id);
-//       return [...state, ...resultArrIds];
-//     }
-//     default:
-//       return state;
-//   }
-// };
+const byId = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_SUCCESS: {
+      const { payload: resultArr } = action;
+      const newState = {...state}
+      resultArr.forEach((friend) => {
+        newState[friend.id] = friend;
+      });
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
 
-// const friendsReducer = combineReducers({ byId, allIds });
-// export default friendsReducer;
+const allIds = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_SUCCESS: {
+      const { payload: resultArr } = action;
+      const resultArrIds = resultArr.map(friend => friend.id);
+      return [...state, ...resultArrIds];
+    }
+    default:
+      return state;
+  }
+};
+
+const friendsReducer = (state, action) => {
+  if (state === undefined) {
+    return {allIds: allIds(state, action), byId: byId(state, action)};
+  }
+  return {allIds: allIds(state.allIds, action), byId: byId(state.byId, action)};
+}
+export default friendsReducer;
 
 export const getFriends = (state) => {
   const { byId: byIdState, allIds: allIdsState } = state.friends;
--- friends/src/reducers/reducerIndex.js
+++ friends/src/reducers/reducerIndex.js
@@ -1,58 +1,8 @@
 import { combineReducers } from 'redux';
-// import friends from './friendsReducer'
-// import fetchState from './fetchStateReducer'
-import { FETCH_START } from '../actions/actions';
-import { FETCH_ERROR } from '../actions/actions';
-import { FETCH_SUCCESS } from '../actions/actions';
+import friendsReducer from './friendsReducer'
+import fetchStateReducer from './fetchStateReducer'
 
-const fetchStateReducer =  (state = 'resting', action) => {
-  switch (action.type) {
-    case FETCH_START:
-      return 'fetching';
-    case FETCH_SUCCESS:
-      return 'resting';
-    case FETCH_ERROR:
-      return 'resting';
-    default:
-    return state;
-  }
-}
-
-const byId = (state = {}, action) => {
-  switch (action.type) {
-    case FETCH_SUCCESS: {
-      const { payload: resultArr } = action;
-      const newState = {...state}
-      resultArr.forEach((friend) => {
-        newState[friend.id] = friend;
-      });
-      return newState;
-    }
-    default:
-      return state;
-  }
-};
-
-const allIds = (state = [], action) => {
-  switch (action.type) {
-    case FETCH_SUCCESS: {
-      const { payload: resultArr } = action;
-      const resultArrIds = resultArr.map(friend => friend.id);
-      return [...state, ...resultArrIds];
-    }
-    default:
-      return state;
-  }
-};
-
-const friendReducer = (state, action) => {
-  if (state === undefined) {
-    return {allIds: allIds(state, action), byId: byId(state, action)};
-  }
-  return {allIds: allIds(state.allIds, action), byId: byId(state.byId, action)};
-}
-
-const reducer = combineReducers({friends: friendReducer, fetchState: fetchStateReducer});
+const reducer = combineReducers({friends: friendsReducer, fetchState: fetchStateReducer});
 export default reducer;
 
 
