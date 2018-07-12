de65fa783360e0a24250bde07c200a974b82e6a6
raymondgrc <raymond_grc@yahoo.com>
2018-07-12 16:37:00 -0500

    Add reducers for friend GET request, untested

---
+++ friends/src/reducers/friendReducer.js
@@ -0,0 +1,25 @@
+import {FETCHING_FRIENDS, FETCHED_FRIENDS, FETCHING_FRIENDS_ERROR, SAVING_FRIEND, SAVED_FRIEND SAVING_FRIEND_ERROR, UPDATING_FRIEND, UPDATED_FRIEND, DELETING_FRIEND, DELETED_FRIEND} from '../actions';
+const initialState ={
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
+};
+export const charsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_FRIENDS:
+      return {...state, fetching: true};
+    case FETCHED_FRIENDS:
+      return {...state, fetching: false, fetched: true, friends: action.payload};
+    case FETCHING_FRIENDS_ERROR:
+      return {...state, error: action.payload};
+    default:
+      return state;
+  }
+};
