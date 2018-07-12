66d7a40dbec898f5aecbced95b6e8840ac8e79cf
Vu Cao <cdhvu@yahoo.com>
2018-07-11 18:49:47 -0700

    Setup initial state, and wired reducer to App

---
+++ friends/src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { connect } from 'react-redux';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    fetchingFriends: state.fetchingFriends,
+    friendsFetched: state.friendsFetched,
+    friendsSaved: state.friendsSaved,
+    savingFriends: state.savingFriends,
+    updatingFriend: state.updatingFriend,
+    friendUpdated: state.friendUpdated,
+    deletingFriend: state.deletingFriend,
+    friendDeleted: state.friendDeleted,
+    friends: state.friends,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps)(App);
