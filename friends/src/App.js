4a2ac5c071cca8a11089724e4312b45f483fab90
snowcoding <vimalshah77@gmail.com>
2018-07-12 14:02:35 -0700

    Wire up connect, MSP in App.js Wire up  Provider, createStore,midware, thunk and logger in index.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      Hello
+        Hello
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = () => {
+  return {
+
+  }
+}
+export default connect(mapStateToProps, {})(App);
--- friends/src/index.js
+++ friends/src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(()=>{},applyMiddleware(reduxThunk, logger))
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
