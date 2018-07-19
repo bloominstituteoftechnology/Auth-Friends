c2ebcb4b501816f312db683b349c571bd18667e3
Katie Gorbell <katiegorbell@gmail.com>
2018-07-19 16:59:37 -0500

    building create friend form

--- friends/src/Components/CreateFriendForm.js
+++ friends/src/Components/CreateFriendForm.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
 class CreateFriendForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            age: '',
+            email: ''
+        }
+    }
+
+    updateInputState = e => {
+        this.setState({ [e.target.name]: e.target.value})
+    }
+
     render() {
         return (
             <div>
                 <form>
-                    <input type='text' placeholder='name' />
-                    <input type='number' placeholder='age' />
-                    <input type='text' placeholder='email' />
+                    <input 
+                        onChange={this.updateInputState}
+                        type='text'
+                        placeholder='name'
+                        name='name'
+                        value={this.state.name}
+                    />
+                    <input 
+                        onChange={this.updateInputState}
+                        type='number'
+                        placeholder='age'
+                        name='age'
+                        value={this.state.age}
+                    />
+                    <input 
+                        onChange={this.updateInputState}
+                        type='text'
+                        placeholder='email'
+                        name='email'
+                        value={this.state.email}
+                    />
                     <button>Add New Friend</button>
                 </form>
             </div>
