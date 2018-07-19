46db0b4d8fa1f8ddad77e1313c8b4f32efdcf9d1
Kairi <kairijb@gmail.com>
2018-07-19 16:52:51 -0500

    made reducers

--- friends/src/reducers/reducers.js
+++ friends/src/reducers/reducers.js
@@ -18,12 +18,124 @@ const initialState = {
     friendUpdated: false,
     deletingFriend: false,
     friendDeleted: false,
-    friends: [],
-    error: null
+    error: null,
+    friends: []
   };
 
   export const friendsReducer = (state = initialState, action) => {
       switch(action.type) {
-          
+          case FETCHING_FRIENDS:
+            return Object.assign({},state, {
+                fetchingFriends: true,
+                friendsFetched: false,
+                friendsSaved: false,
+                savingFriends: false,
+                updatingFriend: false,
+                friendUpdated: false,
+                deletingFriend: false,
+                friendDeleted: false,
+                error: null,
+            })
+            case FREINDS_FETCHED:
+            return Object.assign({},state, {
+                fetchingFriends: false,
+                friendsFetched: true,
+                friendsSaved: false,
+                savingFriends: false,
+                updatingFriend: false,
+                friendUpdated: false,
+                deletingFriend: false,
+                friendDeleted: false,
+                error: null,
+            })
+            case FRIENDS_SAVED:
+            return Object.assign({},state, {
+                fetchingFriends: false,
+                friendsFetched: false,
+                friendsSaved: true,
+                savingFriends: false,
+                updatingFriend: false,
+                friendUpdated: false,
+                deletingFriend: false,
+                friendDeleted: false,
+                error: null,
+                friends: action.payload
+            })
+            case FRIENDS_SAVING:
+            return Object.assign({},state, {
+                fetchingFriends: false,
+                friendsFetched: false,
+                friendsSaved: false,
+                savingFriends: true,
+                updatingFriend: false,
+                friendUpdated: false,
+                deletingFriend: false,
+                friendDeleted: false,
+                error: null,
+            })
+            case FRIENDS_UPDATING:
+            return Object.assign({},state, {
+                fetchingFriends: false,
+                friendsFetched: false,
+                friendsSaved: false,
+                savingFriends: false,
+                updatingFriend: true,
+                friendUpdated: false,
+                deletingFriend: false,
+                friendDeleted: false,
+                error: null,
+            })
+            case FRIENDS_UPDATED:
+            return Object.assign({},state, {
+                fetchingFriends: false,
+                friendsFetched: false,
+                friendsSaved: false,
+                savingFriends: false,
+                updatingFriend: false,
+                friendUpdated: true,
+                deletingFriend: false,
+                friendDeleted: false,
+                error: null,
+                friends: action.payload
+            })
+            case FRIENDS_DELETING:
+            return Object.assign({},state, {
+                fetchingFriends: false,
+                friendsFetched: false,
+                friendsSaved: false,
+                savingFriends: false,
+                updatingFriend: false,
+                friendUpdated: false,
+                deletingFriend: true,
+                friendDeleted: false,
+                error: null,
+            })
+            case FRIENDS_DELETED:
+            return Object.assign({},state, {
+                fetchingFriends: false,
+                friendsFetched: false,
+                friendsSaved: false,
+                savingFriends: false,
+                updatingFriend: false,
+                friendUpdated: false,
+                deletingFriend: false,
+                friendDeleted: true,
+                error: null,
+                friends: action.payload
+            })
+            case FRIENDS_ERROR:
+            return Object.assign({},state, {
+                fetchingFriends: false,
+                friendsFetched: false,
+                friendsSaved: false,
+                savingFriends: false,
+                updatingFriend: false,
+                friendUpdated: false,
+                deletingFriend: false,
+                friendDeleted: false,
+                error: action.payload,
+            })
+          default: 
+            return state;
       }
   }
