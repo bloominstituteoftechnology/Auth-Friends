b2aa91a7ac6a7c82dfb29a9407021e73a677b828
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-15 14:40:09 -0700

    add friends reducer

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddeware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { friendsReducer } from '../reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(friendsReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
 
