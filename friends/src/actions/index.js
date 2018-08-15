4f93894c43428c238f8461fde77aae3cfcc468be
mkerbleski <mkerbles@gmail.com>
2018-08-15 17:16:13 -0600

    Added timer to check that store/state is changing and updating.

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -2,6 +2,8 @@ export const FETCHING = 'FETCHING';
 
 export const getFriends = () => {
   return function(dispatch){
-    dispatch({type: FETCHING, payload: "fetching request"});
+    setTimeout(() => {
+      dispatch({type: FETCHING, payload: "fetching request"});
+    }, 1000)
   }
 }
