583423d9607e779b528620ecceb1a86f952a759e
Ashwin <aswinjeganath@gmail.com>
2018-08-16 02:42:49 +0530

    Add fetchFriends asycn action creator

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Typography from 'typography';
+import theme from 'typography-theme-judah';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { fetchFriends } from './actions';
+
+const typography = new Typography(theme);
+
+typography.injectStyles();
+
+fetchFriends()(() => {});
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
