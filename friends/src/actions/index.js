6b6fde5a786fe02a7cf00b9cb31cee519a2fad0f
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-08-15 17:15:48 -0400

    Initial action creators

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
 export const FRIENDS_FETCHED = 'FRIENDS_FETCHED';
 export const SAVING_FRIEND = 'SAVING_FRIEND';
@@ -7,3 +9,45 @@ export const FRIEND_UPDATED = 'FRIEND_UPDATED';
 export const DELETING_FRIEND = 'DELETING_FRIEND';
 export const FRIEND_DELETED = 'FRIEND_DELETED';
 export const ERROR = 'ERROR';
+
+const URL = 'http://localhost:5000/api/friends';
+
+export const fetchFriends = () => {
+  return function(dispatch) {
+    dispatch({ type: FETCHING_FRIENDS });
+
+    axios.get(URL)
+          .then( res => dispatch({ type: FRIENDS_FETCHED, payload: res.data }))
+          .catch( err => dispatch({ type: ERROR, payload: err }));
+  }
+};
+
+export const saveFriend = (id, friend) => {
+  return function(dispatch) {
+    dispatch({ type: SAVING_FRIEND });
+
+    axios.post(URL + id, friend)
+          .then( res => dispatch({ type: FRIEND_SAVED, payload: res.data }))
+          .catch( err => dispatch({ type: ERROR, payload: err }));
+  }
+};
+
+export const updateFriend = (id, friend) => {
+  return function(dispatch) {
+    dispatch({ type: UPDATING_FRIEND });
+
+    axios.put(URL + id, friend)
+          .then( res => dispatch({ type: FRIEND_UPDATED, payload: res.data }))
+          .catch( err => dispatch({ type: ERROR, payload: err }));
+  }
+};
+
+export const deleteFriend = id => {
+  return function(dispatch) {
+    dispatch({ type: DELETING_FRIEND });
+
+    axios.delete(URL + id)
+          .then(res => dispatch({ type: FRIEND_DELETED, payload: res.data }))
+          .catch( err => dispatch({ type: ERROR, payload: err }));
+  }
+}
