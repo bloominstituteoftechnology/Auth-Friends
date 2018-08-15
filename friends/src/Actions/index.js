5014d499f2099b71d9430533b1006c6bede67aa3
Trevor <trevorfehrman@gmail.com>
2018-08-15 14:14:09 -0700

    Intitial commit, learning about redux-actions

---
+++ friends/src/Actions/index.js
@@ -0,0 +1,42 @@
+import { createActions } from "redux-actions";
+import axios from 'axios';
+
+const URL = '/api/friends'
+
+export const {
+	fetchFriendsRequest,
+	fetchFriendsFailure,
+	saveFriendsRequest,
+	saveFriendsFailure,
+	updateFriendsRequest,
+	updateFriendsFailure,
+	deleteFriendsRequest,
+	deleteFriendsFailure,
+	fetchFriendsSuccess,
+	saveFriendsSuccess,
+	updateFriendsSuccess,
+	deleteFriendsSuccess
+} = createActions(
+	"FETCH_FRIENDS_REQUEST",
+	"FETCH_FRIENDS_FAILURE",
+	"SAVE_FRIENDS_REQUEST",
+	"SAVE_FRIENDS_FAILURE",
+	"UPDATE_FRIENDS_REQUEST",
+	"UPDATE_FRIENDS_FAILURE",
+	"DELETE_FRIENDS_REQUEST",
+	"DELETE_FRIENDS_FAILURE",
+	"FETCH_FRIENDS_SUCCESS",
+	"SAVE_FRIENDS_SUCCESS",
+	"UPDATE_FRIENDS_SUCCESS",
+	"DELETE_FRIENDS_SUCCESS"
+);
+
+export const fetchFriends = () => async dispatch => {
+    dispatch(fetchFriendsRequest());
+    try {
+        let response = await axios.get(URL);
+        console.log(response.data)
+    } catch(err) {
+        console.log(err);
+    }
+};
---
+++ friends/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Typography from 'typography';
+import theme from 'typography-theme-judah';
+
+import './index.css';
+import App from './App';
+
+import { fetchFriends } from './Actions';
+
+const typography = new Typography(theme);
+
+typography.injectStyles();
+
+fetchFriends()(() =>{});
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
