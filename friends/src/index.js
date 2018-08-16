76ade45bf3de88543ca53770dfa8681e0a2eb0da
Carey <careycbaldwin@gmail.com>
2018-08-16 12:53:42 -0400

    wrapped App in Provider, connected to store referencing rootReducer which will be a combined reducer

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware } from 'redux';
+//applyMiddleware allows us to extend the funcitonality of redux
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//create a rootReducer in reducers/index using combine reducers
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+<Provider store = {store}>
+<App />
+</Provider>,
+ document.getElementById('root'));
 registerServiceWorker();
