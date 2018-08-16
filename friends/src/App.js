6ec00fdff20da0b47d401e8ee956ec0fbcacc449
tulf <timzulf@gmail.com>
2018-08-16 19:53:15 -0400

    Jay Rock

--- friends/src/App.js
+++ friends/src/App.js
@@ -8,11 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">From Falling in love with doing exotic drugs and playing inside of clubs I'm never the one to cuff </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
