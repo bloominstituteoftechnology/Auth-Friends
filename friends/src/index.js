eaa573b2c65694febb793816b85deed1f68e0396
szincone <sawyerzincone@gmail.com>
2018-08-16 14:15:31 -0600

    imported needed dependencies to index

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, thunk)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = 5000;
 const app = express();
 
@@ -9,57 +9,57 @@ let nextId = 6;
 let friends = [
   {
     id: 1,
-    name: 'Ben',
+    name: "Ben",
     age: 30,
-    email: 'ben@lambdaschool.com',
+    email: "ben@lambdaschool.com"
   },
   {
     id: 2,
-    name: 'Austen',
+    name: "Austen",
     age: 45,
-    email: 'austen@lambdaschool.com',
+    email: "austen@lambdaschool.com"
   },
   {
     id: 3,
-    name: 'Ryan',
+    name: "Ryan",
     age: 15,
-    email: 'ryan@lambdaschool.com',
+    email: "ryan@lambdaschool.com"
   },
   {
     id: 4,
-    name: 'Sean',
+    name: "Sean",
     age: 35,
-    email: 'sean@lambdaschool.com',
+    email: "sean@lambdaschool.com"
   },
   {
     id: 5,
-    name: 'Michelle',
+    name: "Michelle",
     age: 67,
-    email: 'michelle@gmail.com',
-  },
+    email: "michelle@gmail.com"
+  }
 ];
 
 app.use(bodyParser.json());
 
 app.use(cors());
 
-app.get('/api/friends', (req, res) => {
+app.get("/api/friends", (req, res) => {
   setTimeout(() => {
     res.send(friends);
   }, 1000);
 });
 
-app.get('/api/friends/:id', (req, res) => {
+app.get("/api/friends/:id", (req, res) => {
   const friend = friends.find(f => f.id == req.params.id);
 
   if (friend) {
     res.status(200).json(friend);
   } else {
-    res.status(404).send({ msg: 'Friend not found' });
+    res.status(404).send({ msg: "Friend not found" });
   }
 });
 
-app.post('/api/friends', (req, res) => {
+app.post("/api/friends", (req, res) => {
   const friend = { id: getNextId(), ...req.body };
 
   friends = [...friends, friend];
@@ -67,7 +67,7 @@ app.post('/api/friends', (req, res) => {
   res.send(friends);
 });
 
-app.put('/api/friends/:id', (req, res) => {
+app.put("/api/friends/:id", (req, res) => {
   const { id } = req.params;
 
   const friendIndex = friends.findIndex(f => f.id == id);
@@ -78,15 +78,15 @@ app.put('/api/friends/:id', (req, res) => {
     friends = [
       ...friends.slice(0, friendIndex),
       friend,
-      ...friends.slice(friendIndex + 1),
+      ...friends.slice(friendIndex + 1)
     ];
     res.send(friends);
   } else {
-    res.status(404).send({ msg: 'Friend not found' });
+    res.status(404).send({ msg: "Friend not found" });
   }
 });
 
-app.delete('/api/friends/:id', (req, res) => {
+app.delete("/api/friends/:id", (req, res) => {
   const { id } = req.params;
 
   friends = friends.filter(f => f.id !== Number(id));
