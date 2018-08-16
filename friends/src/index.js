1dc5d6d30460d473f31ea99b7bfc5c7440963ee2
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-16 15:26:40 -0500

    setup index.js w/ Provider and made all imports

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { rootReducer } from './reducers';
+
+const store = createStore( rootReducer ,  applyMiddleware(thunk, logger) );
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
