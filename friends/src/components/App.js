b8271e033c8ffaba6ac0aa11d3dff9fb32227b04
codejoncode <jonathanjholloway@gmail.com>
2018-08-16 16:37:33 -0500

    Turned the lights on using connect from react-redux. The logger is showing all of the state changes and the friends array on the state has 5 friends. Still have not put them on the page.

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import { connect } from 'react-redux';
+//import {friendsReducer} from '../reducers/friendsReducer' don't think i need this. 
+import {getFriends, postFriends, updateFriends, deleteFriends} from '../actions';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getFriends()
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,4 +25,16 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  //console.log(state); 
+  return {
+    fetchingFriends: state.friendsReducer.fetchingFriends,
+    postingFriends:  state.friendsReducer.postingFriends,
+    updatingFriends: state.friendsReducer.updatingFriends,
+    deletingFriends: state.friendsReducer.deletingFriends,
+    error: state.friendsReducer.error,
+    friends: state.friendsReducer.friends,
+  }
+}
+
+export default connect(mapStateToProps, {getFriends, postFriends, updateFriends, deleteFriends})(App)
