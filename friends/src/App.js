137db8b3fb970f5aaa2a05538e09279fedaf0b9f
Sean <sean47900@gmail.com>
2018-08-16 22:46:15 -0500

    Friend form is now added, no functionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,15 +4,31 @@ import './App.css';
 import Friends from './friends';
 import { connect } from 'react-redux';
 import { fetchFriends } from './actions/actions';
-
+import FriendForm from './FriendForm'
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      age: 0,
+      email: ''
+    }
+  }
   componentDidMount(){
     this.props.fetchFriends();
+  }
+  addFriend = event => {
+    event.preventDefault();
+    console.log('submit');
+  }
+   handleChange = event => {
+    this.setState({[event.target.name]: event.target.value});
   }
   render() {
     return (
       <div className="App">
      <Friends friends={this.props.friends} />
+     <FriendForm handleSubmit={this.addFriend} handleChange={this.handleChange} />
       </div>
     );
   }
