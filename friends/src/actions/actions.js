ea97cec9fa20e0afe080de78ad3d7bcd73452bb4
Sean <sean47900@gmail.com>
2018-08-16 21:02:43 -0500

    added the actions and reducer

---
+++ friends/src/actions/actions.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+export const FETCHING_FRIENDS = 'FETCHING_FREINDS';
+export const FETCHED_FRIENDS = 'FETCHED_FRIENDS';
+export const SAVED_FRIENDS = 'SAVED_FRIENDS';
+export const DELETING_FRIENDS = 'DELETING_FRIENDS';
+export const DELETED_FRIENDS = 'DELETED_FRIENDS';
+export const SAVING_FRIENDS = 'SAVING_FRIENDS';
+export const UPDATING_FRIENDS = 'UPDATING_FRIENDS';
+export const UPDATED_FRIENDS = 'UPDATED_FRIENDS';
+export const ERROR = 'ERROR';
+
+export const getFriends = () => {
+    const promise = axios.get('http://localhost:5000/');
+    return function(dispatch){
+        dispatch({type:FETCHING_FRIENDS});
+        promise
+            .then(({data}) => {
+                dispatch({type: FETCHED_FRIENDS, payload: data});
+            })
+            .catch(err=>{
+                dispatch({type: ERROR, payload: err});
+            })
+    }
+}
---
+++ friends/src/reducers.js
@@ -0,0 +1,37 @@
+import { FETCHING_FRIENDS, FETCHED_FRIENDS, ERROR } from '../actions';
+ const initialState = {
+    fetchingFriends: false,
+    friendsFetched: false,
+    friendsSaved: false,
+    savingFriends: false,
+    updatingFriend: false,
+    friendUpdated: false,
+    deletingFriend: false,
+    friendDeleted: false,
+    friends: [],
+    error: null
+}
+ export const friendsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCHING_FRIENDS:
+            return {
+                ...state,
+                fetchingFriends: true
+            }
+        case FETCHED_FRIENDS:
+            return {
+                ...state,
+                friendsFetched: true,
+                fetchingFriends: false,
+                friends: action.payload
+            }
+        case ERROR:
+            return {
+                ...state,
+                fetchingFriends: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+};
