c067084d1a18bb2eac7ddba82fb5ba5fc61f0048
Arthur Bates <aabatesj@gmail.com>
2018-08-23 16:12:44 -0400

    Arthur Bates
    initial setup
    
    first PR

---
+++ friends/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
+
+
+const MiddlewareQueue = applyMiddleware(logger, reduxThunk);
+const store = createStore(rootReducer, MiddlewareQueue);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
