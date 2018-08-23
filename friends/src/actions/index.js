9f2a6c5578893aace6dd661e853bd0111e14d63e
Arthur Bates <aabatesj@gmail.com>
2018-08-23 18:16:31 -0400

    action/index.js
    guess I forgot these actions

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 export const FETCH_FRIEND = 'FETCH_FRIEND';
 export const FETCHED = 'FETCHED';
+export const SAVE_FRIEND = 'SAVE_FRIEND';
+export const SAVED = 'SAVED';
+export const UPDATE_FRIEND = 'UPDATE_FRIEND';
+export const UPDATED = 'UPDATED';
+export const DELETE_FRIEND = 'DELETE_FRIEND';
+export const DELETED = 'DELETED'
 export const ERROR = 'ERROR';
 
 export const fetchingFriend = () => {
