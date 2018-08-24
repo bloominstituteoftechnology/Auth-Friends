ecbbbf3654915a79c15455f860b2f539ec48490c
bri79319 <bri79319@gmail.com>
2018-08-23 20:25:28 -0400

    had my PM helped me with a bug

--- friends/src/App.js
+++ friends/src/App.js
@@ -16,7 +16,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
           <FriendForm />
           <Friends />
         </header>
--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -5,13 +5,16 @@ import { getFriends } from "./actions";
 const Friends = props => {
   return (
     <div>
-      {props.friends.map(friend => (
-        <div className="friends">
-          <h2>Name: {friend.name}</h2>
-          <h3>Age: {friend.age}</h3>
-          <h3>Email: {friend.email}</h3>
-        </div>
-      ))}
+      {props.friends.map(friend => {
+        console.log("friend in map", friend);
+        return (
+          <div className="friends">
+            <h2>Name: {friend.name}</h2>
+            <h3>Age: {friend.age}</h3>
+            <h3>Email: {friend.email}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 };
