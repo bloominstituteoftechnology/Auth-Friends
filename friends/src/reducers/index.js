ac309078630e017fd0c53d93257ff3685f81e7d6
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-20 17:02:49 -0400

    set up initial state

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,4 +1,22 @@
-const initialState = {};
+const initialState = {
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [
+    {
+      id: 1,
+      name: 'Joe',
+      age: 24,
+      email: 'joe@lambdaschool.com',
+    }
+  ],
+  error: null
+};
 
 export default function(state = initialState, action) {
   switch(action.type) {
