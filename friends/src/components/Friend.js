bc066ba83f3657bb42f85274d0dd2e038ca88729
Michael Agard <mdagard@gmail.com>
2018-09-20 19:16:54 -0400

    remove all but name from friend

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -3,19 +3,17 @@ import { connect } from 'react-redux';
 import { deleteFriend } from '../actions';
 import { Link } from 'react-router-dom';
 
-const Friend = proops => {
+const Friend = props => {
   return (
     <li className="Friend">
-      <Link to={`/friend/${proops.friend.id}`} key={proops.friend.id}>
-        <h3>{proops.friend.name}</h3>
+      <Link to={`/friend/${props.friend.id}`} key={props.friend.id}>
+        <h3>{props.friend.name}</h3>
       </Link>
-      <p>Age: {proops.friend.age}</p>
-      <p>Email: {proops.friend.email}</p>
-      <button onClick={() => proops.deleteFriend(proops.friend.id)}>X</button>
+
+      <button onClick={() => props.deleteFriend(props.friend.id)}>X</button>
     </li>
   );
 };
-
 export default connect(
   null,
   { deleteFriend }
