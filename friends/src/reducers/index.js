81d49fd04ecba219cd637355b06c04bf0acec357
Kevin Nguyen <kevinn858@gmail.com>
2018-09-20 15:52:10 -0700

    initial set up

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+    friends: []
+};
+
+export const reducer = (state=initialState, action) =>{
+    switch(action.type){
+        default:
+            return state;
+    }
+}
