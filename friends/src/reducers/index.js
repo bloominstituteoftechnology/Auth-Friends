a7de2a0bd69209c4733c6c55c49c2c46356bb646
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-20 16:18:45 -0400

    Set initial state, and created reducer

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,40 @@
+import { FETCHING, FETCHED, ERROR, FRIEND_ADDED } from '../actions';
+
+const initialState = {
+    fetchingFriends: false,
+    friendsFetched: false,
+    friendAdded: false,
+    addingFriend: false,
+    friends: [],
+    error: null
+};
+
+export const friendsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCHING:
+            return Object.assign({}, state,
+                {
+                    fetchingFriends: true
+                });
+        case FETCHED:
+            return Object.assign({}, state, {
+                friends: action.payload,
+                fetchingFriends: false,
+                friendsFetched: true
+            });
+        case FRIEND_ADDED:
+            return Object.assign({}, state,
+                {
+                    friends: [...state.friends, action.payload],
+                    addingFriend: false,
+                    friendAdded: true,
+                });
+        case ERROR:
+            return Object.assign({}, state, {
+                fetchingFriends: false,
+                error: action.payload
+            });
+        default:
+            return state;
+    }
+};
