5fc877b0c6fae268b827b0a65f63bfbfb8de4ae2
decagondev <decagondevelopment@gmail.com>
2018-09-20 19:25:23 +0100

    (MVP DAY2) : added initialState content

--- friends/src/reducers/friendRedicer.js
+++ friends/src/reducers/friendRedicer.js
@@ -1,5 +1,14 @@
 const initialState = {
-  
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
 }
 
 export const friendsReducer = (state = initialState, action) => {
