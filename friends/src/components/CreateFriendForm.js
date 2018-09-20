9e17319fa39fe7727451925c4a4c06d93419759e
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-20 17:06:44 -0400

    added Friends, CreateFriendsForm, UpdateFriendsForm components

---
+++ friends/src/components/CreateFriendForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Friends extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Create Friends</h1>
+      </div>
+    );
+  };
+};
---
+++ friends/src/components/Friends.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Friends extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Friends</h1>
+      </div>
+    );
+  };
+};
