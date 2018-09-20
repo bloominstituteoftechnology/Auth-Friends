3ad2c0c5a7821f7037920075d86f3e609195f66a
Timothy Hoang <timh1203@gmail.com>
2018-09-20 12:38:03 -0400

    Add initial state data

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,5 +1,21 @@
 const initialState = {
-  
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [
+    {
+      id: 1,
+      name: 'Joe',
+      age: 24,
+      email: 'joe@lambdaschool.com',
+    }
+  ],
+  error: null
 }
 
 export default function (state = initialState, action) {
