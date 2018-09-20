2ca20f092a76866b8d37ebc910510d23afd1f688
Timothy Hoang <timh1203@gmail.com>
2018-09-20 14:22:11 -0400

    Add create friend form

--- friends/src/components/CreateFriendForm.js
+++ friends/src/components/CreateFriendForm.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
 
 export default class Friends extends Component {
+  state = {
+    name: '',
+    age: '',
+    email: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Create Friends Component</h2>
         <form>
-
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            name="age"
+            type="number"
+            placeholder="Age"
+            value={this.state.age}
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <button>Submit</button>
         </form>
       </div>
     )
