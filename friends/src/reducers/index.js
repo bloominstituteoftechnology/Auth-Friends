c7eb870219d471fbbaa1be60448a738b85ccc9eb
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-20 17:10:07 -0400

    added action cases to reducer for adding friends

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,39 +1,52 @@
-import { FETCHING, FETCHED, ERROR, FRIEND_ADDED } from '../actions';
+import {
+  FETCHING,
+  FETCHED,
+  ERROR,
+  FRIEND_ADDED,
+  ADDING_FRIEND,
+  ADD_FRIEND_ERROR
+} from "../actions";
 
 const initialState = {
-    fetchingFriends: false,
-    friendsFetched: false,
-    friendAdded: false,
-    addingFriend: false,
-    friends: [],
-    error: null
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendAdded: false,
+  addingFriend: false,
+  friends: [],
+  error: null
 };
 
 export const friendsReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FETCHING:
-            return Object.assign({}, state,
-                {
-                    fetchingFriends: true
-                });
-        case FETCHED:
-            return Object.assign({}, state, {
-                friends: action.payload,
-                fetchingFriends: false,
-                friendsFetched: true
-            });
-        case FRIEND_ADDED:
-            return Object.assign({}, state,
-                {
-                    friends: [...state.friends, action.payload],
-                    friendAdded: true,
-                });
-        case ERROR:
-            return Object.assign({}, state, {
-                fetchingFriends: false,
-                error: action.payload
-            });
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case FETCHING:
+      return Object.assign({}, state, { fetchingFriends: true });
+    case FETCHED:
+      return Object.assign({}, state, {
+        friends: action.payload,
+        fetchingFriends: false,
+        friendsFetched: true
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+        fetchingFriends: false,
+        error: action.payload
+      });
+    case ADDING_FRIEND:
+      return {
+        ...state,
+        addingFriend: true
+      };
+    case FRIEND_ADDED:
+      return {
+        ...state,
+        addingFriend: false,
+        friendAdded: true,
+        friends: action.payload
+      };
+    case ADD_FRIEND_ERROR:
+      console.log(action.payload);
+      return { ...state, addingFriend: false, error: action.payload };
+    default:
+      return state;
+  }
 };
