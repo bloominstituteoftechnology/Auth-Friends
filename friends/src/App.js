e09e7c7338f793243a195d2d7bb6ee20a495661e
alejandrok93 <alejandrok93@gmail.com>
2018-09-20 12:46:51 -0700

    Initial project set up

---
+++ friends/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Friends app:</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
