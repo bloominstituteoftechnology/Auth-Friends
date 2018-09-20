2b7380f55500d421f82ebda994cd1901b9c84f60
Michael Agard <mdagard@gmail.com>
2018-09-19 22:48:25 -0400

    remove componentwillmount

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import FriendsList from './FriendsList';
 import FriendForm from './FriendForm';
 export default class App extends Component {
-  componentDidMount() {
-    this.props.isFetch();
-  }
   render() {
     return (
       <div className="App">
