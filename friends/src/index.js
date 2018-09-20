eb85b869b660207b3e5b1f334594a8272c059ded
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-20 17:01:39 -0400

    set up src/index.js and default reducer case

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { applyMiddleware, compose, combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import rootReducer from './reducers';
+
+const allReducers = combineReducers({ rootReducer });
+const initialState = {};
+const allMiddlewares = compose(
+  applyMiddleware(thunk,logger),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+const store = createStore(allReducers, initialState, allMiddlewares);
+
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
