025d67335710faa1eab275be715fe2bd3aca113d
Csillag61 <csillagok@gmail.com>
2018-09-19 22:37:07 -0400

    Folders created, dependences added,

---
+++ friends/src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Friends from '.Friends';
+import FriendForm from '.FriendForm',
+import {getFriends} from '..actions';
+import {connect} from 'react-redux';
+
+class App extends Component {
+  componentDidMount(){
+    this.props.getFriends();
+  }  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">{`The Gates`}</h1>
+          <FriendForm/>
+        </header>
+        {this.props.error ? <h3>Error Fetching</h3>:null}
+        <div className="Flex-Container">
+          {this.props.gettingFriends ? (
+            <img src={logo} className="App-logo" alt="logo" />
+          ):(
+            <Friends friends={this.props.friends}/>
+          )}
+        </div>
+        </div>
+    );
+          }
+        }
+        const mapStateToProps = state=>{
+          const {friendsReducer.friends,
+          error: friendsReducer.error,
+        gettingFriends: friendsReducer.gettingFriends
+      };
+        };
+export default connect(mapStateToProps, {getFriends})(App);
