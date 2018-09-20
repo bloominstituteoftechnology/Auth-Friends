426a57659aad659b5f8e61316e1603f0d0398d19
decagondev <decagondevelopment@gmail.com>
2018-09-20 19:27:57 +0100

    (MVP DAY2) : added ERROR case to reducers

--- friends/src/reducers/friendRedicer.js
+++ friends/src/reducers/friendRedicer.js
@@ -9,18 +9,24 @@ const initialState = {
   friendDeleted: false,
   friends: [],
   error: null
-}
+};
 
 export const friendsReducer = (state = initialState, action) => {
-
-  switch(action.type) {
-    case 'FETCHING_FRIENDS':
+  switch (action.type) {
+    case "FETCHING_FRIENDS":
       return Object.assign({}, state, { fetchingFriends: true });
 
-    case 'FRIENDS_FETCHED':
-      return Object.assign({}, state, { friends: action.payload, friendsFetched: true, fetchingFriends: false });
+    case "FRIENDS_FETCHED":
+      return Object.assign({}, state, {
+        friends: action.payload,
+        friendsFetched: true,
+        fetchingFriends: false
+      });
+
+    case "ERROR":
+      return Object.assign({}, state, { error: action.payload });
 
     default:
       return state;
   }
-}
+};
