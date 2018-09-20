483e1489056a569c99836db22d51708ca09e0cf8
Michael Agard <mdagard@gmail.com>
2018-09-20 01:00:56 -0400

    addFriend post function

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -4,14 +4,32 @@ export const FETCHING = 'FETCHING';
 export const FETCHED = 'FETCHED';
 export const ERRORS = 'ERRORS';
 
+export const addFriend = friend => {
+  return dispatch => {
+    dispatch({ type: FETCHING });
+    axios
+      .post('http://localhost:5000/api/friends', friend)
+
+      .then(response => {
+        dispatch({ type: FETCHED, payload: response.data });
+        console.log(response.data);
+      })
+
+      .catch(error => dispatch({ type: ERRORS, error: error }));
+  };
+};
+
 export const fetchFriends = () => {
   return dispatch => {
     dispatch({ type: FETCHING });
     axios
-      .get('http://localhost:5000/')
+      .get('http://localhost:5000/api/friends')
+
       .then(response => {
-        dispatch({ type: FETCHED, payload: response.data.results });
+        console.log(response.data);
+        dispatch({ type: FETCHED, payload: response.data });
       })
+
       .catch(error => dispatch({ type: ERRORS, error: error }));
   };
 };
