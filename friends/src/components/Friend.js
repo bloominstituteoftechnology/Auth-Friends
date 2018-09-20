b107c98fde0042e16e18cc5afcf98ae196c01656
Michael Agard <mdagard@gmail.com>
2018-09-20 16:05:27 -0400

    import link to use to link to individual friend page

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { deleteFriend } from '../actions';
+import { Link } from 'react-router-dom';
 
 const Friend = proops => {
   return (
     <li className="Friend">
-      <h3>{proops.friend.name}</h3>
+      <Link to={`/friend/${proops.friend.id}`} key={proops.friend.id}>
+        <h3>{proops.friend.name}</h3>
+      </Link>
       <p>Age: {proops.friend.age}</p>
       <p>Email: {proops.friend.email}</p>
       <button onClick={() => proops.deleteFriend(proops.friend.id)}>X</button>
