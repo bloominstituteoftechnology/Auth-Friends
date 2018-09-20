12b45706fd688640b56f19850285944128fe1ca6
Clint Kunz <clintkunz@gmail.com>
2018-09-20 13:54:18 -0600

    created components

---
+++ friends/src/components/CreateFriendForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class CreateFriendForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    handleInput = event => {
+        event.preventDefault();
+
+    }
+
+    render() {
+        return (
+            <div>
+                <form action="">
+                    <input type="text" placeholder="name"/>
+                    <input type="text" placeholder="age"/>
+                    <input type="text" placeholder="email"/>
+                    <input type="submit" OnClick={console.log('submit')}/>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default CreateFriendForm; 
---
+++ friends/src/components/Friends.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import proptypes
+
+function Friend(props) {
+    return (
+        <div className="friend-cards">
+            {/* props.friends.map(friend => {}) */}
+        </div>
+    )
+}
+
+//Friend.propTypes...
+
+export default Friend; 
+
