ccdc13e2ac00ed1eaf284f437e685795b7ff450b
Timothy Hoang <timh1203@gmail.com>
2018-09-20 12:41:24 -0400

    Add friends and friends form components

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Friends from './components/Friends'
+import CreateFriendForm from './components/CreateFriendForm'
+import UpdateFriendForm from './components/UpdateFriendForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Friends App</h1>
+        <Friends />
+        <CreateFriendForm />
+        <UpdateFriendForm />
       </div>
     );
   }
---
+++ friends/src/components/UpdateFriendForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Friends extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Update Friends Component</h1>
+      </div>
+    )
+  }
+}
