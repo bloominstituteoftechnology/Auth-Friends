f334cf12e9835c7a1be1b2182fca507146e2e122
m-j-hacker <mjhacker@gmail.com>
2018-09-20 15:48:49 -0500

    began filling out fetchFriends action creator

---
+++ friends/src/store/actions/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const FETCHED = 'FETCHED';
+export const ERROR = 'ERROR';
+
+export const fetchFriends = () => {
+    return dispatch => {
+        dispatch({ type: FETCHING });
+        axios
+        .get('/api/friends')
+        .then(response => {
+            dispatch({
+                type: FETCHED,
+                payload: response.data
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({ type: ERROR });
+        })
+    }
+}
