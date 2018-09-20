653846b74595b427b4b59b89227ea3048353cfeb
Michael Agard <mdagard@gmail.com>
2018-09-20 00:58:44 -0400

    fix fetchFriends reference

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Friend from './Friend';
-import { fetchFriends } from '../actions/index';
+import { fetchFriends } from '../actions';
 
 class FriendsList extends Component {
   componentDidMount() {
