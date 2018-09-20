a2be0819de4892c89ec2e114036a6d7a4f0e4a68
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-20 17:25:10 -0400

    Created Friends Component

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,1 +1,19 @@
 
+import React from 'react';
+
+function Friends (props) {
+    if (!props.friends || !props.friends.length) {
+        return <h1>You have no friends. :( Go fetch them!</h1>
+    }
+    return <div className="characters-list-wrapper">
+        {props.friends.map(friend => (
+          <div className="friend-card" key={friend.id}>
+            {friend.name}
+            {friend.age}
+            {friend.email}
+          </div>
+        ))}
+      </div>;
+}
+
+export default Friends;
