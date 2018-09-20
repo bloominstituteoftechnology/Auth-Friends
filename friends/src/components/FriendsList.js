63fd9d356568e29b5fdd3ffad7e33e1c3d986061
Michael Agard <mdagard@gmail.com>
2018-09-19 22:50:34 -0400

    add FriendList component

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Friend from './Friend';
+import { fetchFriends } from '../actions/index';
+
+class FriendsList extends Component {
+  componentDidMount() {
+    this.props.fetchFriends();
+  }
+  render() {
+    return (
+      <div className="FriendsList">
+        {this.props.fetching ? (
+          <p>Reticulating Splines...</p>
+        ) : (
+          <ul>
+            {this.props.friends.map(friend => {
+              return <Friend key={friend.id} friend={friend} />;
+            })}
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  friends: state.friendsReducer.friends,
+  fetching: state.friendsReducer.fetching
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchFriends }
+)(FriendsList);
