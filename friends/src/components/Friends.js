8005592d6e192e28ced91d6050a79f9135b8bb72
MikeRGarrity <mikergarrity@gmail.com>
2018-09-20 16:38:25 -0500

    get functionality working, friends are displaying

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+ const Friends = props =>
+  <div className='card'>
+    <p>{props.data.id}</p>
+    <p>{props.data.name}</p>
+    <p>{props.data.age}</p>
+    <p>{props.data.email}</p>
+  </div>
+
+export default Friends; 
--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {applyMiddleware,createStore} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import {friendsReducer} from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ const store=createStore(friendsReducer,applyMiddleware(thunk, logger));
+
+
+ ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root')
+);
