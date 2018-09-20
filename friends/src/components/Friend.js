01ad9ea5a3034086ba1f54c3ee6941245cc66b68
Michael Agard <mdagard@gmail.com>
2018-09-20 01:14:35 -0400

    change email to reflect email

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -5,7 +5,7 @@ const Friend = props => {
     <li className="Friend">
       <h3>{props.friend.name}</h3>
       <p>Age: {props.friend.age}</p>
-      <p>Email: {props.friend.age}</p>
+      <p>Email: {props.friend.email}</p>
     </li>
   );
 };
