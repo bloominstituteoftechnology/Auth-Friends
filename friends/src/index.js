ecf835bc8c9225e79e10880ce5fd22ff79165d00
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-20 15:59:16 -0400

    Created Store and imported Middleware

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
+ document.getElementById('root'));
+
