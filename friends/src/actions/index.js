a9dc261a4bcdcec0a79cc1a39a950410e8450b57
Michael Agard <mdagard@gmail.com>
2018-09-20 02:06:40 -0400

    add deleteFriend function

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -4,6 +4,20 @@ export const FETCHING = 'FETCHING';
 export const FETCHED = 'FETCHED';
 export const ERRORS = 'ERRORS';
 
+export const deleteFriend = id => {
+  return dispatch => {
+    dispatch({ type: FETCHING });
+    axios
+      .delete(`http://localhost:5000/api/friends/${id}`)
+      .then(response => {
+        console.log(response.data);
+        dispatch({ type: FETCHED, payload: response.data });
+      })
+
+      .catch(error => dispatch({ type: ERRORS, error: error }));
+  };
+};
+
 export const addFriend = friend => {
   return dispatch => {
     dispatch({ type: FETCHING });
@@ -12,7 +26,7 @@ export const addFriend = friend => {
 
       .then(response => {
         dispatch({ type: FETCHED, payload: response.data });
-        console.log(response.data);
+        // console.log(response.data);
       })
 
       .catch(error => dispatch({ type: ERRORS, error: error }));
@@ -26,7 +40,7 @@ export const fetchFriends = () => {
       .get('http://localhost:5000/api/friends')
 
       .then(response => {
-        console.log(response.data);
+        // console.log(response.data);
         dispatch({ type: FETCHED, payload: response.data });
       })
 
