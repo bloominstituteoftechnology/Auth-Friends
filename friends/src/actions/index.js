62bc2fdffa1de7ab96d7513ca677ca8f87b6a203
Andrew <ghobrial.andrew@gmail.com>
2018-09-20 16:55:08 -0400

    index.js in actions complete

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
-export const BLAHBLAH = 'BLAHBLAH';
+export const FETCHING_DATA = 'FETCHING_DATA';
+export const DATA_FETCHED = 'DATA_FETCHED';
+export const FETCH_ERROR = 'FETCH_ERROR';
 
-
-
-// export const getData = () => {
-//   return (dispatch) => {
-//     dispatch({ type: FETCHING_DATA });
-//     axios
-//       .get('https://swapi.co/api/people')
-//       .then((response) => {
-//         dispatch({ type: DATA_FETCH_SUCCESS, payload: response.data.results });
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         dispatch({ type: DATA_FETCH_ERROR });
-//       });
-//   };
-// };
+export const fetchData = () => {
+  return (dispatch) => {
+    dispatch({ type: FETCHING_DATA });
+    axios
+      .get('http://localhost:5000/api/friends')
+      .then((response) => {
+        dispatch({ type: DATA_FETCHED, payload: response.data });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: FETCH_ERROR });
+      });
+  };
+};
