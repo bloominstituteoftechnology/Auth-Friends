3813a2c6c324bdf642a6c543f9e2146ffff61f95
Michael Agard <mdagard@gmail.com>
2018-09-20 00:59:18 -0400

    replace Object.assign with spread operator. rename reducer

--- friends/src/reducers/friendsReducer.js
+++ friends/src/reducers/friendsReducer.js
@@ -2,22 +2,24 @@ import { FETCHING, FETCHED, ERRORS } from '../actions';
 
 const initialState = { friends: [], fetching: false, error: '' };
 
-export const charsReducer = (state = initialState, action) => {
+export const friendsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCHING:
-      return Object.assign({}, state, { fetching: true });
+      return { ...state, fetching: true };
 
     case FETCHED:
-      return Object.assign({}, state, {
-        name: action.payload,
+      return {
+        ...state,
+        friends: action.payload,
         fetching: false
-      });
+      };
 
     case ERRORS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         fetching: false,
         error: 'Error fetching friends'
-      });
+      };
 
     default:
       return state;
