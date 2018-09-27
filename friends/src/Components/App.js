cb3d5ff8324b031873bc294cbeb6a413e1f6ce55
Matthew Wright <matthewbwright@gmail.com>
2018-09-27 13:51:16 -0700

    sets up reducers index.js

--- friends/src/Components/App.js
+++ friends/src/Components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import FriendsList from './FriendsList';
 
-import './App.css';
+import '../App.css';
 
 class App extends Component {
   render() {
--- friends/src/store/reducers/index.js
+++ friends/src/store/reducers/index.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react';
-import FriendsList from './FriendsList';
+import {
+  FETCHING_FRIENDS,
+  FETCHING_SUCCESS,
+  ERROR_FRIENDS,
+  FRIEND_ADD,
+  ADD_SUCCESS,
+  ADD_ERROR
+} from '../actions';
 
-import '../App.css';
+const intitialState = {
+  friends: [],
+  fetching: false,
+  error: null
+};
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <FriendsList />
-      </div>
-    );
+export const friendReducers = (state = intitialState, action) => {
+  switch (action.type) {
+    case FETCHING_FRIENDS:
+      return Object.assign({}, state, { fetching: true });
+    case FETCHING_SUCCESS:
+      return Object.assign({}, state, {
+        fetching: false,
+        friends: action.payload
+      });
+    case ERROR_FRIENDS:
+      return Object.assign({}, state, { error: 'Error' });
+    case FRIEND_ADD:
+      return Object.assign({}, state, { fetching: true });
+    case ADD_SUCCESS:
+      return Object.assign({}, state, {
+        fetching: false,
+        friends: action.payload
+      });
+    case ADD_ERROR:
+      return Object.assign({}, state, { error: 'Error' });
   }
-}
-
-export default App;
+};
