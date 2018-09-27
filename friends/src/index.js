e76b4836189530d31a4cebfdcac990e89b40944e
A2i <abdulfatah.a.ahmad@gmx.com>
2018-09-27 18:31:28 -0400

    Add store

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'
 
+const store = createStore(rootReducer,applyMiddleware(logger, thunk));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, 
+document.getElementById('root'));
 
