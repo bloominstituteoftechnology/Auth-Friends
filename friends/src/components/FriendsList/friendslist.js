a90176b86f8c324671fd0c7b3defb5737e40f366
Marious Davis <mlancelot88@gmail.com>
2018-09-27 17:58:58 -0500

    removal of styled-components that block successful compile

--- friends/src/components/FriendsList/friendslist.js
+++ friends/src/components/FriendsList/friendslist.js
@@ -1,34 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Styled from "styled-components";
-import Friend from "./friend";
-
-const ErrorP = Styled.p`
-    color: #FF0000;
-`;
 
 const FriendsList = props => {
   return props.crudStates.postingFriend ||
     props.crudStates.gettingFriends ||
     props.crudStates.puttingFriend ||
     props.crudStates
-      .deletingFriend /* || props.crudStates.gettingSingleFriend */ ? (
+      .deletingFriend (
     <p>Getting friends. Please wait. :)</p>
-  ) : props.crudStates.crudError !== null ? (
-    <ErrorP>Friend {props.crudStates.crudError}</ErrorP>
-  ) : (
-    <div>
-      {props.friends.map(friend => (
-        <Friend
-          friend={friend}
-          editHandler={props.editHandler}
-          deleteFriend={props.deleteFriend}
-          key={friend.id}
-        />
-      ))}
-    </div>
-  );
-};
+  ) ;};
 
 FriendsList.propTypes = {
   friends: PropTypes.arrayOf(
@@ -44,8 +24,6 @@ FriendsList.propTypes = {
     postedFriend: PropTypes.bool,
     gettingFriends: PropTypes.bool.isRequired,
     gotFriends: PropTypes.bool,
-    // gettingSingleFriend: PropTypes.bool.isRequired,
-    // gotSingleFriend: PropTypes.bool,
     puttingFriend: PropTypes.bool.isRequired,
     putFriend: PropTypes.bool,
     deletingFriend: PropTypes.bool.isRequired,
