5dd4f00aa867849ec3e1fefec024ab2c0801ff09
Marious Davis <mlancelot88@gmail.com>
2018-09-27 15:56:40 -0500

    minor spacing done for neatness

---
+++ friends/src/components/FriendsList/friend.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Styled from "styled-components";
+
+const BoldDiv = Styled.div`
+    font-weight: bolder;
+    cursor: pointer;
+`;
+
+const Friend = props => {
+  return (
+    <div>
+      <h3 onClick={() => props.editHandler(props.friend)}>
+        {props.friend.name}
+      </h3>
+      <BoldDiv onClick={() => props.deleteFriend(props.friend.id)}>X</BoldDiv>
+      <p>Age: {props.friend.age}</p>
+      <p>Email: {props.friend.email}</p>
+    </div>
+  );
+};
+Friend.propTypes = {
+  friend: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    email: PropTypes.string.isRequired
+  }),
+  editHandler: PropTypes.func.isRequired,
+  deleteFriend: PropTypes.func.isRequired
+};
+export default Friend;
