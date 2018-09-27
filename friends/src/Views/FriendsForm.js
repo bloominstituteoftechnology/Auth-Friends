1a8e77840a538ef1f363b6eee75043b641f84fb1
Matthew Wright <matthewbwright@gmail.com>
2018-09-27 13:02:53 -0700

    sets up FriendsForm.js

---
+++ friends/src/Views/FriendsForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function FriendsForm(props) {
+  return (
+    <form>
+      <input
+        name="name"
+        type="text"
+        placeholder="Friend's name"
+        value={props.input.name}
+        onChange={props.handleChange}
+      />
+      <input
+        name="age"
+        type="number"
+        placeholder="Friend's age"
+        value={props.input.age}
+        onChange={props.handleChange}
+      />
+      <input
+        name="email"
+        type="text"
+        placeholder="Friend's email"
+        value={props.input.email}
+        onChange={props.handleChange}
+      />
+    </form>
+  );
+}
+
+export default FriendsForm;
