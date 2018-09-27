2f306151889f3909989fa98e51b2d6425448b50b
lyang9 <lyang9@buffalo.edu>
2018-09-27 17:42:39 -0400

    Exported reducer and created action types

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,10 +1,9 @@
 import {
   FETCHING_FRIENDS,
   FRIENDS_FETCHED,
-  ERROR_FETCHED,
   SAVING_FRIEND,
   FRIEND_SAVED,
-  ERROR_SAVED
+  ERROR
 } from '../actions';
 
 const initialState = {
@@ -14,4 +13,33 @@ const initialState = {
   friendSaved: false,
   friends: [],
   error: null
-}
+};
+
+export const friendsReducer = ( state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_FRIENDS:
+      return Object.assign({}, state, { fetchingFriends: true });
+    case FRIENDS_FETCHED:
+      return Object.assign({}, state, {
+        friends: action.payload,
+        fetchingFriends: false,
+        friendsFetched: true
+      });
+    case SAVING_FRIEND:
+      return Object.assign({}, state, { savingFriend: true });
+    case FRIEND_SAVED:
+      return Object.assign({}, state, {
+        friends: action.payload,
+        savingFriend: false,
+        friendSaved: true
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+        fetchingFriends: false,
+        savingFriend: false,
+        error: action.payload
+      });
+    default:
+      return state;
+  }
+};
