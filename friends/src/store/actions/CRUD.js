aa6152abc0fed90ff04432e91d0246ab73210410
Marious Davis <mlancelot88@gmail.com>
2018-09-27 16:56:23 -0500

    CRUD.js & index.js files built in actions directory. functionality added

---
+++ friends/src/store/actions/CRUD.js
@@ -0,0 +1,69 @@
+import Axios from "axios";
+export const CRUD_ERROR = "CRUD_ERROR";
+export const POSTING_FRIEND = "POSTING_FRIEND";
+export const POSTED_FRIEND = "POSTED_FRIEND";
+export const GETTING_FRIENDS = "GETTING_FRIENDS";
+export const GOT_FRIENDS = "GOT_FRIENDS";
+export const PUTTING_FRIEND = "PUTTING_FRIEND";
+export const PUT_FRIEND = "PUT_FRIEND";
+export const DELETING_FRIEND = "DELETING_FRIEND";
+export const DELETED_FRIEND = "DELETED_FRIEND";
+
+const dataSource = "http://localhost:5000/api/friends";
+
+// Create (POST)
+export const postFriend = newFriend => {
+  console.log("postFriends newFriend", newFriend);
+  return dispatch => {
+    dispatch({ type: POSTING_FRIEND });
+    Axios.post(dataSource, newFriend)
+      .then(response => {
+        dispatch({ type: POSTED_FRIEND, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: CRUD_ERROR, payload: err.message });
+      });
+  };
+};
+
+// Read (GET)
+export const getFriends = () => {
+  return dispatch => {
+    dispatch({ type: GETTING_FRIENDS });
+    Axios.get(dataSource)
+      .then(response => {
+        dispatch({ type: GOT_FRIENDS, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: CRUD_ERROR, payload: err.message });
+      });
+  };
+};
+
+// Update (PUT)
+export const putFriend = updatedFriend => {
+  return dispatch => {
+    dispatch({ type: PUTTING_FRIEND });
+    Axios.put(`${dataSource}/${updatedFriend.id}`, updatedFriend)
+      .then(response => {
+        dispatch({ type: PUT_FRIEND, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: CRUD_ERROR, payload: err.message });
+      });
+  };
+};
+
+// Delete (DELETE)
+export const deleteFriend = friendId => {
+  return dispatch => {
+    dispatch({ type: DELETING_FRIEND });
+    Axios.delete(`${dataSource}/${friendId}`)
+      .then(response => {
+        dispatch({ type: DELETED_FRIEND, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: CRUD_ERROR, payload: err.message });
+      });
+  };
+};
---
+++ friends/src/store/actions/index.js
@@ -0,0 +1,15 @@
+export {
+  CRUD_ERROR,
+  postFriend,
+  POSTING_FRIEND,
+  POSTED_FRIEND,
+  getFriends,
+  GETTING_FRIENDS,
+  GOT_FRIENDS,
+  putFriend,
+  PUTTING_FRIEND,
+  PUT_FRIEND,
+  deleteFriend,
+  DELETING_FRIEND,
+  DELETED_FRIEND
+} from "./crudactions";
