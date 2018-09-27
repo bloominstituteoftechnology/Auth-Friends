2ce262d179bb99ffc8d33b32bdeb345aa118313d
lyang9 <lyang9@buffalo.edu>
2018-09-27 17:46:12 -0400

    Imported axios and created all action types

---
+++ friends/src/actions/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
+export const FRIENDS_FETCHED = 'FRIENDS_FETCHED';
+export const SAVING_FRIEND = 'SAVING_FRIEND';
+export const FRIEND_SAVED = 'FRIEND_SAVED';
+export const ERROR = 'ERROR'; 
+
