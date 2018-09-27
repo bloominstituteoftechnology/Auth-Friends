a355572684d4789f5b44c13dc81a01fdcbd796d6
Marious Davis <mlancelot88@gmail.com>
2018-09-27 16:35:15 -0500

    imported Middleware & Redux dependacies into index.js. cleaned up App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import FriendsContainer from "../containers/friendscontainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FriendsContainer />
       </div>
     );
   }
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./store/reducers/";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reduxDevToolsHook =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    reduxDevToolsHook
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
