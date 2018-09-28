b27dfb8e67a0c8b9528d251abf00593ac326072c
lyang9 <lyang9@buffalo.edu>
2018-09-28 02:14:07 -0400

    Added onSubmit to form and onChange to input

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -3,21 +3,24 @@ import React from 'react';
 const FriendForm = props => {
   return (
     <div className='FriendForm'>
-      <form>
+      <form onSubmit={props.saveFriend}>
         <input
           name='name'
           placeholder='name'
           value={props.state.name}
+          onChange={props.handleInput}
         />
         <input
           name='age'
           placeholder='age'
           value={props.state.age}
+          onChange={props.handleInput}
         />
         <input
           name='email'
           placeholder='email'
           value={props.state.email}
+          onChange={props.handleInput}
         />
         <button type='submit'>Add Friend</button>
       </form>
