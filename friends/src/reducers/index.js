1e9a4ce8bb6801edd69164400e1b699a7ca4c3f2
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-24 15:35:47 -0700

    added components and got friends to display

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,41 @@
+import { FETCHING_FRIENDS, FRIENDS_FETCH_SUCCESS, FRIENDS_FETCH_FAILURE } from '../actions';
+
+const initialState = {
+    fetchingFriends: false,
+    friendsFetched: false,
+    friendsSaved: false,
+    savingFriends: false,
+    updatingFriend: false,
+    friendUpdated: false,
+    deletingFriend: false,
+    friendDeleted: false,
+    friends: [
+        {
+            id: 1,
+            name: 'Joe',
+            age: 24,
+            email: 'joe@lambdaschool.com',
+        }
+    ],
+    error: null
+  };
+
+  export const friendsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCHING_FRIENDS:
+            return { ...state, fetchingFriends: true };
+
+        case FRIENDS_FETCH_SUCCESS:
+            return {
+                ...state,
+                fetchingFriends: false,
+                friends: [...state.friends, ...action.payload]
+            };
+
+        case FRIENDS_FETCH_FAILURE:
+            return { ...state, fetchingFriends: false, error: action.payload };
+
+        default:
+            return state;        
+    };
+  };
