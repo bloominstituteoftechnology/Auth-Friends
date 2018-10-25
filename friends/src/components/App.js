fa3c3a9005003aea360f751d38b639f49c626e9f
Lee Len Formento <leebatef@gmail.com>
2018-10-25 18:22:21 -0400

    MVPPPPPPP

---
+++ friends/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Friend from "./Friend";
+import FriendForm from "./FriendForm";
+
+ class App extends Component {
+	render() {
+		return (
+		<div className="App">
+        <FriendForm />
+        <Friend />
+		</div>
+		);
+	}
+}
+ export default App;
+
