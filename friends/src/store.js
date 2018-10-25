9ddc3a9afce03a1ed8ba7e30e790b1bed5ba9658
Drewsup123c <drewsup123c@gmail.com>
2018-10-25 13:41:05 -0600

    created store.js and wrapped App with the provider

---
+++ friends/src/store.js
@@ -0,0 +1,32 @@
+import charsReducer from "./reducers";
+import { applyMiddleware, createStore, compose } from "redux";
+
+const logger = store => next => action => {
+    console.group("Redux Action");
+    console.log("current state", store.getState());
+    console.log("action", action);
+    let result = next(action);
+    console.log("new state", store.getState());
+    console.groupEnd();
+    return result;
+};
+
+// const thunk = store => next => action => {
+//     if (typeof action === "function") {
+//         if (!store.getState().chars.fetching) {
+//         action(store.dispatch);
+//         } else {
+//             console.log("WHAT ARE YOU TRYING TO DO IM ALREADY FETCHING???");
+//         }
+//     } else {
+//         return next(action);
+//     }
+// };
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    charsReducer,
+    composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+export default store;
