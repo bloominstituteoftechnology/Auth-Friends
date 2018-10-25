c2412a617d1d1b897610ff6d21a7b681ecfbbc12
Matthew Wright <matthewbwright@gmail.com>
2018-10-25 12:08:23 -0700

    adds state

---
+++ friends/src/Components/FriendsList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Friend from '../Views/Friend';
+import FriendsForm from '../Views/FriendsForm';
+
+class FriendsList extends Component {
+  state = {
+    name: '',
+    age: '',
+    email: ''
+
+  }
+}
+
+export default FriendsList
