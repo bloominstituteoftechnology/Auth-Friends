59acd5db4008d75589004fd9b60d742e3575d8d0
txiong000 <tkx422@gmail.com>
2018-10-25 15:07:07 -0500

    added css

--- friends/src/components/friend.js
+++ friends/src/components/friend.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import './friend.css'
 
 const Friends = (props) => {
     return (
-        <div>
-            <h2>Name: {props.name}</h2>
-            <h2>Age: {props.age}</h2>
+        <div className="flex-children">
+            <h4>Name: {props.name}</h4>
+            <h4>Age: {props.age}</h4>
+            <h4>Email: {props.email}</h4>
         </div>
     )
 }
--- friends/src/components/friendsList.js
+++ friends/src/components/friendsList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchFriends } from '../actions';
 import Friend from './friend';
+import './friend.css'
 
 class  FriendList extends React.Component {
     constructor() {
@@ -19,9 +20,17 @@ class  FriendList extends React.Component {
     
         return (
             <div>
-                {this.props.friends.map(friend => {
-                    return <Friend name={friend.name} age={friend.age}/>
-                })}
+                <div className="input-container">
+                    <input placeholder="Name"></input>
+                    <input placeholder="Age"></input>
+                    <input placeholder="Email"></input>
+                    <button>Submit</button>
+                </div>
+                <div className="flex-container">
+                    {this.props.friends.map(friend => {
+                        return <Friend name={friend.name} email={friend.email} age={friend.age}/>
+                    })}
+                </div>
             </div>
         )
     }
