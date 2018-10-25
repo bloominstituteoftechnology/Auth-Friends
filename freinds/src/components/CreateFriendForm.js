70d5b029c93415941ec3202420f557beedb67506
Unknown <zachary.arney@gmail.com>
2018-10-25 16:21:46 -0500

    Created CreateFriendForm and UpdateFriendForm

---
+++ freinds/src/components/CreateFriendForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateFriendForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateFriendForm
---
+++ freinds/src/components/UpdateFriendForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UpdateFriendForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UpdateFriendForm
