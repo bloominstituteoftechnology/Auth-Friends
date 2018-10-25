277c63af06cf59180dce27245b674d2ed8efc4fe
Matthew Wright <matthewbwright@gmail.com>
2018-10-25 12:41:00 -0700

    sets up fetch friends

--- friends/src/store/actions/index.js
+++ friends/src/store/actions/index.js
@@ -9,3 +9,17 @@ export const FRIEND_UPDATED = 'FRIEND_UPDATED';
 export const DELETING_FRIEND = 'DELETING_FRIEND';
 export const FRIEND_DELETED = 'FRIEND_DELETED';
 export const ERROR = 'ERROR';
+
+export const fetchFriends = () => {
+  return dispatch => {
+    dispatch({ type: FETCHING_FRIENDS });
+    const promise = axios.get('http://localhost:5000/api/friends/');
+    promise
+      .then(response => {
+        dispatch({ type: FRIENDS_FETCHED, payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
