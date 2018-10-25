387ff6aa31a747a9920efa6e060204128685b4d7
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-24 17:30:29 -0700

    added redux tools function

--- friends/src/index.js
+++ friends/src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose  } from 'redux';
 import { friendsReducer } from './reducers';
 
-const store = createStore(friendsReducer, applyMiddleware(thunk, logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(friendsReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(
     <Provider store={store}>
