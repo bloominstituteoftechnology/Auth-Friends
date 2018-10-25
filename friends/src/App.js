3bae8bfe184b60eb14e5421effb991c68a612140
Chad Jemmett <chad.jemmett@gmail.com>
2018-10-25 13:52:12 -0600

    removed some files

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FriendsListView from './views/FriendsListView'
 
 class App extends Component {
   render() {
--- friends/src/index.js
+++ friends/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {connect} from 'react-redux';
 import friendsReducer from './reducers';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})  : compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger))
+const store = createStore(friendsReducer, enhancer)
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
