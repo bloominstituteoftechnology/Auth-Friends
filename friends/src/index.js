727bfd5115044c27e4fca8666142ee6631c89232
Chad Jemmett <chad.jemmett@gmail.com>
2018-10-25 13:29:41 -0600

    Chad Jemmett Redux Friends app

---
+++ friends/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {connect} from 'react-redux';
+import friendsReducer from './reducers';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
