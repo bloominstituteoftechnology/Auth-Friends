ac2ae2dd4105ae77a99df791060d838edb89bfbf
SavannahGreuel <smallidgesavannah@gmail.com>
2018-10-25 14:37:12 -0600

    added action creators for axios

---
+++ friends/src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const FETCH_FRIENDS = 'FETCH_FRIENDS'
+export const FETCH_FRIENDS_SUCCESS = 'FETCH_FRIENDS_SUCCESS'
+export const FETCH_FRIENDS_FAILURE = 'FETCH_FRIENDS_FAILURE'
+
+export const fetchFriends = () => dipatch => {
+    dispatch ({ type: FETCH_FRIENDS });
+
+    axios 
+        .get(`http://localhost:5000/api/friends`)
+        .then(response => {
+            dispatch({ type: FETCH_FRIENDS_SUCCESS , payload: response.data})
+        })
+        .catch(error => {
+            dispatch ({ type: FETCH_FRIENDS_FAILURE, payload: error})
+        });
+}
