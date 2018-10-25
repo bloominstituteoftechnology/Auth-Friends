ed4d285ece1fc80b71fbe849f3133028e64a7f16
unknown <quan.nguyen199712@gmail.com>
2018-10-25 18:38:13 -0400

    finished with mvp

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -4,11 +4,40 @@ import Friend from "./friend";
 
 const Friends = props => {
   return (
-    <ul>
-      {props.friends.map(friend => {
-        return <Friend key={friend.name} friend={friend} />
-      })}
-    </ul>
+    <div className="container">
+      <ul>
+        {props.friends.map(friend => {
+          return <Friend key={friend.name} friend={friend} />;
+        })}
+      </ul>
+      <form type="submit">
+        Name:
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={props.newFriend.name}
+          onChange={props.handleChanges}
+        />
+        Age:
+        <input
+          type="text"
+          placeholder="Age"
+          name="age"
+          value={props.newFriend.age}
+          onChange={props.handleChanges}
+        />
+        Email:
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={props.newFriend.email}
+          onChange={props.handleChanges}
+        />
+        <button onClick={props.addFriend }> Add friend</button>
+      </form>
+    </div>
   );
 };
 
--- friends/src/view/friendView.js
+++ friends/src/view/friendView.js
@@ -2,22 +2,46 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { Friends } from "../components";
-import { fetchFriends } from "../actions";
+import { fetchFriends, addFriends } from "../actions";
+import { throws } from "assert";
 
 class FriendView extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      newFriend: {
+        name: "",
+        age: "",
+        email: ""
+      }
+    };
   }
   componentDidMount() {
     this.props.fetchFriends();
   }
+  handleChanges=(event)=>{
+    this.setState({
+      newFriend: {
+        ...this.state.newFriend,
+        [event.target.name]: event.target.value
+      }
+    });
+  }
+  addFriend=(event)=>{
+    this.props.addFriends(this.state.newFriend)
+  }
   render() {
-    // if (this.props.fetchingFriends) {
-    //   <h1>Fetching some starwar name from wherever</h1>;
-    // }
+    if (this.props.fetchingFriends) {
+      return <h1>Fetching some starwar name from wherever</h1>;
+    }
     return (
       <div className="CharactersList_wrapper">
-        <Friends friends={this.props.friends} />;
+        <Friends
+          friends={this.props.friends}
+          newFriend={this.state.newFriend}
+          handleChanges={this.handleChanges}
+          addFriend={this.addFriend}
+        />
       </div>
     );
   }
@@ -30,7 +54,7 @@ const mapStateToProps = state => {
     friendsSaved: state.friendReducer.friendsSaved,
     savingFriends: state.friendReducer.savingFriends,
     updatingFriend: state.friendReducer.updatingFriend,
-    friendUpdated:state.friendReducer.friendUpdated,
+    friendUpdated: state.friendReducer.friendUpdated,
     deletingFriend: state.friendReducer.deletingFriend,
     friendDeleted: state.friendReducer.friendDeleted,
     friends: state.friendReducer.friends,
@@ -39,5 +63,5 @@ const mapStateToProps = state => {
 };
 export default connect(
   mapStateToProps,
-  { fetchFriends }
+  { fetchFriends, addFriends }
 )(FriendView);
