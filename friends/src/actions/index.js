844f47c72f1f5a4c00a11a8208c5b481faa94947
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-25 15:14:59 -0500

    actions index js

---
+++ friends/src/actions/index.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+export const FETCHING_CHARS = 'FETCHING_FRIENDS';
+export const FETCHING_CHARS_SUCCESS = 'FETCHING_FRIENDS_SUCCESS';
+export const FETCHING_CHARS_FAILURE = 'FETCHING_FRIENDS_FAILURE';
+export const CREATING_FRIENDS="CREATING_FRIENDS"; 
+export const CREATED_FRIENDS="CREATED_FRIENDS"; 
+export const SINGLE_FRIEND="SINGLE_FRIEND"; 
+export const TOGGLE_FRIEND="TOGGLE_FRIEND"; 
+
+export const fetchfriends = () => dispatch => {
+  dispatch({ type: FETCHING_FRIENDS });
+  axios
+    .get('http://localhost:5000/api/friends')
+    .then(response => {
+      dispatch({ type: FETCHING_FRIENDS_SUCCESS, payload: response.data });
+    })
+    .catch(error => {
+      dispatch({ type: FETCHING_FRIENDS_FAILURE, payload: error });
+    });
+};
+
+
+export const createFriends = friend =>{
+    const newFriend = axios.post(`${URL}`, friend); 
+    return dispatch => {
+        dispatch({ type: CREATING_FRIENDS}); 
+        newFriend
+        .then(({ data }) => {
+            dispatch({ type: CREATED_FRIENDS, payload: data}); 
+        })
+        .catch(err =>{
+            dispatch({type: ERROR, payload: err}); 
+        })
+    };
+}; 
+
+export const toggleShowUpdate = () => {
+    return{
+        type: TOGGLE_FRIEND
+    };
+}; 
+
+export const updateSingleFriend = friend => {
+    return {
+        type: SINGLE_FRIEND, 
+        payload: friend 
+    }; 
+};
