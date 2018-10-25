b9c7d97062cd47bc2cf5c977b5378fc8c2c7226d
Lee Len Formento <leebatef@gmail.com>
2018-10-25 14:24:24 -0400

    Map over friends

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,28 @@
+import React, {Component}from 'react';
+import { fetchFriends } from '../actions/friendsActions';
+import { connect } from "react-redux";
+
+class Friends extends Component {
+	componentDidMount() {
+		this.props.fetchFriends();
+	}
+	render() {
+		return (
+			<div className="friends">
+				{this.props.friends.map(friend => (
+					<div className="friend" key={friend.id}>
+						<h3>Name: {friend.name}</h3>
+						<p>Age: {friend.age}</p>
+						<p>Age: {friend.email}</p>
+					</div>
+				))}
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	return { friends: state.friends };
+};
+
+export default connect(mapStateToProps,{ fetchFriends })(Friends);
