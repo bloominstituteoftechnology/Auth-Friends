0d0aaf966ee030f00b402cd3339583e403e10e61
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-25 09:33:17 -0600

    Inial setup, initialState on friendsReducer.js

---
+++ friends/src/reducers/friendsReducer.js
@@ -0,0 +1,16 @@
+// need my action types imported from '../actions';
+
+const initialState = {
+    
+    fetchingFriends: false,
+    friendsFetched: false,
+    friendsSaved: false,
+    savingFriends: false,
+    updatingFriend: false,
+    friendUpdated: false,
+    deletingFriend: false,
+    friendDeleted: false,
+    friends: [],
+    error: null
+
+}
