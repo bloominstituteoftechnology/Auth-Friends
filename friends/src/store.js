e5eb8fd9c9a6d79f68f6b62e35fe0a0c944dd1db
jsmnglng <jsmn.glng@gmail.com>
2018-10-25 11:46:32 -0700

    Setup reducers

--- friends/src/store.js
+++ friends/src/store.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-const store = createStore(() => { });
+const store = createStore(rootReducer);
 
 export default store;
