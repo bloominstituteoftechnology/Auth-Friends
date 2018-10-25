7219e858871303cd205d525860defe79669d6ed4
ssmodish <smodish@hotmail.com>
2018-10-25 16:50:00 -0400

    following the pattern: views

---
+++ friends/src/views/FriendsListView.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { FriendsList } from '../components';
+import { fetchFriends } from '../actions';
+
+class FriendsListView extends React.Component {
+  render() {
+    return(
+      if (this.props.isFetching) {
+        console.log('fetching');
+        return <div className='loader'>Loading...</div>;
+      }
+      return (
+        <div className='FriendsList_wrapper'>
+          <FriendsList friends={this.props.friends} />
+        </div>
+      )
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    friends: state.friendsReducer.friends,
+    isFetching: state.friendsReducer.isFetching
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchFriends }
+)(FriendsListView);
