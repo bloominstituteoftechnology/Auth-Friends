34a97e51b1443b792b50580652797166c842164c
Matthew Wright <matthewbwright@gmail.com>
2018-10-25 12:11:15 -0700

    adds initialState

--- friends/src/store/actions/index.js
+++ friends/src/store/actions/index.js
@@ -1,1 +1,14 @@
 import
+
+const initialState = {
+  friends: [],
+  fetchingFriends: false,
+  friendsFetched: false, 
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  frienDeleted: false,
+  error: null
+}
