11020eacb7079d4e986e06b4e8905bd857aa0920
Matthew Wright <matthewbwright@gmail.com>
2018-10-25 14:54:45 -0700

    adds Friends Form

--- friends/src/Views/FriendsForm.js
+++ friends/src/Views/FriendsForm.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
-class FriendsForm extends React.Component {
-  render() {
-    return <div>FRIENDSFORM</div>;
-  }
+function FriendsForm(props) {
+  return (
+    <form className="">
+      <input
+        className="friend-input"
+        name="name"
+        type="text"
+        placeholder="Friend's name"
+        value={props.input.name}
+        onChange={props.handleChange}
+      />
+      <input
+        className="friend-input"
+        name="age"
+        type="text"
+        placeholder="Friend's age"
+        value={props.input.age}
+        onChange={props.handleChange}
+      />
+      <input
+        className="friend-input"
+        name="email"
+        type="text"
+        placeholder="Friend's email"
+        value={props.input.email}
+        onChange={props.handleChange}
+      />
+      <button
+        className="submit-button"
+        type="submit"
+        onClick={props.handleClick}
+      >
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default FriendsForm;
