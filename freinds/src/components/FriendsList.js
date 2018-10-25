a2a0e250d1d3833b9c5a112ce6d269086f43848b
Unknown <zachary.arney@gmail.com>
2018-10-25 18:01:46 -0500

    Set up and connected FriendsList to Redux

--- freinds/src/components/FriendsList.js
+++ freinds/src/components/FriendsList.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { fetchFriends } from '../actions';
+import { connect } from 'react-redux';
+import Friend from './Friend';
 
-export class FriendsListView extends Component {
+export class FriendsList extends Component {
+  componentDidMount() {
+    this.props.fetchFriends();
+  }
   render() {
+    if (this.props.fetchingFriends) {
+      return <h3>I have friends I swear...</h3>;
+    }
     return (
       <div>
-        
+        {this.props.friends.map((friend) => {
+          return <Friend key={friend.id} friend={friend} />;
+        })}
       </div>
-    )
+    );
   }
 }
 
-export default FriendsListView
+const mapStateToProps = (state) => {
+  console.log('MAPPINGSTATE', state);
+  return {
+    fetchingFriends: state.friendsReducer.fetchingFriends,
+    friendsFetched: state.friendsReducer.friendsFetched,
+    friends: state.friendsReducer.friends,
+    error: state.friendsReducer.error,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchFriends }
+)(FriendsList);
