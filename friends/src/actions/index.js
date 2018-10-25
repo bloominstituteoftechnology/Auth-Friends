676bb0d3f5c6493c6b9e8e83d1cdcc6ca81eae23
Steven O'Neill <oneillsteven957@gmail.com>
2018-10-25 18:36:39 -0500

    More updates to CSS

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -21,7 +21,6 @@ export const fetchfriends = () => dispatch => {
     });
 };
 
-
 export const createFriends = friend =>{
     const newFriend = axios.post(`http://localhost:5000/api/friends`, friend); 
     return dispatch => {
--- friends/src/components/SelectedFriend.js
+++ friends/src/components/SelectedFriend.js
@@ -4,10 +4,10 @@ import '../App.css';
 export default function SelectedFriend(props) {
   return (
     <div className="selected-friend">
+      <button className="close-button" onClick={() => props.handleShowFriend({})}>X</button>
       <h4>{props.selected.name}</h4>
-      <button onClick={() => props.handleShowFriend({})}>X</button>
       <div>{props.selected.email}</div>
-      <div>{props.selected.age}</div>
+      <div className="selected-age">{props.selected.age}</div>
       <button onClick={() => props.toggleShowUpdate()}>
         Update Friend
       </button>
