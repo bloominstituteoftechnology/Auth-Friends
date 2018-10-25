30bb3f84a8bd4b0d546a95eba805d1804df7950b
KHollobaugh <khollobaugh@gmail.com>
2018-10-25 17:30:35 -0400

    added provider and middleware

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import friendReducer from './reducers/friendReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(friendReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
 
 
