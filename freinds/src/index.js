430d824058ab7954952989002537fb5289a7d4f2
Unknown <zachary.arney@gmail.com>
2018-10-25 18:00:30 -0500

    Set up reducers continued

--- freinds/src/index.js
+++ freinds/src/index.js
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import './index.css';
 import App from './App';
+import combineReducers from './reducers'
 import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers =
