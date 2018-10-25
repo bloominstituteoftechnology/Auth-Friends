86a82454905708bfdcba29d2d9b411aec6092336
Mindysueasaurus <mymindyyeager@gmail.com>
2018-10-25 16:01:40 -0500

    changed app to just import friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,36 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
 import  Friends  from './components/Friends';
-
-import { getFriends } from './actions';
-
 import './App.css';
 
 class App extends Component {
-
-  componentDidMount() {
-    this.props.getFriends();
-  }
   render() {
-    if (this.props.fetching) {
-      return <h1>Getting Friends, Please wait</h1>
-     }
-     return (
-       <div >
-         <Friends friends={this.props.friends} />
-       </div>
-     );
+    return (
+      <div className="App">
+        <Friends />
+      </div>
+    )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    friends: state.friendReducer.friends
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  { getFriends }
-)(App);
+export default App;
