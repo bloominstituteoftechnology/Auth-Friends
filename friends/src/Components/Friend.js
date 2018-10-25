f07b62ff988e0891d5099c87cb4ec32d75eece85
Gannon <gkleblanc@gmail.com>
2018-10-25 16:39:12 -0400

    worked on the components and finished hashing out my file structure based on the last assignment

---
+++ friends/src/Components/Friend.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Friend = props => {
+  return (
+    <div className="friend-card">
+      <h3>{props.friend.name}</h3>
+      <p>{props.friend.age}</p>
+      <p>{props.friend.email}</p>
+    </div>
+  );
+};
+export default Friend;
---
+++ friends/src/Components/FriendList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Friend from "./Friend";
+const FriendList = props => {
+  return (
+    <div className="friend-list">
+      {props.friends.map(friend => {
+        return <Friend key={friend.id} friend={friend} />;
+      })}
+    </div>
+  );
+};
+export default FriendList;
