7e76f15c555f9cfe824fffb6681f511ca81710a6
Unknown <zachary.arney@gmail.com>
2018-10-26 01:03:53 -0500

    Imported CreateFriendForm into App

--- freinds/src/App.js
+++ freinds/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import FriendsList from './components/FriendsList';
+import CreateFriendForm from './components/CreateFriendForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CreateFriendForm />
         <FriendsList />
       </div>
     );
