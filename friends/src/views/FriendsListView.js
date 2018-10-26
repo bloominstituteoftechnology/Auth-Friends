83b24bab286b317cda69b4321732fef4424c0056
Yanrong Xu <xuyanrong@icloud.com>
2018-10-25 23:47:05 -0400

    Componts structured

---
+++ friends/src/views/FriendsListView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { FriendsList } from '../components';
+
+class FriendsListView extends Component {
+    render() {
+        return (
+            <FriendsList />
+        );
+    }
+}
+
+ export default FriendsListView;
