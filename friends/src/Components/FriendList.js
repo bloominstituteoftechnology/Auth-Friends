b4436f917b98587da9148ede9c6c151e4ef3c055
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-11-01 18:22:54 -0400

    Components

---
+++ friends/src/Components/FriendList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetch } from '../Actions';
+import Friend from './Friend';
+
+class FriendList extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    if (this.props.fetchingFriends) {
+      return <div className='loading'>Loading Friends...</div>
+    }
+    return (
+      <div className='friend-list'>Friend list
+      {this.props.friends.map(friend => {
+        return(
+          <Friend key={friend.id} friend={friend}/>
+        )
+      })}
+      </div>
+    )
+  }
+
+}
+
+const mapStateToProps = state => {
+  return {
+    friends: state.friends,
+    fetchingFriends: state.fetchingFriends
+  }
+}
+
+export default connect(mapStateToProps, { fetch })(FriendList);
