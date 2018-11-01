2238bfceb41bd907751dad76fb98654630ae06e5
mikaelacurrier <mikaela.currier@gmail.com>
2018-11-01 17:46:49 -0500

    forgot to commit, but mvp and stretch done

---
+++ friends/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import  thunk  from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
