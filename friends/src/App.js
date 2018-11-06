7fe1541e95472c4adf3f3d2e9da91b34142689b7
Robert Billington <chillingtonbillington@gmail.com>
2018-11-06 11:52:40 -0600

    Robert Billington, FSWPT2, Redux Friends, PM @ John Spraul. Hi John. Added middleware, and dependencies to index.js

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
