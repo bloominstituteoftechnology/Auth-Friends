6177bc58235161369ee57fd1a3023d04812ccfae
Rick Fleming <elfeming111@yahoo.com>
2018-11-06 21:19:43 -0700

    Added imports for redux in index.js

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+
+
 import './index.css';
 import App from './App';
 
