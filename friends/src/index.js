733830f5b403de54db5421050e10f4195b1fd044
Julie Jonak <juliejonak10@gmail.com>
2018-11-08 00:25:07 -0600

    Creating boilerplate

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Reducer from './Reducers/Reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middleware = applyMiddleware(thunk);
+const store = createStore(Reducer, middleware);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, document.getElementById('root')
+);
+
