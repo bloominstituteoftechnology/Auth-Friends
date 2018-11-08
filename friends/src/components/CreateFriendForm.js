d8d2c605c183b3f48a553089ed153a2172d5c5d7
Colin Dismuke <colin.dismuke@gmail.com>
2018-11-07 22:31:30 -0600

    Added CreateFriendForm component

--- friends/src/components/CreateFriendForm.js
+++ friends/src/components/CreateFriendForm.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
+import { connect } from 'react-redux';
 
-const CreateFriendForm = props => {
-  return <div></div>;
+import { createFriend } from '../actions';
+
+class CreateFriendForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: '',
+      email: '',
+    }
+  }
+
+  inputHandler = (event) => {
+    this.setState({ [event.target.name ]: event.target.value })
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.createFriend(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.submitHandler}>
+        <input type="text" name="name" value={this.state.name} onChange={this.inputHandler} placeholder=" Name" />
+        <input type="text" name="age" value={this.state.age} onChange={this.inputHandler} placeholder=" Age" />
+        <input type="text" name="email" value={this.state.email} onChange={this.inputHandler} placeholder=" Email" />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
 };
 
-export default CreateFriendForm;
+const mapStateToProps = state => {
+  return {}
+}
+
+export default connect(mapStateToProps, { createFriend: createFriend })(CreateFriendForm);
