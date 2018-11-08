0737d281a019b761acbfc6df1dc67493178821dc
iepoch <staclem@live.com>
2018-11-07 21:49:20 -0700

    Added a friend page

---
+++ friends/src/components/AddFriend.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AddFriend extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return;
+  }
+}
+
+export default AddFriend;
