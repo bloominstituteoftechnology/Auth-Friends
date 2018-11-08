753b88f5e9c14ea78cc9fc4314925cb942e37e4c
Jennifer King <jenbellpgh@gmail.com>
2018-11-08 01:43:18 -0500

    Added ability to add friends

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -3,15 +3,30 @@ export const LOADING  = 'LOADING';
 export const UPDATING = 'UPDATING';
 export const ERROR    = 'ERROR';
 
+// GET data from API to populate store:
 export const getFriends = () => {
   return (dispatch) => {
     dispatch({ type: LOADING })
     axios.get('http://localhost:5000/api/friends')
-      .then( response => {
+      .then( (response) => {
         dispatch({ type: UPDATING, friends: response.data })
       })
-      .catch( err => {
+      .catch( (error) => {
         dispatch({ type: ERROR, errorMsg: "Cannot get friends. Try again."})
+      });
+  };
+};
+
+// POST needed to add a new friend:
+export const addFriend = (friend) => {
+  return (dispatch) => {
+    dispatch( {type: LOADING })
+    axios.post('http://localhost:5000/api/friends', friend)
+      .then( (response) => {
+        dispatch( {type: UPDATING, friends: response.data} )
+      })
+      .catch( (error) => {
+        dispatch( {type: ERROR, errorMsg: "Error adding new friend."} )
       });
   };
 };
--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { addFriend } from '../actions';
 
 class AddFriend extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
-      age: 0,
+      age: '',
       email: ''
     };
   };
 
+  // Take the text typed into the input form & add to local state
+  handleInput = (event) => {
+    this.setState( {[event.target.name]: event.target.value} );
+  };
+
+  // Submit the form
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addFriend(this.state);
+    this.setState({
+      name: '',
+      age: '',
+      email: ''
+    });
+  };
+
   render(){
     return(
-      <div>Friend Form</div>
+      <form onSubmit={this.handleSubmit}>
+        <input name='name' value={this.state.name} onChange={this.handleInput} placeholder='Name' />
+        <input name='age' value={this.state.age} onChange={this.handleInput} placeholder='Age' />
+        <input name='email' value={this.state.email} onChange={this.handleInput} placeholder='Email' />
+        <button type='submit'>Add Friend</button>
+      </form>
     );
   }
 
 };
 
-export default AddFriend;
+const mapStateToProps = (state) => {
+  return {};
+};
+
+export default connect(mapStateToProps, {addFriend: addFriend})(AddFriend);
