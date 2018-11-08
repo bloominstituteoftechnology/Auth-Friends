65e0fab6dd7567567e4cff273c29b58cb1e2fa8c
Colin Dismuke <colin.dismuke@gmail.com>
2018-11-07 22:44:29 -0600

    Added Friends component to display list of friends

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux';
 
-const Friends = props => {
-  return <div></div>;
-};
+import { getFriends } from '../actions';
+import Friend from './Friend';
 
-export default Friends;
+class Friends extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.getFriends();
+  }
+
+  render() {
+    return (
+      <div>
+        { this.props.fetchingFriends ? <h2>LOADING...</h2> : null }
+        { this.props.error !== '' ? <h2>{this.props.error}</h2> : null }
+        { this.props.friends.map( friend => <Friend friend={friend} key={friend.id} />)}
+      </div>
+    )
+  }
+
+}
+
+const mapStateToProps = state => {
+  return {
+    friends: state.friends,
+    fetchingFriends: state.fetchingFriends,
+    error: state.error,
+  }
+}
+
+export default connect(mapStateToProps)(Friends);
