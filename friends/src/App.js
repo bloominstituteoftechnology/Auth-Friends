64be11e472616adb0bf9ae3514ecc1100f8cc088
Eric Fleming <efleming111@yahoo.com>
2018-11-08 05:47:04 -0700

    Refactored app moving initial db loading to friends list

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
-import {fetchFriends} from './actions/actions';
-import { connect } from "react-redux";
+import React from 'react';
 
 import './App.css';
 import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriend';
 
 
-class App extends Component {
-  componentDidMount(){
-    this.props.fetchFriends();
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>Friends List</h1>
-        <AddFriend/>
-        <FriendsList/>
-        {
-          // *Create Delete Component
-          // *Create Update Friend Component
-        }
-      </div>
-    );
-  }
+const App = (props)=>{
+  return (
+    <div className="App">
+      <h1>Friends List</h1>
+      <AddFriend/>
+      <FriendsList/>
+      {
+        // *Create Delete Component
+        // *Create Update Friend Component
+      }
+    </div>
+  );
 }
 
-export default connect(()=>({}), {fetchFriends})(App);
+export default App;
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import {fetchFriends} from '../actions/actions';
 import FriendCard from './FriendCard';
 
-const FriendsList = (props)=>{
-    return(
-        <div>
-            {
-                props.fetching ?
-                <div>Loading Friend Data...</div> :
-                <ul>
-                    {props.friends.map(friend=><FriendCard key={friend.id} friend={friend}/>)}
-                </ul>
-            }
-        </div>
-    )
+class FriendsList extends React.Component{
+
+    componentDidMount(){
+        this.props.fetchFriends();
+    }
+
+    render(){
+        const {friends, fetching} = this.props
+        return(
+            <div>
+                {
+                    fetching ?
+                    <div>Loading Friend Data...</div> :
+                    <ul>
+                        {friends.map(friend=><FriendCard key={friend.id} friend={friend}/>)}
+                    </ul>
+                }
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state)=>{
@@ -24,4 +33,4 @@ const mapStateToProps = (state)=>{
     }
 }
 
-export default connect(mapStateToProps)(FriendsList);
+export default connect(mapStateToProps, {fetchFriends})(FriendsList);
