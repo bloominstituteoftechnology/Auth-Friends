dcca9fb68c80b01c4636487d258427ea2c90ab82
iepoch <staclem@live.com>
2018-11-07 21:42:33 -0700

    Basic Skelton Structure

---
+++ friends/src/components/UpdateFriend.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+
+class UpdateFriend extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return
+    }
+}
+
+
+
+
+export default UpdateFriend
