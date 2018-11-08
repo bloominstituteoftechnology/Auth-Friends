38be75e2ed7578df605cd17c15f3a6fae5b4c70f
Robert Billington <chillingtonbillington@gmail.com>
2018-11-08 13:22:26 -0600

    Building components

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import Friends from './Components/Friends';
+import NewFriendForm from './Components/NewFriendForm';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-       
+        <NewFriendForm />
+        <Friends />
       </div>
     );
   }
