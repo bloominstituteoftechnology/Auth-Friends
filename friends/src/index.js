58a445de2e33628ac5e41f07d993aaf487715af4
Emmanium <pradoe.ep@gmail.com>
2018-11-07 21:42:52 -0700

    setup boilerplate for project

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(reducer, middleware);
+const root = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, root);
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,19 @@
+const initialState = {
+  fetchingFriends: false,
+  friendsFetched: false,
+  friendsSaved: false,
+  savingFriends: false,
+  updatingFriend: false,
+  friendUpdated: false,
+  deletingFriend: false,
+  friendDeleted: false,
+  friends: [],
+  error: null
+}
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
