ae342b1e65c9fa325b7cc94e39f1e5f7bc2ad5f6
Ben <bennybooftw@gmail.com>
2018-11-08 22:12:18 -0500

    Setup reducers

---
+++ friends/src/reducers/reducers.jsx
@@ -0,0 +1,36 @@
+import {
+  FETCHING, 
+  FETCHING_SUCCESS, 
+  FETCHING_FAILURE, 
+  SAVING_FRIEND, 
+  SAVING_FRIEND_SUCCESS, 
+  SAVING_FRIEND_FAILURE
+} from "../actions/actions";
+
+const initialState = {
+  friends: [],
+  fetching: false,
+  saving: false,
+  error: null
+}
+
+export const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING:
+    return {...state, fetching:true};
+    case FETCHING_SUCCESS:
+    return {...state, fetching: false, friends: [...state.friends, ...action.payload]};
+    case FETCHING_FAILURE:
+    return {...state, fetching:false, error:action.payload};
+    
+    case SAVING_FRIEND:
+    return {...state, saving:true};
+    case SAVING_FRIEND_SUCCESS:
+    return {...state, saving:false, friends:[...action.payload]};
+    case SAVING_FRIEND_FAILURE:
+    return {...state, saving:false, error:action.payload};
+    
+    default:
+    return state;
+  }
+}
