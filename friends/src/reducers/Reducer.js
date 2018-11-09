672f1a235b78f55ba0e09a6902aee216f5a24eec
Rob Salzberg <robsalzberg@gmail.com>
2018-11-08 20:55:41 -0500

    Created Redux boilerplate

---
+++ friends/src/reducers/Reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    isGood: false,
+}
+export default function (state = initialState, action) {
+        switch(action.type) {
+             default:
+               return state;
+        }
+}
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import Reducer from './Reducer';
+export default combineReducers({
+                reduce: Reducer
+});
