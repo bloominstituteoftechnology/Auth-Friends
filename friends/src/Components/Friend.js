25a87be37e3e2e9ab335df06a40d55389b9063c7
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-11-08 21:07:25 -0700

    Reducer is made

--- friends/src/Components/Friend.js
+++ friends/src/Components/Friend.js
@@ -1,11 +1,14 @@
 import React,{Component} from 'react'
+import '../App.css'
 
 const Friend = (props) => {
 
     return (
-        <div>
+        <div className='friend-format'>
             <h3> {props.name} </h3>
-            <h3> {props.}
+            <h3> {props.age} </h3>
+            <h3> {props.email} </h3>
+
         </div>
     )
 }
--- friends/src/index.js
+++ friends/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux;'
+import thunk from 'redux-thunk';
+
+const middleware=applyMiddleware(thunk)
+
+const store = createStore(friendReducer,middleware)
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
