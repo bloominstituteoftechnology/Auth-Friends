5595f27b8f504985aa775f7aeb0a41bd67479fc1
FreeKeyBoy <jjhutton81@gmail.com>
2018-11-08 19:06:30 -0800

    Created component files FriendsList.js and Update Friends.js, and started the components boilerplate. Wrapped App in Router in index.js.

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+ class FriendsList extends Component {
+  constructor(props) {
+    super(props);
+  }
+   render() {
+      return (
+        <div>No Friends</div>
+    )
+  }
+}
+ export default FriendsList;
---
+++ friends/src/components/UpdateFriend.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class UpdateFriend extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+     render() {
+        return
+    }
+}
+
+ export default UpdateFriend 
