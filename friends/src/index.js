b8ba062fdcdc22402bc3bcbaf981cfbc8a949892
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-11-08 20:05:08 -0700

    index.js worked on a bit.

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+
+import FriendReducer from './reducers';
+
+const middleware = applyMiddleware(thunk)
+const store = createStore(FriendReducer, middleware)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
