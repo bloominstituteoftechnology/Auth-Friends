4818b0306cd01827d0ac2010ccd3346c6409af6c
TheGameFreak720 <luis.alvarez@commithub.com>
2018-11-08 22:31:44 -0500

    set up FriendList component

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-export default () => (
-    <div>
-    
-    </div>
-);
+import { getFriends } from '../actions/friendsActions';
+import Friend from './Friend';
+
+class FriendList extends Component {
+    componentDidMount() {
+        this.props.getFriends()
+    }
+    render() {
+        <div>
+            { this.props.loading ? <h1>Loading</h1> : null },
+            { this.props.error !== '' ? <h1>{this.props.error}</h1> : null }
+            { this.props.friends.map( friend => <Friend friend={friend} key={friend.id}/> ) } 
+        </div>
+    }
+}
+
+const mapStateToProps = state => ({
+    friends: state.friends,
+    error: state.error,
+    loading: state.loading
+});
+
+export default connect(mapStateToProps, { getFriends })(FriendList);
