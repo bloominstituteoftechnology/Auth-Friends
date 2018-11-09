4e48b1fc469ce16861d36a81dd6ea88e37143184
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-11-08 18:40:32 -0700

    Set up provider and middleware createStore on index.js file

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+// needed dependancies
+// applyMiddleware from redux
+import { applyMiddleware } from 'redux';
+// thunk from redux-thunk
+import thunk from 'redux-thunk'
+// logger from redux-logger
+import logger from 'redux-logger'
+import rootReducer from './reducers/reducers.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middleware = applyMiddleware(logger, thunk);
+
+const store = createStore(
+    rootReducer,
+    middleware
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
