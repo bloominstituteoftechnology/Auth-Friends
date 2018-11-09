7a2a1ca5f086995eec030176ea51972ead7cd07a
kammagik <kammagik@yahoo.com>
2018-11-08 20:11:05 -0800

    update

--- friends/src/components/CreateFriendForm.js
+++ friends/src/components/CreateFriendForm.js
@@ -6,7 +6,7 @@ class CreateFriend extends Component {
     constructor(props){
         super(props)
         this.state = {
-            id: ''
+            id: '',
             name: '',
             age: '',
             email: '',
@@ -23,10 +23,20 @@ class CreateFriend extends Component {
     }
 
     render(){
+        return(
         <form onSubmit= {this.submitHandler}>
-            <input type="number" name="id" value={this.state.id} onChange={this.inputHandler} placeholder="ID">
-            <input type="text" name="name" value={this.state.name} onChange={this.inputHandler} placeholder="NAME">
-            <input type="number" name="age" value={this.state.age} onChange={this.inputHandler} placeholder="AGE">
-            <input type="text" name="email" value={this.state.name} onChange={this.inputHandler} placeholder="NAME">
+            <input type="number" name="id" value={this.state.id} onChange={this.inputHandler} placeholder="ID"/>
+            <input type="text" name="name" value={this.state.name} onChange={this.inputHandler} placeholder="NAME"/>
+            <input type="number" name="age" value={this.state.age} onChange={this.inputHandler} placeholder="AGE"/>
+            <input type="text" name="email" value={this.state.name} onChange={this.inputHandler} placeholder="NAME"/>
+            <button type = "submit">ADD</button>
+        </form>
+        )
     }
 }
+ 
+    const mapStateToProps = state => {
+        return{}
+    }
+
+export default connect(mapStateToProps, { createFriend: createFriend})(CreateFriend)
