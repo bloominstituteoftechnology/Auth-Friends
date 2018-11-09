83da390f201e4ee23c70e5671f6365d2a1bb18b5
paulapivat <paul.apivat@gmail.com>
2018-11-09 16:56:07 +0700

    fetch friends data from server, render on page, minimal styling, in react

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 import Friends from './components/Friends'
 import CreateFriendForm from './components/CreateFriendForm'
 import UpdateFriendForm from './components/UpdateFriendForm'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        friends: [],
+    }
+  }
+
+  componentDidMount(){
+    axios
+    .get(`http://localhost:5000/api/friends`)
+    .then(response => {
+        this.setState({ friends: response.data })
+    })
+    .catch(err => {
+        console.log("Fail to GET Friends from data", err)
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -16,7 +37,7 @@ class App extends Component {
             Friends in React-Redux.
           </p>
           <CreateFriendForm />
-          <Friends /> 
+          <Friends friends={this.state.friends} /> 
           <UpdateFriendForm />
 
         </header>
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,14 +1,42 @@
 import React from 'react';
 
+import './Friends.css';
+
 class Friends extends React.Component {
     // constructor(){
     //     super();
+    //     this.state ={
+    //         friends: [],
+    //     }
     // }
+
+    // componentDidMount(){
+    //     axios
+    //     .get(`http://localhost:5000/api/friends`)
+    //     .then(response => {
+    //         this.setState({ friends: response.data })
+    //     })
+    //     .catch(err => {
+    //         console.log("Fail to GET Friends from data", err)
+    //     })
+    // }
+
     
+
     render(){
         return (
-            <div>
-                List of Friends here.
+            <div className="friend-container">
+               <h1> List of Friends here. </h1>
+                {this.props.friends.map(friend => {
+                    return (
+                        <div className="friend-card" key={friend.id}>
+                            <h3> Name: {friend.name} </h3>
+                            <p>Age: {friend.age} </p>
+                            <p>Email: {friend.email} </p>
+                            <p>ID: {friend.id} </p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
