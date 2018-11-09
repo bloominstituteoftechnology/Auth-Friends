4fd7aa07bb507fb41d802ed99497304f505f72ba
Mitchell Robles <mitchellr92@live.com>
2018-11-08 18:42:36 -0800

    added dependencies and added store

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import logger from 'react-logger';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    applyMiddleware(thunk, logger)
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
