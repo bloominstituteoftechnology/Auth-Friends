c0e9e9a6759dd16a8809b9c02fb9efd3219ef50e
stevekutz <stkutz@gmail.com>
2018-11-08 23:38:36 -0600

    built MyFriendsList component

--- friends/src/components/MyFriendsList.js
+++ friends/src/components/MyFriendsList.js
@@ -1,1 +1,54 @@
 import React from 'react';
+import {fetch} from "../actions/action";
+import {connect} from 'react-redux';
+
+class MyFriendsList extends React.Component {
+  constructor() {           // may generate useless constructor warning !!!
+    super()
+  }
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+
+  render() {
+
+    if(this.props.error !== null) {
+      return <h2> {this.props.error}</h2>
+    }
+
+    if(this.props.fetching) {
+      console.log('locating friends...');
+      return <h3> Looking for my redux friends...</h3>
+
+    }
+
+
+    return (
+      <div className = 'MyFriendsList-container'>
+        <MyFriendsList friends = {this.props.friends}/>
+      </div>
+
+    )
+
+  }
+
+}
+
+const mapStateToProps = (state) => {
+
+
+  return{
+    friends: state.reduxFriends.friends,
+    fetching: state.reduxFriends.fetching,
+    fetched: state.reduxFriends.fetched,
+    error: state.reduxFriends.error
+
+  }
+
+
+
+
+
+};
