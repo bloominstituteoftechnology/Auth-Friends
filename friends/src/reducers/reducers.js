ddb02541912cfb305f58415f89be71361d36a0e8
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-11-08 18:46:16 -0700

    Set up reducer based on project instructions

---
+++ friends/src/reducers/reducers.js
@@ -0,0 +1,18 @@
+const initialState = {
+    fetchingFriends: false,
+    friendsFetched: false,
+    friendsSaved: false,
+    savingFriends: false,
+    updatingFriend: false,
+    friendUpdated: false,
+    deletingFriend: false,
+    friendDeleted: false,
+    friends: [],
+    error: null
+}
+
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+
+export default rootReducer;
