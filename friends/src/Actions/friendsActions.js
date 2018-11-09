7d7ad043e11aa96c84da9f72b79e0be20d04ec00
Denise Escobar <descobar1490@gmail.com>
2018-11-08 21:55:30 -0600

    Created my first action to get friends

---
+++ friends/src/Actions/friendsActions.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+export const GET_FRIENDS = "GET_FRIENDS"
+export const LOADING = "LOADING"
+export const ERROR = "ERROR"
+
+export const getFriends = () =>{
+    return(dispatch) =>{
+        dispatch({type : LOADING})
+        axios
+            .get('http://localhost:5000/friends')
+            .then(response => {
+                dispatch({type: GET_FRIENDS})
+            })
+            .catch( err => {
+                dispatch({type: Error, errorMessage: "Can't get your friends :("})
+            })
+    }
+}
