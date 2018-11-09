324cc1dd3b1e03a8bdd4e61d1af0f770e37321d2
Rob Salzberg <robsalzberg@gmail.com>
2018-11-09 00:11:08 -0500

    Added Redux Dev Tool Extension Support

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,20 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./store/reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './store/reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+	rootReducer,
+	composeEnhancer(applyMiddleware(thunk, logger)),
+  );
 
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
 	</Provider>,
-	document.getElementById("root")
+	document.getElementById('root')
 );
 registerServiceWorker();
