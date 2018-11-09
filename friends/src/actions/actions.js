9878fd3c44dfa4485cd82a25dc910a44d46aae53
Bruce Cabanayan <bcabanayan@Unknown.domain>
2018-11-08 19:55:25 -0700

    Built out FriendsList component to render list of friends

---
+++ friends/src/actions/actions.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const LOADING = 'LOADING';
+export const GET_FRIENDS = 'GET_FRIENDS';
+export const ERROR = 'ERROR'
+
+export const getFriends = () => {
+    return (dispatch) => {
+        dispatch({type: LOADING});
+        axios
+            .get(`http://localhost:5000/api/friends`)
+            .then(response => {
+                dispatch({type: GET_FRIENDS, friends: response.data})
+            })
+            .catch(err => {
+                dispatch({type: Error, errorMessage: "We had some trouble finding your friends."})
+            })
+        }
+    }
+}
