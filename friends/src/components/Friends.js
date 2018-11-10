2a83e3fa035e090826c5c06745058eb5d27d0150
paulapivat <paul.apivat@gmail.com>
2018-11-10 19:31:23 +0700

    refactor Friends.js

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -4,12 +4,12 @@ import FriendPage from './FriendPage';
 import './Friends.css';
 
 class Friends extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            id: ''
-        }
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         id: ''
+    //     }
+    // }
 
     
 
@@ -36,10 +36,3 @@ class Friends extends React.Component {
 
 export default Friends
 
-                        /* <div className="friend-card" key={friend.id}>
-                            <h3> Name: {friend.name} </h3>
-                            <p>Age: {friend.age} </p>
-                            <p>Email: {friend.email} </p>
-                            <p>ID: {friend.id} </p>
-                            <div style={{cursor: "pointer"}} onClick={this.handleDelete}> X </div>
-                        </div> */
