6461c58d7e489377dd5ed582494f132421d69db0
Alex Martin <atonymartin20@yahoo.com>
2018-11-10 09:10:19 -0600

    Completed index.js main file and started building reducers.

---
+++ friends/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const ERROR = 'ERROR';
+export const FETCHINGFRIENDS = 'FETCHINGFRIENDS';
+export const
+
+fetchingFriends: false,
+friendsFetched: false,
+friendsSaved: false,
+savingFriends: false,
+updatingFriend: false,
+friendUpdated: false,
+deletingFriend: false,
+friendDeleted: false,
+friends: [],
+error: null
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { friendsReducer } from './friendsReducer';
+
+export default combineReducers({
+    friendsReducer
+});
