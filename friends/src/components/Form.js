1e32b04bf4ef89515f0439bd4d8587cb2748b07f
vy3191 <venkyafhs@gmail.com>
2018-11-11 22:39:56 -0500

    Added form component styled form component

--- friends/src/components/Form.js
+++ friends/src/components/Form.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 
-
 class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            name: '',
+            email:'',
+            age:''
+        }
+    }
+
+    inputHandler = (event) => {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
-                
+                <form>
+                    <input type="text" name="name"
+                           value={this.state.name}
+                           placeholder="Name"
+                           onClick={this.props.inputHandler} />
+                    <input type="text" name="email"
+                           value={this.state.email}
+                           placeholder="Email"
+                           onClick={this.props.inputHandler} />  
+                    <input type="text" name="age"
+                           value={this.state.age}
+                           placeholder="Age"
+                           onClick={this.props.inputHandler} /> 
+                    <input type="submit" value="Add" />           
+                </form>   
             </div>
         );
     }
 }
 
 export default Form;
+
--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
 const Friend = (props) => {
     return(
-        <div>
-            <h3>{props.friend.name}</h3>
-            {/* <h2>this is working now...</h2> */}
+        <div className='friend'>
+            <h1>{props.friend.name}'s details:</h1>
+            <h2><span>Name:</span>{" "}{props.friend.name}</h2>
+            <h2><span>Email:</span>{" "}{props.friend.email}</h2>
+            <h2><span>Age:</span>{" "}{props.friend.age}</h2>
         </div>
     )
 }
