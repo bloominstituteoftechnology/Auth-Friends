549d32f6b271e23c7ce8656777449110a93e83f9
jbierly1 <james.bierly@gmail.com>
2018-11-17 12:33:23 -0500

    made index page

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from "./reducers";
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    rootReducer,applyMiddleware(thunk, logger)
+  );
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
