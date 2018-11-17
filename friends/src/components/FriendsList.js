7422fe410e23445ea2d699ea00afc7a957d14e7e
AlexOstrander <alexanderostrander23@gmail.com>
2018-11-17 13:10:02 -0500

    Added friends and imported all required packages. Created friendslist components, reducers and actions.

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { getFriends } from '../actions/actions.js'
+import Friend from './Friend.js' 
+
+class FriendsList extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount(){
+        this.props.getFriends()
+    }
+
+    render(){
+        return (
+            <div>
+                {this.props.fetchingFriends ? <h1>Loading friends...</h1> : null}
+                {this.props.error !== '' ? <h1>{this.props.error}</h1> : null }
+                {this.props.friends.map(friend => <Friend friend={friend} key={Math.random()}/>)}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        friends: state.friends,
+        error: state.error,
+        fetchingFriends: state.fetchingFriends
+    }
+}
+
+export default connect(mapStateToProps, {getFriends})(FriendsList)
---
+++ friends/src/index.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+// needed dependancies
+// applyMiddleware from redux
+import { applyMiddleware } from 'redux';
+// thunk from redux-thunk
+import thunk from 'redux-thunk'
+// logger from redux-logger
+import logger from 'redux-logger'
+import rootReducer from './reducers/reducers.js'
+
+const middleware = applyMiddleware(logger, thunk);
+
+const store = createStore(
+    rootReducer,
+    middleware
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
