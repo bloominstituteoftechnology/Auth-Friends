4d0aec8bab95f64abf2c6ed4267d4264cfce7fa6
Jesse Theisen <theiseja@gmail.com>
2018-11-17 12:15:31 -0600

    MVP: axios get added

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
+import FriendsList from "./Components/FriendsList";
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Here is a small list of my Friends!</h1>
+        <FriendsList />
+      
       </div>
     );
   }
 }
-
 export default App;
