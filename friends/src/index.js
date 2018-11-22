7a9706d282f5c460742e794f0dff1faf53ac827f
cocoitali <cmburatto@gmail.com>
2018-11-21 23:55:50 -0700

    MVP (added redux dev tools)

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
 import App from "./App";
-import  friendsReducer  from "./reducers";
+import friendsReducer from "./reducers";
 import "./index.css";
 
-const middleWare = applyMiddleware(logger, thunk)
-const store = createStore(friendsReducer, middleWare);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleWare = applyMiddleware(logger, thunk);
+const store = createStore(friendsReducer, composeEnhancers(middleWare));
 
 ReactDOM.render(
   <Provider store={store}>
