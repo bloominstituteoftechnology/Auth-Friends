383d8e328caef98277ecbf43eeee1c8db0921ef0
Jesse Theisen <theiseja@gmail.com>
2018-11-24 10:05:19 -0600

    Fixed the index.js issue I was having, was a simple spelling mistake. MVP should be complete

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
+import rootReducer from './Reducers';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
