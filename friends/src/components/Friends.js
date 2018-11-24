0a12d603248b6dccbe3d8e4e092e03f95a1c42d4
cocoitali <cmburatto@gmail.com>
2018-11-24 00:07:27 -0700

    CHANGE (made Friends and functional component, put all lifecycle methods, e listners, etc in App along with state)

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,43 +1,40 @@
-import React, {Component} from "react";
+import React from "react";
 import { connect } from "react-redux";
-import {fetchFriends} from "../actions";
+import { fetchFriends } from "../actions";
 
-class Friends extends Component {
-  componentDidMount() {
-    this.props.fetchFriends();
-  }
-  render() {
+const Friends = props => {
+ 
   return (
     <div>
-      {this.props.fetchingFriends ? (
+      {props.fetchingFriends ? (
         <div> ...fetching your friends. do you have friends? </div>
       ) : (
         <ul>
-          {this.props.friends.map(friend => {
-            return  <div key={friend.id}>
-            <h2>{friend.name}</h2>
-            <p>{friend.age}</p>
-            <p>{friend.email}</p>
-            </div>
+          {props.friends.map(friend => {
+            return (
+              <div key={friend.id}>
+                <h2>{friend.name}</h2>
+                <p>{friend.age}</p>
+                <p>{friend.email}</p>
+              </div>
+            );
           })}
         </ul>
-  )}
+      )}
     </div>
   );
 };
-}
-
 
 
 const mapStateToProps = state => {
-    return {
-      fetchingFriends: state.fetchingFriends,
-      friends: state.friends,
-      error: state.errorMessage
-    };
+  return {
+    fetchingFriends: state.fetchingFriends,
+    friends: state.friends,
+    error: state.errorMessage
   };
+};
 
 export default connect(
   mapStateToProps,
-  {fetchFriends}
+  { fetchFriends }
 )(Friends);
