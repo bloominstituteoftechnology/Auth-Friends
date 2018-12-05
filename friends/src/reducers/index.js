4199d473781472151578b18818bd80313d2444f0
ToMMaaY <thuynh629@gmail.com>
2018-12-05 15:39:48 -0800

    Created the initial version of my reducers

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,3 +1,37 @@
 import { combineReducers } from "redux";
+import { FETCHING, SUCCESS, FAILURE } from "../actions";
 
-export default combineReducers({});
+const initialState = {
+  friends: [],
+  isFetching: false,
+  error: null
+};
+
+const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING:
+      return {
+        ...state,
+        isFetching: true
+      };
+
+    case SUCCESS:
+      return {
+        ...state,
+        error: null,
+        isFetching: false,
+        friends: action.payload
+      };
+
+    case FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ reducers });
