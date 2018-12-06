d327bcb8c1b9c0e8c3707a4aa1d71c4a6c47d08d
Trai Lynne <trailynne@gmail.com>
2018-12-06 15:47:23 -0700

    Imported necessary dependecies - {applyMiddleware} from redux - thunk from redux-thunk - logger from redux-logger

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Redux Dependencies
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import rootReducer from './reducers';
 
