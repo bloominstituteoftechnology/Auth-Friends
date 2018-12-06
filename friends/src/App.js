649cfa6b160451f58477f5f068940b0aa7b8d3ba
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-12-05 22:52:44 -0500

    minor changes MVP done

--- friends/src/App.js
+++ friends/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Friends List:</h1>
+        <h1>Redux Friends List:</h1>
         <FriendForm />
         <Friends />
       </div>
