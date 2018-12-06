6df38b0685552e5c5ed458ebffaee2a9dd99db51
tryingtokeepup <kl.hawaii@gmail.com>
2018-12-06 13:42:47 -0800

    We are starting now, let's go hard Jon

---
+++ friends/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+// This is the redux stuff, whoot whoot
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore( function() {}, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+
+<Provider store = {store}><App /></Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
