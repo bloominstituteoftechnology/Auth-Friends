c3105eb2e15a6ed085eba738cd4d9f6085afe5b2
Trai Lynne <trailynne@gmail.com>
2018-12-06 15:43:30 -0700

    Definied initial statate

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,7 +1,22 @@
 // ActionType Exports
- // Initial State 
- // Reducer
- const reducer = (state, action) => {
+
+
+// Initial State 
+const initialState = {
+   fetchingFriends: false,
+   friendsFetched: false,
+   friendsSaved: false,
+   savingFriends: false,
+   updatingFriend: false,
+   friendUpdated: false,
+   deletingFriend: false,
+   friendDeleted: false,
+   friends: [],
+   error: null
+ }
+
+// Reducer
+ const reducer = (state = initialState, action) => {
     switch(action.type){
          default: 
             return state;
