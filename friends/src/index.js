52e4c61ab46dfbc7e0d93a3ae8b912da6752800c
Trai Lynne <trailynne@gmail.com>
2018-12-06 15:35:36 -0700

    Provided App access to store Passed store as a prop of Provider Wrapped app in Provider

--- friends/src/index.js
+++ friends/src/index.js
@@ -13,8 +13,14 @@ import * as serviceWorker from './serviceWorker';
 
 // Store Definition
 const store = createStore(rootReducer);
+const rootElement = document.getElementById('root')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={ store }>
+        <App/>    
+    </Provider>, 
+    rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
