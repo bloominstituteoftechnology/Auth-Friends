f00b7b5885eda102796c878120bbd36c32bc0cfb
kevinmarker1101 <kmarker1101@icloud.com>
2018-12-07 09:07:56 -0600

    cleanup

--- server.js
+++ server.js
@@ -7,36 +7,36 @@ const app = express();
 let nextId = 6;
 
 let friends = [
-  {
-    id: 1,
-    name: 'Ben',
-    age: 30,
-    email: 'ben@lambdaschool.com',
-  },
-  {
-    id: 2,
-    name: 'Austen',
-    age: 45,
-    email: 'austen@lambdaschool.com',
-  },
-  {
-    id: 3,
-    name: 'Ryan',
-    age: 15,
-    email: 'ryan@lambdaschool.com',
-  },
-  {
-    id: 4,
-    name: 'Sean',
-    age: 35,
-    email: 'sean@lambdaschool.com',
-  },
-  {
-    id: 5,
-    name: 'Michelle',
-    age: 67,
-    email: 'michelle@gmail.com',
-  },
+    {
+        id: 1,
+        name: 'Ben',
+        age: 30,
+        email: 'ben@lambdaschool.com',
+    },
+    {
+        id: 2,
+        name: 'Austen',
+        age: 45,
+        email: 'austen@lambdaschool.com',
+    },
+    {
+        id: 3,
+        name: 'Ryan',
+        age: 15,
+        email: 'ryan@lambdaschool.com',
+    },
+    {
+        id: 4,
+        name: 'Sean',
+        age: 35,
+        email: 'sean@lambdaschool.com',
+    },
+    {
+        id: 5,
+        name: 'Michelle',
+        age: 67,
+        email: 'michelle@gmail.com',
+    },
 ];
 
 app.use(bodyParser.json());
@@ -44,60 +44,60 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/api/friends', (req, res) => {
-  setTimeout(() => {
-    res.send(friends);
-  }, 1000);
+    setTimeout(() => {
+        res.send(friends);
+    }, 1000);
 });
 
 app.get('/api/friends/:id', (req, res) => {
-  const friend = friends.find(f => f.id == req.params.id);
+    const friend = friends.find(f => f.id == req.params.id);
 
-  if (friend) {
-    res.status(200).json(friend);
-  } else {
-    res.status(404).send({ msg: 'Friend not found' });
-  }
+    if (friend) {
+        res.status(200).json(friend);
+    } else {
+        res.status(404).send({msg: 'Friend not found'});
+    }
 });
 
 app.post('/api/friends', (req, res) => {
-  const friend = { id: getNextId(), ...req.body };
+    const friend = {id: getNextId(), ...req.body};
 
-  friends = [...friends, friend];
+    friends = [...friends, friend];
 
-  res.send(friends);
+    res.send(friends);
 });
 
 app.put('/api/friends/:id', (req, res) => {
-  const { id } = req.params;
+    const {id} = req.params;
 
-  const friendIndex = friends.findIndex(f => f.id == id);
+    const friendIndex = friends.findIndex(f => f.id == id);
 
-  if (friendIndex > -1) {
-    const friend = { ...friends[friendIndex], ...req.body };
+    if (friendIndex > -1) {
+        const friend = {...friends[friendIndex], ...req.body};
 
-    friends = [
-      ...friends.slice(0, friendIndex),
-      friend,
-      ...friends.slice(friendIndex + 1),
-    ];
-    res.send(friends);
-  } else {
-    res.status(404).send({ msg: 'Friend not found' });
-  }
+        friends = [
+            ...friends.slice(0, friendIndex),
+            friend,
+            ...friends.slice(friendIndex + 1),
+        ];
+        res.send(friends);
+    } else {
+        res.status(404).send({msg: 'Friend not found'});
+    }
 });
 
 app.delete('/api/friends/:id', (req, res) => {
-  const { id } = req.params;
+    const {id} = req.params;
 
-  friends = friends.filter(f => f.id !== Number(id));
+    friends = friends.filter(f => f.id !== Number(id));
 
-  res.send(friends);
+    res.send(friends);
 });
 
 function getNextId() {
-  return nextId++;
+    return nextId++;
 }
 
 app.listen(port, () => {
-  console.log(`server listening on port ${port}`);
+    console.log(`server listening on port ${port}`);
 });
