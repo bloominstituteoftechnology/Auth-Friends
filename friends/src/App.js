2509bfcb85089e9fbe749e249559efc753bb2e7a
1@$@1github <asaofdiamonds@gmail.com>
2018-12-12 16:52:59 -0700

    first commit

---
+++ friends/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">              
+        <h1>I hope I have friends I can add to this App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
