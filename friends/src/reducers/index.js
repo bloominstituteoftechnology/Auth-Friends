2f2c05df283f5dc4fb87db772e29b5c0cf59a5fe
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-02-06 09:05:10 -0500

    ADD SWITCH CASES FOR ACTION TYPES

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -9,6 +9,29 @@ const initialState = {
 export const mainReducer = (state = initialState, action) => {
     switch (action.type){
 
+        case LOADING:
+            return {
+                ...state,
+                friends: [],
+                loading: true,
+                error: ''
+            }
+        
+        case SUCCESS:
+            return {
+                ...state,
+                friends: action.payload,
+                loading: false,
+                error: ''
+            }
+
+        case FAILURE: 
+            return {
+                ...state,
+                friends: [],
+                loading: false,
+                error: action.payload
+            }
         default: 
             return state;
     }
