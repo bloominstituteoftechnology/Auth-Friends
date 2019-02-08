c6ef37973336b1b816b6af6ce9de619425ce73e7
Taylorbelk1 <belktaylor12@gmail.com>
2019-02-08 11:15:02 -0600

    applied ability to persist through reload with localstorage

--- friends/src/index.js
+++ friends/src/index.js
@@ -8,12 +8,16 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { loadState, saveState } from './localstorage';
 
 import {reducer} from './reducers/reducer';
 
-const store = createStore( reducer, applyMiddleware( thunk, logger ));
+const persistedState = loadState();
+const store = createStore( reducer, applyMiddleware( thunk, logger ), persistedState);
 
-
+store.subscribe(() => {
+    saveState(store.getState());
+  })
 
 ReactDOM.render(
 <Provider store={store}>
