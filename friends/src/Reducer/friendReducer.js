50ac16293201c8ec7ba4d5b22cb5e73c826fef82
Alando Appling <alandoappling@gmail.com>
2019-02-07 22:51:40 -0800

    Completed Reducer

---
+++ friends/src/Reducer/friendReducer.js
@@ -0,0 +1,82 @@
+import {
+    FETCH_FRIENDS,
+    FETCH_SUCCESS,
+    ADD_FRIEND,
+    ADD_SUCCESS,
+    DELETE_FRIENDS,
+    DELETE_SUCCESS,
+    UPDATE_FRIEND,
+    UPDATE_SUCCESS,
+    ERROR
+} from '../Actions';
+
+const initialState = {
+    friends: [],
+    loading: false,
+    error: null
+}
+
+export const friendReducer = (state = initialState, action) => {
+    switch(action.type){
+        case FETCH_FRIENDS:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            };
+        case FETCH_SUCCESS:
+            return {
+                ...state,
+                friends: action.payload,
+                loading: false,
+                error: null
+            }
+        case ADD_FRIEND:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            }
+        case ADD_SUCCESS:
+            return {
+                ...state,
+                friends: action.payload,
+                loading: false,
+                error: null
+            }
+        case DELETE_FRIENDS:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            }
+        case DELETE_SUCCESS:
+            return {
+                ...state,
+                friends: action.payload,
+                loading: false,
+                error: null
+            }
+        case UPDATE_FRIEND:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            }
+        case UPDATE_SUCCESS:
+            return {
+                ...state,
+                friends: action.payload,
+                loading: false,
+                error: null
+            }
+        case ERROR:
+            return {
+                ...state,
+                error: action.payload
+            }
+        default:
+            return state;
+
+    }
+}
