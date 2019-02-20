546f6a74872d383db516297d01bf822ac1c9edcc
thomasutsey <tomutsey@yahoo.com>
2019-02-19 23:30:54 -0500

    Initialize friends project
    fork and clone project
    
    create branch
    
    add colaborator
    
    import dependencies
    
    create store
    
    wrap app with provider
    
    create preliminary frame work for project
    
    amend yarn install and start on forked folder
    
    create react app inside folder
    
    add dependecies
    
    yarn start

---
+++ friends/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import friendsreducer from "./reducers/friendsreducer";
+
+const store = createStore(
+  rootReducer, 
+  applyMiddleware (thunk, logger)
+);
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+  </Provider>,
+   document.getElementById('root'));
+
+
