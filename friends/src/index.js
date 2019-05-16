ec0fb5cb3fa0b450942f00c88bbfa950ea22c967
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-16 16:15:20 -0700

    entry index

---
+++ friends/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, logger)
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
