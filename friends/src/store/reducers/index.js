36c6ddb5796c9126c3f7143c197f1bb3968cba63
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-16 16:00:36 -0700

    basic reducer

--- friends/src/store/reducers/index.js
+++ friends/src/store/reducers/index.js
@@ -1,1 +1,33 @@
-import 
+import { FETCHING, SUCCESS, FAILURE } from '../actions';
+
+const initialState = {
+  friends: [],
+  isLoading: false,
+  error: ''
+};
+
+export const getFriends = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING:
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: '',
+        friends: action.payload
+      };
+    case FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
