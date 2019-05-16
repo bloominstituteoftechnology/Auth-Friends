7826e3045aba7e4f54d185968fd9eb633b074774
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-16 15:28:16 -0700

    basic action done

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,12 +4,16 @@ export const FETCHING = 'FETCHING';
 export const SUCCESS = 'SUCCESS';
 export const FAILURE = 'FAILURE';
 
-export const getFriends = () => {
+export const getFriends = () => dispatch => {
     console.log(`--------------Fetching getFriends()`)
     dispatch ({ type: FETCHING });
     axios
     .get(`http://localhost:5000/api/friends`)
     .then(res => {
-        dispatch({  })
+        dispatch({ type: SUCCESS, payload: res.data.results });
+    })
+    .catch(err => {
+        console.log(err);
+        dispatch({ type: FAILURE, payload: err })
     })
 }
