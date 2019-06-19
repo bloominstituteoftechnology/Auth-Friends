ef0e3541f6a669e8cc265a539c0a1ca1969e920e
alexisjcarr <acarr59@gmail.com>
2019-06-19 03:17:49 -0500

    Initial commit. Boilerplate cleaned up. Necessary dependencies added and hooked up.

---
+++ friends/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+
+import "./index.scss";
+import App from "./App";
+import { rootReducer } from "./store/reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
