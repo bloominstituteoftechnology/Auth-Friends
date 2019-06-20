e1346f892a78ecc2b9b3082fcaa2f0106706da97
unknown <samantha.c.egge@gmail.com>
2019-06-20 14:29:02 -0700

    Did initial work on Friends page. Login is receiving token. Friends not displaying list.

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import { getData } from '../actions'
 
 class Friends extends React.Component {
+    componentDidMount() {
+        this.props.getData();
+    }
+
     render () {
+        // if (this.props.fetchingData) {
+        //     <h2>Fetching friends list . . .</h2>
+        //   }
         return (
-            <h1>Test</h1>
+            <div className='friends-list'>
+                {this.props.friends.map(friend => {
+                return (
+                  <div className='friend-item' key={friend.id}>
+                      <h1>{friend.name}</h1>
+                      <p>{friend.age}</p>
+                      <p>{friend.email}</p>
+                  </div>
+                )
+            })}
+          </div>
         )
     }
 }
 
-export default Friends;
+const mapStateToProps = (state) => {
+    return {
+        friends: state.friends,
+        error: state.error,
+        fetchingData: state.fetchingData
+    }
+}
+
+
+
+export default withRouter(connect(mapStateToProps, {getData} )(Friends))
