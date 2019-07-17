2d114f9becfd20e078fb916fe398165bc9c7362d
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-07-17 19:14:10 -0400

    adding folder structure, actions, thunked axios request, and a more boilerplate

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import friendsReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import './index.css';
+
+const store = createStore(
+  friendsReducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ friends/src/reducers/friendsReducer.js
@@ -0,0 +1,5 @@
+const initialState = {
+  friends: [],
+  errorMessage: null,
+  fetching: false
+};
