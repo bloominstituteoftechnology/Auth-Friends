55a679aad7c8119e05a7d424505420d9995f5cc4
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-07-18 19:15:51 -0400

    changes

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -29,7 +30,9 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
