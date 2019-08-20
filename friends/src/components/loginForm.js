2684c38c684d82497e1aeab5d820cfbd75a1e0c7
Richard Lawson <lawson.a.richard@gmail.com>
2019-08-20 14:16:02 -0700

    created loginform
    created loginform am able to sign onto with Lambda user/password with no errors

--- friends/src/components/loginForm.js
+++ friends/src/components/loginForm.js
@@ -1,17 +1,64 @@
 import React from "react";
-import { Form, FormInput, FormGroup } from "shards-react";
+import { connect } from "react-redux";
+import { login } from "../actions";
+import { Form, FormInput, FormGroup, Button } from "shards-react";
 
-export default function FormExample() {
-  return (
-    <Form>
-      <FormGroup>
-        <label htmlFor="#username">Username</label>
-        <FormInput id="#username" placeholder="Username" />
-      </FormGroup>
-      <FormGroup>
-        <label htmlFor="#password">Password</label>
-        <FormInput type="password" id="#password" placeholder="Password" />
-      </FormGroup>
-    </Form>
-  );
+class LoginForm extends React.Component {
+  state = {
+    credentails: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentails: {
+        ...this.state.credentails,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+  login = e => {
+    e.preventDefault();
+    this.props.login(this.state.credentails).then(res => {
+      if (res) {
+        this.props.history.push("/protected");
+      }
+    });
+  };
+  render() {
+    return (
+      <Form>
+        <FormGroup>
+          <FormInput
+            text="text"
+            name="username"
+            value={this.state.credentails.username}
+            onChange={this.handleChange}
+            placeholder="Username"
+          />
+        </FormGroup>
+        <FormGroup>
+          <label htmlFor="#password">Password</label>
+          <FormInput
+            text="text"
+            name="password"
+            value={this.state.credentails.password}
+            onChange={this.handleChange}
+            placeholder="Password"
+          />
+        </FormGroup>
+        <Button onClick={this.login}>Login</Button>
+      </Form>
+    );
+  }
 }
+const mapStateToProps = state => ({
+  error: state.error,
+  loggingIn: state.loggingIn
+});
+export default connect(
+  mapStateToProps,
+  { login }
+)(LoginForm);
