a3a6a4c91bf952597c73708203f03b9bdcf1f208
Arvin Agas <arvagas@gmail.com>
2019-08-20 13:44:04 -0400

    created login form

---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Form, Field, withFormik } from 'formik'
+import * as yup from 'yup'
+import axios from 'axios'
+
+function JSXForm({ values, errors, touched }) {
+
+    return (
+        <Form>
+            <Field type='text' name='username' placeholder='Username'/>
+            {touched.username && errors.username && <p>{errors.username}</p>}
+
+            <Field type='password' name='password' placeholder='Password'/>
+            {touched.password && errors.password && <p className='err'>{errors.password}</p>}
+
+            <button type='submit'>Login</button>
+        </Form>
+    )
+}
+
+const LoginForm = withFormik({
+    mapPropsToValues({ username, password }) {
+        return {
+            username: username || '',
+            password: password || ''
+        }
+    },
+
+    validationSchema: yup.object().shape({
+        username: yup
+            .string()
+            .required('*Username is required'),
+        password: yup
+            .string()
+            .required('*Password is required'),
+    }),
+
+    handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+        if (values.username !== 'admin') setErrors({username: '*Incorrect username'})
+        else if (values.password !== 'password') setErrors({password: '*Incorrect password'})
+        else {
+            axios
+            .post('http://localhost:5000/api/login', values)
+            .then(res => {
+                console.log(res)
+                localStorage.setItem('token', res.data.payload)
+                resetForm()
+                setSubmitting(false)
+            })
+            .catch(err => {
+                console.log(err)
+                setSubmitting(false)
+            })
+        }
+    }
+})(JSXForm)
+
+export default LoginForm
