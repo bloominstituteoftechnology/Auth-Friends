e40b9d257c163212b63d411bba95c8cd8626a949
Arvin Agas <arvagas@gmail.com>
2019-08-20 12:22:38 -0400

    set up reducer and initialState

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    isLoggedIn: false,
+    friendsList: [],
+    isLoading: false,
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default :
+            return state
+    }
+}
