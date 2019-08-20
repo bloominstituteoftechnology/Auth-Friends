570da98095afeb10f8af355c2f99344398e530dd
Arvin Agas <arvagas@gmail.com>
2019-08-20 16:30:27 -0400

    app properly routes pages

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import NavBar from './components/NavBar'
 import AppRouter from './components/AppRouter'
 
 function App() {
   return (
     <div>
+      <NavBar />
       <AppRouter />
     </div>
   )
