4a275a2b064440d9e20e9b60fec5869706825355
Christopher Hernandez <cerberean661@gmail.com>
2019-09-17 16:13:46 -0700

    set up redirection from /Login to /friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
-import { Route } from 'react-router-dom';
+import Friends from './components/Friends';
+import { Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Route path="/login" component={Login} />
+      <Route path="/friends" component={Friends} />
     </div>
   );
 }
---
+++ friends/src/components/Friends.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Friends = (props) => {
+    return (
+        <div>
+            <h2>Friends</h2>
+        </div>
+    );
+};
+
+export default Friends;
