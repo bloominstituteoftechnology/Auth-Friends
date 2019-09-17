41e94f715350cef33bf77033bfea5a7acd93e29e
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-09-17 17:17:09 -0500

    Added browser router

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login'
+
+import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+				<nav>
+					<Link to="/login">Login</Link>
+				</nav>
+				<Switch>
+					<Route component={Login}/>
+				</Switch>
+			</Router>
     </div>
   );
 }
