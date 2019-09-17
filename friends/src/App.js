6852c454a3458b9a999e92a30bc712feccb1c3a9
Christopher Hernandez <cerberean661@gmail.com>
2019-09-17 15:04:32 -0700

    setting up Login.js, login input is console logged

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
+      <Login />
     </div>
   );
 }
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
+// username: Lambda School
+// password: i<3Lambd4
 
 const Login = (props) => {
-    const [creds, setCreds] = useState({});
+    const [creds, setCreds] = useState({username: "", password: ""});
     const handleChange = event => {
+
         setCreds({...creds, [event.target.name]: event.target.value});
     };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        axios.post('http://localhost:5000/api/login', creds)
+            .then(res => {
+                console.log(res);
+                localStorage.setItem('token', res.data.payload)
+            })
+            .catch(err => console.group(err.response));
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="type"
                     name="username"
                     placeholder="Username"
