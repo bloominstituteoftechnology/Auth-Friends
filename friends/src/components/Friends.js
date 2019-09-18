39f7a9654c31306ff62934613ca864ff9bdf5cd3
Christopher Hernandez <cerberean661@gmail.com>
2019-09-17 19:52:56 -0700

    adding axios in Friends.js

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Friends = (props) => {
+    const [friendsList, setFriendsList] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/friends')
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => console.log(err.response));
+    }, []);
+
     return (
         <div>
             <h2>Friends</h2>
