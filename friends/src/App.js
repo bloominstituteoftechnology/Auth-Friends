eb3995778b36df2026986cff1bbff3ffd248f927
Christopher Hernandez <cerberean661@gmail.com>
2019-09-17 23:36:25 -0700

    added Redirect & new route in App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import './App.css';
 import Login from './components/Login';
 import Friends from './components/Friends';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Redirect } from 'react-router-dom';
+// import { prependOnceListener } from 'cluster';
+
+const ProtectedRoute = (props) => {
+  const propsWithNoComponent = {...props, component: undefined};
+  return <Route {...propsWithNoComponent} render={props => {
+    if (localStorage.getItem('token')) {
+      return <props.component {...props} />;
+    } else {
+      return <Redirect to="/login" />;
+    }
+  }} />;
+};
 
 function App() {
   return (
     <div className="App">
       <Route path="/login" component={Login} />
-      <Route path="/friends" component={Friends} />
+      <ProtectedRoute path="/friends" component={Friends} />
     </div>
   );
 }
