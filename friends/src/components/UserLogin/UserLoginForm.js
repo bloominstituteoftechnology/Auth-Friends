acfa8ccc3fe6aafc89724f1705a0a099bb080000
abtorres <abtorres079@gmail.com>
2019-10-07 03:00:00 -0600

    axiosAuth.js complete

---
+++ friends/src/components/UserLogin/UserLoginForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+function UserLoginForm({ errors, touched, values }) {
+
+    return (
+        <Form>
+            <div>
+                {touched.userName && errors.userName && <p>{errors.userName}</p>}
+                <Field className='field' type='text' name='userName' placeholder='User Name'/>
+            </div>
+            <div>
+                {touched.userPassword && errors.userPassword && <p>{errors.userPassword}</p>}
+                <Field className='field' type='password' name='userPassword' placeholder='Password'/>
+            </div>
+            <button type='submit'>Submit</button>
+        </Form>
+    )
+}
+
+const FormikLoginForm = withFormik({
+    mapPropsToValues({ userName, userPassword, setToken }) {
+        return {
+            userName: userName || "",
+            userPassword: userPassword || ""
+        };
+    },
+
+    validationSchema: Yup.object().shape({
+        userName: Yup.string()
+                .required('User Name is Required'),
+        userPassword: Yup.string()
+                .required('Password is Required')
+    }),
+
+    handleSubmit(values, { props }) {
+
+    }
+})(UserLoginForm)
+
+export default FormikLoginForm;
