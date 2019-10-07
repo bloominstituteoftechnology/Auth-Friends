a45df698bb48ec657389e478ac58b80346ec2358
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2019-10-06 20:25:09 -0700

    appjs

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import Login from './components/login'
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+
+        <div>
+          <Link to='/login'>Login</Link>
+        </div>
+
+        <div>
+          <Link to='/protected'>Protected</Link>
+        </div>
+
+
+        <Login />
+      </div>
+
+
+    </Router>
+
   );
 }
 
