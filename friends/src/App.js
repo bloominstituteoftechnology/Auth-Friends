d7ebe47fba662fa609c23bfa232ac19ccb7c4f7f
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-15 15:36:42 +0100

    Refactored state to use redux

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,34 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as withAuth from './helpers/withAuth';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Login from './components/Login';
 import Friends from './components/Friends';
 
 export const App = () => {
-	const [formData, setForm] = useState({ username: "", password: "" });
-	const [isLoading, setLoading] = useState(true);
-	const onInputChange = e => {
-		const newFormState = {
-			...formData,
-			[e.target.name]: e.target.value
-		}
-		setForm(newFormState);
-	}
-
 	return (
-		<Router>
-			<div className="inner">
-				<Route exact path="/" render={
-					props => <Login
-						{...props}
-						isLoading={isLoading}
-						formData={formData}
-						setForm={setForm}
-						onInputChange={onInputChange} />
-				} />
-				<withAuth.asRoute path="/friends" component={Friends} />
-			</div>
-		</Router>
+		<div className="inner">
+			<Route path="/" render={
+				props => <Login {...props} />
+			} />
+			<withAuth.asRoute path="/friends" component={Friends} />
+		</div>
 	);
 }
 
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import * as reducers from './state/reducers';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={
+		createStore(
+			combineReducers({
+				login: reducers.loginReducer,
+				isLoading: reducers.loadingReducer,
+				friendsList: reducers.friendsReducer,
+				addFriend: reducers.addFriendReducer
+			}),
+			applyMiddleware(thunk))
+	}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
+);
