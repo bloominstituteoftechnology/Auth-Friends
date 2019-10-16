5f1986f41bf8ed1b675741295a978c675fb2109c
theJOHNwatt <j.watt10@gmail.com>
2019-10-16 12:44:58 -0700

    aoo created. axois post and login finished. working on protected routes and friends list now

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import FriendsList from './components/FriendsList';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+          <Link to='/login'>Login</Link><br />
+        
+          <Link to='/protected'>Friends List</Link>
+      
+      <Switch>
+        <Route path='/login' component={Login} />
+        <PrivateRoute path='/protected' component={FriendsList}/>
+        <Route component={Login} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/Login.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+
+    handleChanges = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login = e => {
+        e.preventDefault();
+        axiosWithAuth()
+        .post('/api/login', this.state.credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            this.props.history.push('/protected');
+        })
+        .catch(err => console.log(err.response));
+    };
+
+    render(){
+        return (
+            <div>
+                <form onSubmit={this.login}>
+                    <input
+                    type='text'
+                    name='username'
+                    value={this.state.credentials.username}
+                    onChange={this.handleChanges}
+                    />
+                    <input
+                    type='text'
+                    name='password'
+                    value={this.state.credentials.password}
+                    onChange={this.handleChanges}
+                    />
+                    <button>Log In</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
