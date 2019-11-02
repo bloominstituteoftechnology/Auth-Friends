c7c4d9ad0b80b6ddc7f360cfe1426115cad3c6f5
spettigrew <sara.pettigrew5@gmail.com>
2019-11-02 16:43:42 -0600

    imported components to app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,6 +6,9 @@ import Login from './components/Login';
 import Account from './components/Account';
 import Logout from './components/Logout';
 import FriendList from './components/FriendList';
+import DisplayFriend from './components/DisplayFriend';
+import UpdateFriend from './components/UpdateFriend';
+import DeleteFriend from './components/DeleteFriend';
 
 
 
@@ -25,9 +28,15 @@ function App() {
           {signedIn && <Link to="/account">My Account</Link>}
           {signedIn && <Link to="/logout">Logout</Link>}
           <Link to='/friendlist'>Friend List</Link>
+          <Link to='/displayfriend'>Display Friend</Link>
+          <Link to='/updatefriend'>Update Friend</Link>
+          <Link to='/deletefriend'>Delete Friend</Link>
         </nav>
 
       <Route exact path='/friendlist' component={FriendList} />
+        <Route exact path='/displayfriend' component={DisplayFriend} />
+        <Route exact path='/updatefriend' component={UpdateFriend} />
+        <Route exact path='/deletefriend' component={DeleteFriend} />
       <Route exact path='/login' component={Login} />
       <PrivateRoute exact path='/account' component={Account} />
       <PrivateRoute exact path="/logout" component={Logout} />
