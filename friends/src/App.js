580f6d7de185cce5eda3b09e7392a21b6de9499a
Robert Kulp <pedalinon@gmail.com>
2019-11-03 17:03:14 -0800

    App on the way

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Route, withRouter } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <nav className="App-header">
+       <Link 
+        to='/Login'
+        className='link'
         >
-          Learn React
-        </a>
-      </header>
+          Login
+        </Link>
+
+        
+      </nav>
+
+      <Route 
+        exact path='/login'
+        component={Login}
+      />
     </div>
   );
 }
---
+++ friends/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Logout() {
+	return (
+		<div className='logout'>
+			<h3>Logout Component</h3>
+		</div>
+	)
+}
+
+export default Logout;
