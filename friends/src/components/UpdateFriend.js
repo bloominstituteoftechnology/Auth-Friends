92ba863bdd092fa206ac3d4870a19a54369c0d5d
spettigrew <sara.pettigrew5@gmail.com>
2019-11-06 20:14:11 -0700

    delete comp deleted and updatefriend, delete btn

--- friends/src/components/UpdateFriend.js
+++ friends/src/components/UpdateFriend.js
@@ -15,12 +15,40 @@ function UpdateFriend() {
         })
     })
 
+    const handleDelete = (e) =>{
+        e.preventDefault()
+
+        const user = users.filter(user => user.id === id)
+
+        if (window.confirm('Delete Friend?')) {
+            setUpdateFriend(updateFriend.filter(friend => friend.id !== id))
+
+            api().delete('/api/friends/123')
+                .then(res => {
+                    console.log(res)
+                    setDeleteFriend(res.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                    setUpdateFriend([ ...updateFriend, friend ])
+                })
+        }
+    }
+
     return (
-        <div>
-            {updateFriend.map(friend => (
-                <p>{friend.name}, {friend.id}</p>
+        <>
+            <h1>Friends</h1>
+    
+            {updateFriend.map((friend) => (
+                <div key={friend.id} className="friend">
+                    <Link className="friend-update" to={`/friends/${friend.id}`}>
+                        Update Friend
+                    </Link>
+                    <div className="friend-delete" onClick={(e) => handleDelete(e, friend.id)}>
+                        Delete Friend
+                    </div>
             ))}
-        </div>
+        </>
     )
 }
 
