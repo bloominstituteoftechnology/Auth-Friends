23c7975ef78325926516db4216441806dfee1f51
Toniddarden1 <Toniddarden@gmail.com>
2019-12-15 00:30:20 -0500

    bug fix

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import {BrowserRouter as Route, Link, Switch} from 'react-router-dom';
 
 import Login from './Components/Login';
 import PrivateRoute from './utils/PrivateRoute';
--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -2,28 +2,25 @@ import React, { useState, useEffect } from "react";
 import axiosWithAuth from "../utils/AxiosWithAuth";
 import Friends from "./Friends";
 import AddNewFriend from "./AddNewFriend";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { getFriends } from "../actions";
-import { withRouter } from "react-router-dom";
 
 const FriendList = props => {
   console.log("friend-list props:", props);
   const [friend, setFriend] = useState([]);
 
-  useEffect(() => {
-    getData();
-  }, []);
+
 
   const getData = () => {
     axiosWithAuth()
       .get("http://localhost:5000/api/friends")
       .then(res => {
         setFriend(res.data);
-        props.history.push("/protected");
       })
-      .catch(err => console.error("err", err));
+      .catch(err => console.error("err getdata", err));
   };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const addFriend = name => {
     axiosWithAuth()
