b4ce8acd4d68596ef6f4afcba18f74ea52e86f39
Rodrigo Graça <1134310+rodrigograca31@users.noreply.github.com>
2019-12-17 18:47:21 +0000

    🐛 Fix caching issue

--- server.js
+++ server.js
@@ -51,6 +51,11 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
+  next();
+});
+
 function authenticator(req, res, next) {
   const { authorization } = req.headers;
   if (authorization === token) {
