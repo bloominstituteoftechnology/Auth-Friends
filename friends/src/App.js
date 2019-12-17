309a7d1014763b14df479425300db631c9891007
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-12-17 11:25:00 +0000

    Project set up working on withAuth and login page

---
+++ friends/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
