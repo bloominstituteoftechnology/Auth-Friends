4a2c483498277c6216fd7c8a8627e21d8aa216bf
Aasa-Christian <aasachristian@yahoo.com>
2019-12-18 11:51:31 -0500

    reached mvp

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,24 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import AxiosWithAuth from '../utilz/AxiosWithAuth';
+
 
 
 const FriendsList = () =>{
 const [friends, setFriends] =useState({
-    id: '',
     name: "",
     age: '',
     email: ""
 })
 
+useEffect(() => {
+    AxiosWithAuth()
+    .get('/friends')
+    .then(res => {
+        console.log(res.data)
+    })
+},[{friends}])
+    
+
+
 const handleChange = e => {
     setFriends({...friends, [e.target.name]: e.target.value})
 }
+
+const postFriend = e => {
+    e.preventDefault();
+    AxiosWithAuth()
+    .post('/friends/', friends)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => console.log(err))
+}
+
+
+    
+
+
+
+
     return(
-        <form>
+        <form onSubmit={postFriend}>
         <input type="text" name="name" onChange={handleChange} placeholder="name" />
         <input type="text" name="age" onChange={handleChange} placeholder="age" />
         <input type="text" name="email" onChange={handleChange} placeholder="email"/>
         <button type="submit">Add Friend</button>
 </form>
+
+        
     )
 
 
