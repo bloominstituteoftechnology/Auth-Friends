2e38928cdefb534c6ca4b7650c21a58d561738ed
Trey McGarity <trey.mcgarity99@gmail.com>
2020-01-20 05:12:17 -0600

    mvp

---
+++ friends/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Login from "./components/LoginPage";
+import PrivateRoute from "./components/PrivateRoute";
+import Friends from "./components/Friends";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/protected" component={Friends} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
