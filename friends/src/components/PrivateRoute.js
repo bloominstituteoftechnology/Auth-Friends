339197bd5e2a0f69522120fbc0c1d3c138fd315b
chiragthesia <harithesia@gmail.com>
2020-01-21 17:47:23 -0600

    MVP reached

---
+++ friends/src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...theRest }) =>{
+    return(
+        <Route
+            {...theRest}
+            render = {()=>{
+                if (localStorage.getItem("token")){
+                    return <Component/>;
+                } else {
+                    return <Redirect to="/login"/>;
+                }
+            }}
+        />
+    )
+}
+
+export default PrivateRoute;
