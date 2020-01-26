816c7936d8a16e076cb8ab725d9eef269fe8dfee
Your name <a.m.verdine@gmail.com>
2020-01-26 11:04:34 -0500

    Setup App.js, links, and Routes

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
