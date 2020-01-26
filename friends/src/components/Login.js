e372499358baa827faa94cb02e5e3a3b5723cc74
Your name <a.m.verdine@gmail.com>
2020-01-26 11:50:19 -0500

    Build Login Component and Login form

---
+++ friends/src/components/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log In</button>
+        </form>
+      </div>
+    );
+  }
+}
