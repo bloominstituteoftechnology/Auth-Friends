5c3729fab12502a2405920a69f61bc5ba1df6902
Your name <a.m.verdine@gmail.com>
2020-01-26 12:11:00 -0500

    Set Login component state, build change handler, and login POST request

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -2,6 +2,36 @@ import React from "react";
 import axios from "axios";
 
 class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:5000/api/login", this.state.credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+      })
+      .catch(err => {
+        console.log("ERROR", err);
+      });
+  };
+
   render() {
     return (
       <div>
@@ -25,3 +55,5 @@ class Login extends React.Component {
     );
   }
 }
+
+export default Login;
