4ee204c9c251b7cd26dec33fa94a0b3cd9a5a34c
Anna Morris <annapatton444@gmail.com>
2020-01-26 18:26:04 -0600

    Update Routes.js

--- friends/src/components/Routes.js
+++ friends/src/components/Routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Login from "./Login";
 import List from "./List";
 import PrivetRoute from "./PrivetRoute";
-// import CreateForm from "./CreateForm";
 
 function Routes() {
   return (
@@ -16,14 +15,10 @@ function Routes() {
           <li>
             <Link to="/protected">My Friends</Link>
           </li>
-          {/* <li>
-            <Link to="/create">Add a Friend</Link>
-          </li> */}
         </ul>
         <Switch>
           <PrivetRoute exact path="/protected" component={List} />
           <Route path="/login" component={Login} />
-          {/* <Route path="/create" component={CreateForm} /> */}
         </Switch>
       </div>
     </Router>
