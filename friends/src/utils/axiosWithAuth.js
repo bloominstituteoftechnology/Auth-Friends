45eee2e045a4b307fb2bb9e21a35cc34ad584b0a
sooshe <sooshe@sooshebot.com>
2020-01-28 13:14:11 -0600

    add axiosWithAuth and axios dependency to friends file

---
+++ friends/src/utils/axiosWithAuth.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token');
+  return axios.create({
+    baseURL: 'http://localhost:5000',
+    headers: {
+      Authorization: token
+    }
+  });
+};
