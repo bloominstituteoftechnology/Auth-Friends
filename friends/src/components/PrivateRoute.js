ee2489eab953331091b65d932c80e74087fb01f4
sooshe <sooshe@sooshebot.com>
2020-01-28 13:54:32 -0600

    create PrivateRoute and export

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+//note --rest is a 'banana' term, can call it anything!
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (localStorage.getItem("token")) {
+          return <Component {...props} />;
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
