4e420873e0cbc31b6103ad275b9a0e84d37e8a34
sooshe <sooshe@sooshebot.com>
2020-01-28 13:26:37 -0600

    create handlechange+handleSubmit+form in login file

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,2 +1,29 @@
-import React from 'react';
-import { axiosWithAuth } from '../utils/axiosWithAuth';
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const Login = props => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    passwod: ""
+  });
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post()
+      .then()
+      .catch();
+  };
+
+  return (
+    <div className="login-form">
+      <form data-testid="form" onSubmit={handleSubmit}></form>
+    </div>
+  );
+};
+
+export default Login;
