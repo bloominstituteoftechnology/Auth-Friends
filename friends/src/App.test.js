0cf4db2f276131c91a325e3dc4cb83b35c368999
sooshe <sooshe@sooshebot.com>
2020-01-28 13:29:51 -0600

    run init test for sanity check

--- friends/src/App.test.js
+++ friends/src/App.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
