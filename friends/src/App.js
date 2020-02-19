4792e51cfff77cb7f6d85894928119b6662d82ab
Kayla Dailey <kayladailey000@gmail.com>
2020-02-18 19:44:08 -0600

    utils/private route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from './Login';
+import Login from './components/Login';
 
 function App() {
   return (
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 class Login extends React.Component {
   state = {
@@ -19,27 +19,36 @@ class Login extends React.Component {
   };
 
   login = e => {
-    e.preventDefault();
+    e.preventDefault(); 
+    axiosWithAuth()
+      .post('/login', this.state.credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+       
+        this.history.push('/private');
+      })
+      .catch(err => console.log(err));
   };
 
   render() {
     return (
       <div>
-        <h1> Friends List </h1>
         <form onSubmit={this.login}>
           <input
             type="text"
             name="username"
+            placeholder="username"
             value={this.state.credentials.username}
             onChange={this.handleChange}
           />
           <input
             type="password"
             name="password"
+            placeholder="password"
             value={this.state.credentials.password}
             onChange={this.handleChange}
           />
-          <button>Log in</button>
+          <button className="btn">Login</button>
         </form>
       </div>
     );
