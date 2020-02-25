5baadba3a1db8b294cee404fe78642fdb919d573
Kevin <knphillips01@gmail.com>
2020-02-25 15:12:31 -0600

    added routing

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Login from './components/Login'; 
+import PrivateRoute from './components/PrivateRoute';
 
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <ul>
         <li>
           <Link to='/login'>Login</Link>
         </li>
+        <li>
+          <Link to="/protected">Protected Page</Link>
+        </li>
       </ul>
+      <Switch>
+        <PrivateRoute exact path="/protected" />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
