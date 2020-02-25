1c2a60ab0bcdc22d9e6c60019823204e4f32b2c9
Kevin <knphillips01@gmail.com>
2020-02-25 15:49:48 -0600

    added axiosWithAuth

---
+++ friends/src/utils/AxiosWithAuth.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+  const token = window.localStorage.getItem('token');
+
+  return axios.create({
+    headers: {
+      authorization: token
+    },
+    baseURL: 'http://localhost:5000'
+  });
+};
