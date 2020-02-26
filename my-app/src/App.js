d8aa8227046f8216a3635cef0ba503398b5d9bea
carlsachs <sachscarl@gmail.com>
2020-02-26 10:23:42 -0500

    setting up

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import Login from "./components/Login.js";
+import MyFriends from "./components/MyFriends.js";
+import PrivateRoute from "./components/PrivateRoute.js";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <div>
+          <Link to="/login">Login</Link>
+        </div>
+        <div>
+          <Link to="/myfriends">Friends</Link>
+        </div>
+        <Switch>
+          <PrivateRoute exact path="/myfriends" component={MyFriends} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
