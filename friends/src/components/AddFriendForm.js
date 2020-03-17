a70b25ef859d197ee7d82e560b867911faf96561
Your name <a.m.verdine@gmail.com>
2020-03-16 23:48:04 -0400

    Refactor AddFriendForm component to functional component

--- friends/src/components/AddFriendForm.js
+++ friends/src/components/AddFriendForm.js
@@ -1,79 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-class AddFriendForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      age: "",
-      email: ""
-    };
+const AddFriendForm = props => {
+  const [friendData, setFriendData] = useState({
+    name: "",
+    age: "",
+    email: ""
+  });
 
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  onSubmit = e => {
-    e.preventDefault();
-
-    this.setState({
-      name: this.props.name,
-      age: this.props.age,
-      email: this.props.email
-    });
-
+  useEffect(() => {
     axiosWithAuth()
-      .post("/api/friends", this.state.friends)
-      .then(res => console.log(res))
+      .post("/api/friends")
+      .then(
+        res => console.log(res)
+        // res.data.push(friend => {
+        //   this.setState({
+        //     name: this.state.name,
+        //     age: this.state.age,
+        //     email: this.state.email
+        //   });
+        // })
+      )
       .catch(err => console.log(err));
+  }, []);
+
+  const onChange = e => {
+    setFriendData({ [e.target.name]: e.target.value });
   };
 
-  render() {
-    return (
-      <div>
-        <h1>Add Friends</h1>
-        <form onSubmit={this.onSubmit}>
-          <div>
-            <label>Name: </label>
-            <br />
-            <input
-              type="text"
-              name="name"
-              onChange={this.onChange}
-              value={this.props.name}
-            />
-          </div>
-          <div>
-            <label>Age: </label>
-            <br />
-            <input
-              type="text"
-              name="age"
-              onChange={this.onChange}
-              value={this.props.age}
-            />
-          </div>
-          <div>
-            <label>Email: </label>
-            <br />
-            <input
-              type="text"
-              name="email"
-              onChange={this.onChange}
-              value={this.props.email}
-            />
-          </div>
-        </form>
-        <button type="submit">Add Friend</button>
-        <hr />
-      </div>
-    );
-  }
-}
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h1>Add Friends</h1>
+      <form onSubmit={props.onSubmit}>
+        <div>
+          <label>Name: </label>
+          <br />
+          <input
+            type="text"
+            name="name"
+            onChange={props.onChange}
+            value={props.name}
+          />
+        </div>
+        <div>
+          <label>Age: </label>
+          <br />
+          <input
+            type="text"
+            name="age"
+            onChange={props.onChange}
+            value={props.age}
+          />
+        </div>
+        <div>
+          <label>Email: </label>
+          <br />
+          <input
+            type="text"
+            name="email"
+            onChange={props.onChange}
+            value={props.email}
+          />
+        </div>
+      </form>
+      <button type="submit">Add Friend</button>
+      <hr />
+    </div>
+  );
+};
 
 export default AddFriendForm;
