fd78d7c9c52d996a0e0a5f81ee618a2617734609
Teague <teaguehannam@gmail.com>
2020-03-24 15:14:06 -0600

    Login component built

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -16,11 +16,31 @@ class Login extends React.Component {
     }
 
     handleChange = e => {
-   		return "";
+   		this.setState({credentials: {
+            ...this.state.credentials,
+            [e.target.name]: e.target.value
+        }});
+        console.log(this.state.credentials);
     }
 
     handleSubmit = e => {
-    	return "";
+    	e.preventDefault();
+        this.setState({...this.state, isLoading: true});
+
+        axiosWithAuth().post('/api/login', this.state.credentials)
+        .then(res => {
+            //Give localStorage the token
+            window.localStorage.setItem('token', res.data.payload);
+            this.setState({...this.state, isLoading: false});
+            //Send logged in user to protected page
+            this.props.history.push('/protected');
+        })
+        .catch(err => {
+            console.log(err);
+            alert("Unable to sign in");
+            this.setState({...this.state, isLoading: false});
+        })
+
     }   
 
     render() {
