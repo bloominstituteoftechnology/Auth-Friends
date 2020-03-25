80c61e9038ee69cb2b2479f6ba5e563ec725b7c3
emma <sonicjane2020@gmail.com>
2020-03-24 17:25:37 -0700

    met mvp

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Login from "./components/Login";
+import PrivateRoute from './components/PrivateRoute';
+import Friends from './components/Friends';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <h1>Friends</h1>
-        <Link to='./login/'>Login</Link>
+      <div className='container'>
+        <Switch>
+          <PrivateRoute exact path='/friends' component={Friends} />
+          <Route path='/login' component={Login} />
+          <Route component={Login} />
+        </Switch>
       </div>
     </Router>
   );
