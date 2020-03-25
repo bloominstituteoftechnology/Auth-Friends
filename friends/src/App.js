db6bfe80f37aa4a354c71cdd88956164f05214c8
emma <sonicjane2020@gmail.com>
2020-03-24 17:16:54 -0700

    added private route, imported nessecary things in app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link, Switch} from 'reacter-router-dom';
 import './App.css';
 
 function App() {
---
+++ friends/src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+
+const PrivateRoute = ({component: Component, ...props }) => {
+    return  (
+        <Route 
+            {...props}
+            render={(props) => {
+                if (localStorage.getItem('token')) {
+                    return <Component />;
+                } else {
+                    return <Redirect to='/login' />;
+                }
+            }}
+        />
+    )
+}
+
+export default PrivateRoute;
