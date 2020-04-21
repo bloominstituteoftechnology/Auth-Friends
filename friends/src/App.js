e8d894cdaf0f28807561d093d8364385fe7c6156
Michael Perez <mperez6473@gmail.com>
2020-04-21 14:04:16 -0500

    react-router-dom was installed, Added some page breaking info onto App.js. Need to add the components to have App.js load up correctly.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <header>
+                    <ul>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/protected">Friends Home Page</Link>
+                        </li>
+                    </ul>
+                </header>
+                <Switch>
+                    <PrivateRouter
+                        exact
+                        path="/protected"
+                        component={FriendsHomePage}
+                    />
+                    <Route path="/login" component={Login} />
+                    <Route component={Login} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
