3fd8a6d153d062955e961362bbf1bdd0bcb7176b
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-04-21 14:54:49 -0700

    Login.js created and axiosWithAuth added.

---
+++ friends/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const Login = (props) => {
+ const [credentials, setCredentials] = useState({});
+
+
+const login = e => {
+  e.preventDefault();
+  axiosWithAuth().post('http://localhost:5000/api/login', credentials)
+  .then(res => {
+    console.log("Sucessful Login", res.data.payload);
+   localStorage.setItem('token', res.data.payload);
+   props.history.push('/');
+  })
+}
+
+const handleChange = e => {
+  setCredentials({
+    ...credentials,
+    [e.target.name]: e.target.value,
+  })
+}
+
+return (
+  <div>
+    <form onSubmit={login}>
+      <input
+        type="text"
+        name="password"
+        value={credentials.username}
+        onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+          />
+        <button>Log In!</button>
+    </form>
+  </div>
+)
+}
+
+export default Login;
