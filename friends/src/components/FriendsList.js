c928bf241a53e305d889d817162a11a567cb75d7
Michael Perez <mperez6473@gmail.com>
2020-04-21 16:46:40 -0500

    I got each friend to render! noicegit add .

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -15,13 +15,44 @@ class FriendsList extends React.Component {
         authenticAxios()
             .get('/api/friends')
             .then((res) => {
-                console.log(res.data);
+                console.log('res.data: ', res.data);
+                console.log('state: ', this.state);
+                this.setState({
+                    friends: res.data,
+                });
             })
-            .catch((err) => console.log(err));
+            .catch((err) => console.log('error', err));
+    };
+
+    formatThisData = () => {
+        const formatedData = [];
+        console.log('this.state: ', this.state);
+
+        return formatedData;
     };
 
     render() {
-        return <h1>test</h1>;
+        const friends = this.formatThisData();
+        return (
+            <section>
+                <h1>List of Friends:</h1>
+                {console.log('aaaayyyykiiiidd: ', this.state.friends)}
+                {this.state.friends.map((value) => {
+                    console.log('mapped: ', value.name);
+                })}
+                {this.state.friends.map((value) => {
+                    return (
+                        <div key={value.id}>
+                            <h3>Hey there, I am {value.name}!</h3>
+                            <h5>
+                                I am {value.age} years old. If ya wanna talk,
+                                this is my email: {value.email}
+                            </h5>
+                        </div>
+                    );
+                })}
+            </section>
+        );
     }
 }
 
--- friends/src/utils/authenticAxios.js
+++ friends/src/utils/authenticAxios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const authenticAxios = () => {
     const token = JSON.parse(localStorage.getItem('token'));
     return axios.create({
-        headers: { Authorization: token },
+        headers: { Authorization: `${token}` },
         baseURL: 'http://localhost:5000',
     });
 };
