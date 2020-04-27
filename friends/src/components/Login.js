53f8e4b84ae38b7b8aec912cf4fe090380f9aabb
Shantay Grady <shantaygrady@gmail.com>
2020-04-27 10:44:43 -0400

    builds login page

---
+++ friends/src/components/Login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
