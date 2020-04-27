266926eda1daaab8a85c44c9a1492bc177e251d2
Shantay Grady <shantaygrady@gmail.com>
2020-04-27 10:59:16 -0400

    Adds route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Login from './components/Login';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import './App.css';
 
@@ -6,6 +8,23 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Auth Friends!</h1>
+      <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/protected" component={/* insert protected coponent here */} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
