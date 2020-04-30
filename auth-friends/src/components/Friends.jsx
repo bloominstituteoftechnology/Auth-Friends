d977d4c439d69614a8cc501190d90939ec223e15
Noor Al Darraji <49676151+nooraldarraji@users.noreply.github.com>
2020-04-30 02:09:41 -0500

    practice finding custom attr and some other stuff

--- auth-friends/src/components/Friends.jsx
+++ auth-friends/src/components/Friends.jsx
@@ -3,51 +3,52 @@ import axiosWithAuth from "../auth/axiosWithAuth";
 import { useState } from "react";
 
 export default (props) => {
-  const [friends, setFriends] = useState();
-  //
-  const [creds, setCreds] = useState({
+  const [state, setState] = useState({
+    friends: [],
+    isEditing: false,
+    id: 0,
+    placeholders: {
+      uname: "",
+      uage: "",
+      uemail: "",
+    },
     credentials: {
       name: "",
       age: "",
       email: "",
     },
-  });
-
-  //
-  const [user, setUser] = useState({
     updatedUser: {
       name: "",
       age: "",
       email: "",
     },
   });
-  const [isEditing, setIsEditing] = useState(false);
-  //
 
   useEffect(() => {
     axiosWithAuth()
       .get("/api/friends")
       .then((res) => {
-        setFriends(res.data);
+        setState((s) => ({ ...s, friends: res.data }));
       })
       .catch((err) => console.log(err));
-  }, [friends]);
+  }, [state.friends]);
 
   const handleChange = (e) => {
-    setCreds({
+    setState({
+      ...state,
       credentials: {
-        ...creds.credentials,
+        ...state.credentials,
         [e.target.name]: e.target.value,
       },
     });
-    console.log(creds);
+    console.log(state);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(creds);
+    console.log(state);
     axiosWithAuth()
-      .post("/api/friends", creds.credentials)
+      .post("/api/friends", state.credentials)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   };
@@ -62,99 +63,118 @@ export default (props) => {
   };
 
   const handleUpdate = (e) => {
-    setUser({
-      updatedUser: { ...user.updatedUser, [e.target.name]: e.target.value },
+    setState({
+      ...state,
+      updatedUser: { ...state.updatedUser, [e.target.name]: e.target.value },
     });
-    console.log(user);
+    console.log(state);
   };
 
   const handleUpdateSubmit = (e) => {
     e.preventDefault();
-    const { id } = e.target;
+    const id = state.id;
     axiosWithAuth()
-      .put(`/api/friends/${id}`, user.updatedUser)
+      .put(`/api/friends/${id}`, state.updatedUser)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
-    setIsEditing(false);
+    setState({ ...state, isEditing: false });
   };
 
   return (
     <div>
       <h4> Friend List</h4>
-      {friends ? (
-        friends.map((student) => {
+      {state.isEditing ? (
+        <div>
+          <label>Name: </label>
+          <input
+            type="text"
+            name="name"
+            placeholder={state.placeholders.uname}
+            onChange={handleUpdate}
+          />
+          <br />
+          <label>Age:</label>
+          <input
+            type="text"
+            name="age"
+            placeholder={state.placeholders.uage}
+            onChange={handleUpdate}
+          />
+          <br />
+          <label>E-mail:</label>
+          <input
+            type="text"
+            name="email"
+            placeholder={state.placeholders.uemail}
+            onChange={handleUpdate}
+          />
+          <br />
+          <button onClick={handleUpdateSubmit}>UPDATE NOW </button>
+        </div>
+      ) : state.friends ? (
+        state.friends.map((student) => {
           return (
-            <div>
-              {isEditing ? (
-                <div>
-                  <label>Name: </label>
-                  <input
-                    type="text"
-                    name="name"
-                    value={user.updatedUser.name}
-                    onChange={handleUpdate}
-                  />
-                  <br />
-                  <label>Age:</label>
-                  <input
-                    type="text"
-                    name="age"
-                    value={user.updatedUser.age}
-                    onChange={handleUpdate}
-                  />
-                  <br />
-                  <label>E-mail:</label>
-                  <input
-                    type="text"
-                    name="email"
-                    value={user.updatedUser.email}
-                    onChange={handleUpdate}
-                  />
-                  <br />
-                  <button id={student.id} onClick={handleUpdateSubmit}>
-                    UPDATE NOW{" "}
-                  </button>
-                </div>
-              ) : (
-                <div key={student.id}>
-                  <h2>{student.name}</h2>
-                  <h2>{student.age}</h2>
-                  <h2>{student.email}</h2>
-                  <button onClick={handleDelete}>DELETE</button>
-                  <button onClick={() => setIsEditing(true)}>UPDATE</button>
-                </div>
-              )}
+            <div key={student.id}>
+              <h2>{student.name}</h2>
+              <h2>{student.age}</h2>
+              <h2>{student.email}</h2>
+              <button id={student.id} onClick={handleDelete}>
+                DELETE
+              </button>
+              <button
+                id={student.id}
+                uname={student.name}
+                uage={student.age}
+                uemail={student.email}
+                onClick={(e) =>
+                  setState({
+                    ...state,
+                    isEditing: true,
+                    id: Number(e.target.id),
+                    placeholders: {
+                      uname: e.target.getAttribute("uname"),
+                      uage: e.target.getAttribute("uage"),
+                      uemail: e.target.getAttribute("uemail"),
+                    },
+                  })
+                }
+              >
+                UPDATE
+              </button>
             </div>
           );
         })
       ) : (
         <h4>Loading...</h4>
       )}
-      <label>Name: </label>
-      <input
-        type="text"
-        name="name"
-        value={creds.credentials.name}
-        onChange={handleChange}
-      />
-      <br />
-      <label>Age: </label>
-      <input
-        type="text"
-        name="age"
-        value={creds.credentials.age}
-        onChange={handleChange}
-      />
-      <br />
-      <label>Email: </label>
-      <input
-        type="text"
-        name="email"
-        value={creds.credentials.email}
-        onChange={handleChange}
-      />
-      <br />
-      <button onClick={handleSubmit}>ADD</button>
+      <div>
+        <br />
+        <label>Name: </label>
+        <input
+          type="text"
+          name="name"
+          value={state.credentials.name}
+          onChange={handleChange}
+        />
+        <br />
+        <label>Age: </label>
+        <input
+          type="text"
+          name="age"
+          value={state.credentials.age}
+          onChange={handleChange}
+        />
+        <br />
+        <label>Email: </label>
+        <input
+          type="text"
+          name="email"
+          value={state.credentials.email}
+          onChange={handleChange}
+        />
+        <br />
+        <button onClick={handleSubmit}>ADD</button>
+      </div>
     </div>
   );
 };
