c0ba9c7b8cfb36f9e6598b02937b2d6fe06b230d
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-04-30 17:14:17 -0700

    initial friendslist

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "React";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const FriendsList = props => {
+
+    return(
+        <div>
+            <h2> Friends List</h2>
+        </div>
+    )
+
+}
+
+export default FriendsList;
