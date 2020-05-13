eb35bb0232ab8bda25e7af48a5a2f91504395b47
unknown <tommyconner96@gmail.com>
2020-05-13 18:18:50 -0400

    got rid of window.location.reload

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { axiosWithAuth } from '../util/axiosWithAuth'
 import FriendsCard from './FriendsCard'
-import AddFriend from './AddFriend'
 
 const Friends = (props) => {
     const [friends, setFriends] = useState([])
     const [isLoading, setIsLoading] = useState(false)
+    const [newFriend, setNewFriend] = useState({
+      name: '',
+      age: '',
+      email: ''
+  })
+    const [submit, setSubmit] = useState(false)
 
     useEffect(() => {
       setIsLoading(true)
@@ -16,12 +21,64 @@ const Friends = (props) => {
           setIsLoading(false)
           })
         .catch(err => console.log(err))
-  }, [])
+  }, [submit])
+
+  const handleChange = (e) => {
+    e.preventDefault()
+
+    setNewFriend({
+        ...newFriend,
+        [e.target.name]: e.target.value
+    })
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    setSubmit(true)
+    axiosWithAuth()
+        .post("/api/friends", newFriend)
+        .then(response => {
+        console.log(response)
+        setNewFriend({name: '', age: '', email: ''})
+        setSubmit(false)
+        })
+        .catch(err => err)
+
+}
 
   
     return (
       <div>
-        <AddFriend />
+        <form onSubmit={handleSubmit}>
+            <input 
+                type='text'
+                name='name'
+                id='name'
+                placeholder='Name'
+                onChange={handleChange}
+                value={newFriend.name}
+                required
+            />
+            <input 
+                type='number'
+                name='age'
+                id='age'
+                placeholder='Age'
+                onChange={handleChange}
+                value={newFriend.age}
+                required
+            />
+            <input 
+                type='email'
+                name='email'
+                id='email'
+                placeholder='Email Adress'
+                onChange={handleChange}
+                value={newFriend.email}
+                required
+            />
+            <button type='submit'>Add Friend</button>
+        </form>
         {isLoading ? (
         <div>Loading ...</div>
       ) : (
