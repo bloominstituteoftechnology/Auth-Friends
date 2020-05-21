e5052713f93a56e61e9e916b9802d914ccff8a56
Jordan Lanier <jordan.6195@yahoo.com>
2020-05-21 15:50:32 -0400

    done

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Login from './Components/Login';
-import Friends from './Components/Friends';
-import PrivateRoute from './Components/PrivateRoute';
-import './App.css';
+import Login from "./Components/Login"
+import FriendsList from "./Components/FriendsList"
+import AddFriend from "./Components/AddFriend"
+import UpdateFriend from "./Components/UpdateFriend"
+import DeleteFriend from "./Components/DeleteFriend"
+import PrivateRoute from "./Components/PrivateRoute"
 
 
-function App() {
-	return (
-		<Router>
-			<div>
-				<Link to="/login">Login</Link>
-				<Link to="/friends">Friends</Link>
-				<Switch>
-					<PrivateRoute exact path="/friends" component={Friends} />
-					<Route path="/login" component={Login} />
-					<Route component={Login} />
-				</Switch>
-			</div>
-		</Router>
-	);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { credentials: {} }
+  }
+  render() {
+    return (
+      <Router>
+        <Link to='/login'>Login</Link>
+        <Link to='/friends-list'>Friends List</Link>
+        <Link to='/add-friend'>Add Friend</Link>
+        <Link to='/update-friend'>Update Friend</Link>
+        <Link to='/delete-friend'>Delete Friend</Link>
+
+        <Switch>
+
+			
+          <PrivateRoute exact path='/add-friend' component={AddFriend} />
+          <PrivateRoute exact path='/delete-friend' component={DeleteFriend} />
+          <PrivateRoute exact path='/friends-list' component={FriendsList} />
+          <PrivateRoute exact path='/update-friend' component={UpdateFriend} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Router>
+    )
+  }
 }
-
 export default App;
---
+++ friends/src/Components/FriendsList.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+import {axiosWithAuth} from "../utils/axiosWithAuth"
+
+class FriendsList extends React.Component{
+    constructor(){
+        super();
+        this.state = { friends: []}
+    }
+    componentDidMount() {
+        this.getFriendsList();
+    }
+    getFriendsList = () => {
+        const token = window.localStorage.getItem('token')
+        axiosWithAuth().get("/api/friends")
+        .then(res => {
+            console.log(res);
+            this.setState({ friends: res.data})
+        })
+        .catch(err => console.log(err))
+    }
+    render(){
+        return(
+            <div>
+                Friends List:
+        {this.state.friends.map(friend => <div>{`${friend.id}, ${friend.name}, ${friend.age} years old`}</div>)}
+            </div>
+        )
+    }
+}
+
+
+
+export default FriendsList
