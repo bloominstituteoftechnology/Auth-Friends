289e9eac987dde48560222e3266070c7d0a646aa
Owlspec3086 <ethical.worker3@gmail.com>
2020-07-14 23:31:20 -0400

    project fixed

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Friends from './components/Friends';
 import PrivateRoute from './components/PrivateRoute';
@@ -12,8 +12,8 @@ function App() {
       <div className='App'>
         <h1>Hello</h1>
         <Switch>
-          <Route path="/login" component={Login} />
-          <PrivateRoute exact path="/friends" component={Friends} />
+          <Route path='/login' component={Login} />
+          <PrivateRoute exact path='/friends' component={Friends} />
         </Switch>
       </div>
     </Router>
