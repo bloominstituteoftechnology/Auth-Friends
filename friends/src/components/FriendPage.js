00a5dcda678a005fea4691f03e38ca9e39e8dfc6
W-B28 <wtb2804@gmail.com>
2020-08-24 11:05:57 -0500

    MVP

---
+++ friends/src/components/FriendPage.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+
+import { axiosWithAuth } from "../utils/AxiosWithAuth";
+// Compontents
+import Friend from "./Friend";
+import FriendForm from "./FriendForm";
+
+function FriendPage() {
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    axiosWithAuth()
+      .get("http://localhost:5000/api/friends")
+      .then((res) => {
+        setFriends(res.data);
+      })
+      .catch((error) => {
+        console.error("Server Error", error);
+      });
+  }, []);
+
+  const [friends, setFriends] = useState([
+    {
+      id: "",
+      name: "",
+      age: "",
+      email: ""
+    }
+  ]);
+
+  console.log(friends);
+
+  const addNewFriend = (friend) => {
+    const newFriend = {
+      id: Date.now(),
+      name: friend.name,
+      age: friend.age,
+      email: friend.email
+    };
+    setFriends([...friends, newFriend]);
+  };
+
+  return (
+    <div className="App">
+      <h1>My Friends</h1>
+
+      <FriendForm addNewFriend={addNewFriend} />
+      <Friend friends={friends} />
+    </div>
+  );
+}
+
+export default FriendPage;
