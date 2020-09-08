dc9e8eec2a54555a99012bd85980dbef08fb9e7f
noah niesen <noah@neu.local>
2020-09-08 18:24:01 -0500

    removed useState in App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import Login from './components/Login';
