72f3934cf2fab9ba97c1f0a7b8ce79bad6dfc819
William Herman <williamajherman@gmail.com>
2020-09-15 17:13:01 -0400

    login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,15 +1,61 @@
 import React, {useEffect, useState} from 'react';
-import {axiosAuth} from '../utils/axiosWithAuth';
+import {axiosAuthWithToken} from '../utils/axiosWithAuth';
 
 const Login = () => {
 
-    useEffect(() => {
-        axiosAuth()
-    },[])
+    // set state for form values
+    const [formValues, setFormValues] = useState({
+        username: '',
+        password: ''
+    })
+
+    const [errors, setErrors] = useState('');
+
+    // submit credentials, if successful returns token, if unsuccesful console logs error
+    const submitCredentials = (credentials) => {
+        return axiosAuthWithToken()
+            .post('/api/login', { username: credentials.username, password: credentials.password })
+            .then (res => {
+                localStorage.setItem('token', res.data.payload)
+                console.log(res.data.payload)
+            })
+            .catch(err => {
+                setErrors(err.response.data.error)
+            })
+    }
+    
+    // handle submit
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // console.log(formValues)
+        submitCredentials(formValues)
+    }
 
     return (
         <div>
             <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                     <label htmlFor="username">Username
+                        <input
+                            type = "text"
+                            name="username"
+                            value = {formValues.username}
+                            onChange = {e => setFormValues({...formValues, username: e.target.value})}
+                        />
+                        </label>
+                        <label htmlFor="password">Password
+                            <input
+                            type = "password"
+                            name="password"
+                            value = {formValues.password}
+                            onChange = {e => setFormValues({...formValues, password: e.target.value})}
+                            />
+                        </label>
+                        <button type = "submit">Login</button>
+                </form>
+                <p>{errors}</p>
+        
         </div>
     )
 }
