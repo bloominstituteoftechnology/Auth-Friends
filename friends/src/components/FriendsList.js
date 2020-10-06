98c92f6b06bab6bb6778d7b82638e3129236be33
Daniel Terry <daniel.terry2121@gmail.com>
2020-10-05 22:05:52 -0400

    MVP

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,7 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { axiosWithAuth } from '../api/axiousWithAuth';
+import { useHistory } from "react-router";
 
 const FriendsList = () => {
+//attempt to useHistory to rerender after axios post request 
+    const history = useHistory();
+//setting up form state and functions 
+    const [friendForm,setFriendForm] = useState({
+        id:Date.now(),
+        name:"",
+        age:"",
+        email:""
+    });
+//state for use effect
+    const [clicked, setClick] = useState(true);
+    const handleChanges = e => {
+        const newFormData = {
+            ...friendForm, [e.target.name] : e.target.value
+        }
+        setFriendForm(newFormData);
+    }
+    const handleSubmit = e => {
+        e.preventDefault();
+        axiosWithAuth()
+        .post('/friends', friendForm )
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(friendForm)
+        })
+    }
     const [data,setData] = useState([]);
     const getData = () => {
         axiosWithAuth()
@@ -18,14 +47,51 @@ const FriendsList = () => {
     useEffect(() => {
         getData();
         console.log(data);
-    }, []);
+    }, [clicked]);
     return(
         <div>
             <h2>Friends List:</h2>
             {data.map((friends) => (
                 <h3>Name:{friends.name}</h3>
             ))}
+            <div>
+        <h2>ADD FRIEND</h2>
+        <form onSubmit = {handleSubmit}>
+            <label htmlFor = "name">
+                Name:
+            <input
+            type = "text" 
+            name = "name"
+            placeholder = "name"
+            value = {friendForm.name}
+            onChange = {handleChanges}
+             />
+            </label>
+            <label htmlFor = "age">
+                Age:
+            <input
+            type = "text" 
+            name = "age"
+            placeholder = "age"
+            value = {friendForm.age}
+            onChange = {handleChanges}
+             />
+            </label>
+            <label htmlFor = "email">
+                Email:
+            <input
+            type = "email" 
+            name = "email"
+            placeholder = "email"
+            value = {friendForm.email}
+            onChange = {handleChanges}
+             />
+            </label>
+            <button onClick = {() => (setClick(!clicked) && setClick(!clicked))}>Add</button>
+        </form>
         </div>
+        </div>
+        
     )
 }
 
