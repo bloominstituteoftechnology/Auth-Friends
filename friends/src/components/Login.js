7d5328b69188eba9d210dbae3e09fe32053be2ca
Simone Ballard <simone.a.ballard@gmail.com>
2020-10-13 19:28:36 -0600

    axios and private route established

---
+++ friends/src/components/Login.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: '',
+        }
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login = e => {
+        e.preventDefault();
+
+        axios.post('http://localhost:5000/api/login'), this.state.credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            this.props.history.push('/friends');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
+
+    render() {
+    return(
+        <div>
+        <form>
+        <input
+        type="text"
+        name="username"
+        value={this.state.credentials.username}
+        onChange={this.handleChange}
+        />
+        <input
+        type="password"
+        name="password"
+        value={this.state.credentials.password}
+        onChange={this.handleChange}
+        />
+        <button>Submit</button>
+        </form>
+        </div>
+    )
+}
+}
