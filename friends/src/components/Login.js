dcea4334dd451787fe73d0767c709e83b4e47d82
Katiuska <katiuskaalicea@gmail.com>
2020-11-30 22:59:23 -0800

    created handlechange function and part of the login function for when the form is submitted

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -5,19 +5,37 @@ const Login = () => {
     username: "",
     password: "",
   });
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    //the meat goes here
+    setCredentials({
+      username: "",
+      password: "",
+    });
+  };
   return (
     <div>
       <h2>Login Component</h2>
-      <form>
+      <form onSubmit={login}>
         <input
           value={credentials.username}
           name="username"
           placeholder="Enter Username"
+          onChange={handleChange}
         />
         <input
           value={credentials.password}
           name="password"
           placeholder="Enter Password"
+          onChange={handleChange}
         />
         <button>Login</button>
       </form>
