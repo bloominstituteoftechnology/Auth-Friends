a2827713ed74d7f2c51f29a4fa88f910c2a66b84
Katiuska <katiuskaalicea@gmail.com>
2020-11-30 22:53:51 -0800

    created form in login component, imported useState hook and the fields are being managed by the state

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
   return (
     <div>
       <h2>Login Component</h2>
+      <form>
+        <input
+          value={credentials.username}
+          name="username"
+          placeholder="Enter Username"
+        />
+        <input
+          value={credentials.password}
+          name="password"
+          placeholder="Enter Password"
+        />
+        <button>Login</button>
+      </form>
     </div>
   );
 };
