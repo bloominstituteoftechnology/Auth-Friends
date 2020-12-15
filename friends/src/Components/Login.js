fd016ff725b0f858165f48cd3601575b477b1280
c_melchor <cah.melchor@gmail.com>
2020-12-15 12:04:25 -0800

    set up onchange func

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -1,24 +1,51 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Login(props) {
-  const [credentials, setCredentials] = useState({
+const initialState = {
+  credentials: {
     username: "",
     password: ""
-  });
+  }
+};
+
+export default function Login(props) {
+  const [credentials, setCredentials] = useState(initialState);
+
+  const onChange = e => {
+    setCredentials({
+      ...initialState,
+      credentials: { ...credentials, [e.target.name]: e.target.value }
+    });
+  };
 
   return (
     <div>
       <form>
         <label htmlFor="username">
           Username:
-          <input id="username" type="text" placeholder="username" />
+          <input
+            id="username"
+            name="username"
+            type="text"
+            placeholder="username"
+            value={credentials.username}
+            onChange={onChange}
+          />
         </label>
         <br />
         <label htmlFor="password">
           Password:
-          <input id="password" type="text" placeholder="password" />
+          <input
+            id="password"
+            name="password"
+            type="text"
+            placeholder="password"
+            value={credentials.password}
+            onChange={onChange}
+          />
         </label>
+        <br />
+        <button>Login</button>
       </form>
     </div>
   );
