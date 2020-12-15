2db83e5ab91d4ddfbb0ba697c38e7a8adfa81a6a
Sonyei <esonyei@gmail.com>
2020-12-15 18:52:46 -0500

    routes implemented and login rendering

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import Login from './components/Login'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-    <Login/>
+    <ul>
+    <li> <Link to='login'>Login</Link></li>
+    </ul>
+    <Switch>
+    <Route path='/login' component={Login}/>
+    </Switch>
     </div>
+    </Router>
   );
 }
 
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -24,7 +24,8 @@ class Login extends React.Component {
         axios
         .post('http://localhost:5000/api/login', this.state.credentials)
         .then(res => {
-            localStorage.setItem('token', res.data.payload)
+            console.log(res)
+            localStorage.setItem("token", res.data.payload)
         })
         .catch(err => {
             console.log('ERROR: ', err)
