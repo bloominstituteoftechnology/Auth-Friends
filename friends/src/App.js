740c6836445b3af50b6808b7bd54a3fb768b2bff
Francis Bonacic <francisbonacic@icloud.com>
2020-12-15 16:59:16 -0800

    starting out

---
+++ friends/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Again</h1>
+    </div>
+  );
+}
+
+export default App;
