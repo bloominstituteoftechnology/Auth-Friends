9599abb2176059776f0580e98f9eeac60b80e240
Ahmad Wali Sayadi <ahmadwalisayadi@Ahmads-MacBook-Pro.local>
2021-01-17 13:51:40 -0800

    project completed

---
+++ friends/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import "./styles.css"
+
+import Login from './components/Login'
+import FriendsList from "./components/FriendsList"
+import PrivateRoute from "./components/PrivateRoute"
+
+function App () {
+  return (
+    <Router>
+      <div className='App'>
+        <ul>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+          <li>
+            <Link to='/protected'>Friends</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute path='/protected' component={FriendsList} />
+          <Route path='/login' component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
