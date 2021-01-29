aed6f1f16a95a611b4548e9a486fac0da289d0da
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-01-28 18:27:16 -0700

    confused

---
+++ friends/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Login from './components/login'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Friends!</h1>
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ friends/src/components/friendList.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const friendList = () => {
+    const [ friends, setFriends ] = useState([]);
+    
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default friendList;
