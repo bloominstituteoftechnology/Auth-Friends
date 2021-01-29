03934952f5b0941a1fa777fb79123e2b7618a745
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-01-28 18:50:30 -0700

    server keeps going to 3000

--- friends/src/components/friendList.js
+++ friends/src/components/friendList.js
@@ -1,13 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
+import { axiosWithAuth } from '../axios/axiosWithAuth'
 
-const friendList = () => {
+const FriendList = () => {
     const [ friends, setFriends ] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get("/api/friends")
+        .then((res) => {
+            setFriends(res.data);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    },[]);
     
     return (
         <div>
-
+            {friends.map((e) => {
+                return (
+                    <div key={e.id}>
+                        <h2>{e.name}</h2>
+                        <h2>{e.age}</h2>
+                    </div>
+                )
+            })}
         </div>
     )
 };
 
-export default friendList;
+export default FriendList;
