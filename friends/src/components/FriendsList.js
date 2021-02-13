1a76c70d55c27fba81ce8ff07f2707f0d9e97255
Alan Mir <alanpaulphotos@gmail.com>
2021-02-12 23:01:01 -0500

    Created empty FriendsList.js

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+
+class FriendsList extends React.Component () {
+    
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    };
+};
+
+export default FriendsList;
