63904584774866d6294b04572fd307d8df1fb0f8
Alan Mir <alanpaulphotos@gmail.com>
2021-02-12 22:29:27 -0500

    Finished building out Login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,19 +1,48 @@
 import React from 'react';
+import axios from 'axios';
 
-const Login = () => {
-    return (
-        <div>
-            <form>
-                <label htmlFor="username">Username:</label>
-                <input id="username" name="username" />
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
 
-                <label htmlFor="password" >Password:</label>
-                <input id="password" name="password" />
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
 
-                <button>Submit</button>
-            </form>
-        </div>
-    )
+    login = e => {
+        e.preventDefault();
+        axios.post('http://localhost:5000/api/login', this.state.credentials)
+            .then(res => {
+                console.log("AM: Login.js: login: res: ", res);
+                localStorage.setItem("token", res.data.payload);
+            })
+            .catch(err => console.error("Cannot Login to server", err.message));
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.login} >
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" value={this.state.credentials.username} onChange={this.handleChange} />
+
+                    <label htmlFor="password" >Password:</label>
+                    <input type="password" id="password" name="password" value={this.state.credentials.password} onChange={this.handleChange} />
+
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+        }
 };
 
 export default Login;
