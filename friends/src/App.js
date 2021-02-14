34bbdc43f5799bb1c1c9ebb39073bbf7dc927e41
Nico Herrera <nherrera0831@gmail,com>
2021-02-13 21:04:15 -0800

    meets mvp

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,6 +4,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./components/Login";
 import FriendsList from "./components/FriendsList";
 import "./App.css";
+import NewFriend from "./components/NewFriend";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       </ul>
       <Switch>
         <ProtectedRoute exact path="/friends" component={FriendsList} />
+        <Route path="/newfriend" component={NewFriend} />
         <Route path="/login" component={Login} />
       </Switch>
     </div>
