332707570e9f7c036958943a4874681a52597b44
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-02-13 21:42:09 -0700

    App works, just needs styling.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import PrivateRoute from './components/PrivateRoute';
+
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 
 function App() {
+  const logout = e => {
+    localStorage.removeItem('token');
+  };
+
   return (
       <Router>
         <div>
           <nav>
             <Link to='/login'>Login</Link>
-            <Link to='/login'>Logout</Link>
+            <Link to='/login' onClick={logout}>Logout</Link>
             <Link to='/friendslist'>Friends List</Link>
           </nav>
         <Switch>
-          <Route path='/friendslist' component={FriendsList} />
+          <PrivateRoute path='/friendslist' component={FriendsList} />
           <Route path='/login' component={Login} />
           <Route component={Login} />
         </Switch>
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,18 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 function FriendsList() {
-    useEffect(() => {
-        axios.get('http://localhost:5000/api/friends')
+    const [friends, setFriends] = useState([]);
+    const [newFriend, setNewFriend] = useState({
+        id: Math.random(),
+        name: '',
+        age: '',
+        email: ''
+    });
+
+    const handleChange = e => {
+        setNewFriend({...newFriend, [e.target.name]: e.target.value})
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axiosWithAuth().post('http://localhost:5000/api/friends', newFriend)
         .then(res => {
             console.log(res)
+            setFriends(res.data)
+            setNewFriend({
+                id: Math.random(),
+                name: '',
+                age: '',
+                email: ''
+            })
+        })
+        .catch(err => console.log(err))
+    };
+
+    useEffect(() => {
+        axiosWithAuth().get('http://localhost:5000/api/friends')
+        .then(res => {
+            console.log(res)
+            setFriends(res.data)
         })
         .catch(err => console.log(err))
     }, []);
-    
+
     return (
         <div>
-
+            <form onSubmit={handleSubmit}>
+                <input 
+                    name='name'
+                    value={newFriend.name}
+                    onChange={handleChange}
+                />
+                <input 
+                    name='age'
+                    value={newFriend.age}
+                    onChange={handleChange}
+                />
+                <input 
+                    name='email'
+                    value={newFriend.email}
+                    onChange={handleChange}
+                />
+                <button>Add Friend</button>
+            </form>
+            {friends.map(friend => (
+                <h4 key={friend.id}>{friend.name}</h4>
+            ))}
         </div>
     );
 };
