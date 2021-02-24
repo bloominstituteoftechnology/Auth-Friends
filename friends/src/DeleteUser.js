6a185ac4184a0534a14aab8967ee9b6ba6a2f96c
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-02-23 19:33:01 -0500

    commit

---
+++ friends/src/DeleteUser.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import axios from "axios"
+
+
+
+export default function DeleteUser () {
+
+    const initialState = {
+        id:Date.now,
+        name:"",
+        age:"",
+        email:""
+    }
+
+const [friend, setFriend] = useState(initialState)
+
+const handleChange = e => {
+    setFriend(
+       {
+        ...friend,
+        [ e.target.name ] : e.target.value
+      }
+    );
+  };
+  const onSubmit = () =>{    
+  axios
+        .delete("http://localhost:5000/api/friends", friend.id)
+            .then(response =>{
+                setFriend(response.data)
+
+
+            })
+                .catch(err=>{
+                    console.log(err)
+                })
+
+  }
+
+
+    return (
+       <div>
+            <h2>Delete a Friend</h2>
+            <form onSubmit={onSubmit}>
+                <label>
+                    <input
+                     name="name"
+                    value={friend.name}
+                     onChange={handleChange}
+                     />
+                </label>
+                <label>
+                     <input
+                     name="age"
+                     value={friend.age}
+                     onChange={handleChange}
+                     />
+                </label>
+                 <label>
+                    <input
+                    name="email"
+                    value={friend.email}
+                     onChange={handleChange}
+                     />
+                </label>
+      </form>
+             <div>
+                 <button onChange={handleChange}></button>
+            </div>
+        </div>
+    )
